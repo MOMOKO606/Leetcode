d61a505d9cb7955856f2529e1b75f61905efd4af
Long Bian <bianlong0618@qq.com>
2023-10-19 20:15:03 -0400

    Time: 137 ms (61.32%), Space: 18.5 MB (32.58%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,24 +3,23 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        def dfsHelper(i, j, symbol):
+        def dfsHelper(i, j):
             if not (0 <= i < rows and 0 <= j < cols and board[i][j] == "O"): return 
-            board[i][j] = symbol
+            board[i][j] = "$"
             for x, y in [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]:
-                dfsHelper(x, y, symbol)
+                dfsHelper(x, y)
 
 
         rows, cols = len(board), len(board[0])
         for j in [0, cols - 1]:
             for i in range(rows):
-                dfsHelper(i, j, "$") 
+                dfsHelper(i, j) 
         for i in [0, rows - 1]:
             for j in range(cols):
-                dfsHelper(i, j, "$")
-        for i in range(1, rows):
-            for j in range(1, cols):
-                dfsHelper(i, j, "X")
+                dfsHelper(i, j)
+
         for i in range(rows):
             for j in range(cols):
                 if board[i][j] == "$": board[i][j] = "O"
+                elif board[i][j] == "O": board[i][j] = "X"
         
