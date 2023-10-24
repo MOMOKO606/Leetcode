4940b05dc444a4a13235505312e524b76021634e
Long Bian <bianlong0618@qq.com>
2023-10-24 10:22:02 -0400

    Time: 1108 ms (91.52%), Space: 55.3 MB (24.17%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -3,8 +3,6 @@ class Solution:
         intervals, ans, maxEnd = sorted(intervals, key = lambda x: x[1]), 0, -math.inf
         for start, end in intervals:
             if start >= maxEnd: maxEnd = end
-            else:
-                ans += 1
-                maxEnd = min(maxEnd, end)
+            else: ans, maxEnd = ans + 1, min(maxEnd, end)
         return ans
         
