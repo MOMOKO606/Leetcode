36c462aa0e00ca4cf44fa8ef6f8502ed54a12f09
Long Bian <bianlong0618@qq.com>
2023-10-27 17:47:04 -0400

    Time: 48 ms (37.55%), Space: 16.6 MB (29.22%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -2,6 +2,7 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations, ans = sorted(citations, reverse = True), 0
         for i, num in enumerate(citations):
-            if num >= i + 1: ans = max(ans, i + 1)
+            ans = max(ans, min(num, i + 1))
         return ans
+
         
