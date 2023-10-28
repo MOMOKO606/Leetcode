1af3589f79eb69c120d655c80b114a3f54d140bb
Long Bian <bianlong0618@qq.com>
2023-10-28 19:36:48 -0400

    Time: 57 ms (19.18%), Space: 16.5 MB (36.1%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,7 +3,7 @@ class Solution:
         low, high = 0, len(nums) - 1
         while low < high:
             mid = (low + high) // 2
-            if nums[mid] < nums[mid + 1]: low = mid + 1
+            if nums[mid] <= nums[mid + 1]: low = mid + 1
             else: high = mid
         return low
         
