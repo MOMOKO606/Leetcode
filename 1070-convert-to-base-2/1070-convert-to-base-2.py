d9b5d89e15045786ab63391beb3b4ca84c30e6d5
Long Bian <bianlong0618@qq.com>
2024-06-25 23:05:31 -0400

    Time: 37 ms (43.11%), Space: 16.5 MB (42.22%) - LeetHub

---
+++ 1070-convert-to-base-2/1070-convert-to-base-2.py
@@ -0,0 +1,5 @@
+class Solution:
+    def baseNeg2(self, n: int) -> str:
+        if n == 1 or n == 0: return str(n)
+        if n & 1: return self.baseNeg2(math.ceil(n / -2)) + "1"
+        else: return self.baseNeg2(n // -2) + "0"
