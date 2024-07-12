7763321a3ba6da152b562d52f604daca2dbbe089
Long Bian <bianlong0618@qq.com>
2024-07-12 14:15:43 -0400

    Time: 34 ms (74.39%), Space: 16.7 MB (29.88%) - LeetHub

--- 1057-numbers-with-repeated-digits/1057-numbers-with-repeated-digits.py
+++ 1057-numbers-with-repeated-digits/1057-numbers-with-repeated-digits.py
@@ -1,14 +1,15 @@
 class Solution:
     def numDupDigitsAtMostN(self, n: int) -> int:
-        digits, ans, visited = [], n, set([])
-        while n: digits, n = digits + [n % 10], n // 10        
-        digits, n = digits[::-1], len(digits)
-        for i in range(n - 1): ans -= 9 * perm(9, i)
+        digits, ans, visited = [], n, set()
+        while n: n, digits = n // 10, digits + [n % 10]
+        digits = digits[::-1]
+        digits_length = len(digits)
+        for i in range(digits_length - 1): ans -= 9 * math.perm(9, i)
         for i, digit in enumerate(digits):
             for j in range(i == 0, digit):
                 if j in visited: continue
-                ans -= perm(9 - i, n - i - 1)
+                ans -= math.perm(9 - i, digits_length - i - 1)
             if digit in visited: return ans
             visited.add(digit)
         return ans - 1
-
+        
