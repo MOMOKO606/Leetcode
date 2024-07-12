50d2b36205c074c5afba24168e842c50375ee4d3
Long Bian <bianlong0618@qq.com>
2024-07-12 09:05:21 -0400

    Time: 347 ms (82.64%), Space: 16.9 MB (5.52%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,27 +1,29 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        def helper(n, start, target, seq):
-            if n < 3:
-                i, j = start, len(nums) - 1
+        def helper(pos=0, n=4, target=target, seq=[]):
+            if n == 2: 
+                i, j = pos, len(nums) - 1
                 while i < j:
                     key = nums[i] + nums[j]
-                    if key == target: 
+                    if key == target:
                         self.ans.append(seq + [nums[i], nums[j]])
                         i += 1
                         while i < len(nums) and nums[i] == nums[i - 1]:
                             i += 1
                         j -= 1
                         while j >= 0 and nums[j] == nums[j + 1]:
-                            j -= 1
+                            j -= 1 
                     elif key < target:
                         i += 1
                     else:
-                        j -= 1       
+                        j -= 1
                 return 
-            for i in range(start, len(nums) - n + 1):
-                if i > start and nums[i] == nums[i - 1]: continue
-                helper(n - 1, i + 1, target - nums[i], seq + [nums[i]])
-
-        nums, self.ans = sorted(nums), []
-        helper(4, 0, target, [])
+            for k in range(pos, len(nums) - n + 1):
+                if k > pos and nums[k] == nums[k - 1]: continue
+                helper(k + 1, n - 1, target - nums[k], seq + [nums[k]])
+        
+        self.ans, nums = [], sorted(nums)
+        helper()
         return self.ans
+
+        
