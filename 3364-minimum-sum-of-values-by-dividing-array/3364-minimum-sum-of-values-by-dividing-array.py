ab720f03c144ea08ac7e0a188774ad01c0872bde
Long Bian <bianlong0618@qq.com>
2024-07-11 22:40:52 -0400

    Time: 1343 ms (48.91%), Space: 429.7 MB (43.07%) - LeetHub

---
+++ 3364-minimum-sum-of-values-by-dividing-array/3364-minimum-sum-of-values-by-dividing-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumValueSum(self, nums: List[int], andValues: List[int]) -> int:
+        @cache
+        def helper(i=0, j=0, _AND=-1):
+            if i == len(nums) and j == len(andValues): return 0
+            if i == len(nums) or j == len(andValues): return inf
+            _AND &= nums[i]
+            if _AND < andValues[j]: return inf
+            ans = helper(i + 1, j, _AND)
+            if _AND == andValues[j]: 
+                ans = min(ans, nums[i] + helper(i + 1, j + 1, -1))
+            return ans
+
+        ans = helper()
+        return ans if ans != inf else -1
