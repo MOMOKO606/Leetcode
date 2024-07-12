d230fed1ed409b975785ff4b64d05d45dec77ba4
Long Bian <bianlong0618@qq.com>
2024-07-12 10:42:17 -0400

    Time: 2886 ms (6.72%), Space: 18.3 MB (43.62%) - LeetHub

--- 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network.py
+++ 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network.py
@@ -1,35 +1,36 @@
 class Solution:
     def countPairsOfConnectableServers(self, edges: List[List[int]], signalSpeed: int) -> List[int]:
-        def dfsHelper(cur_node, weight, visited):
-            if cur_node in visited: return 0
-            visited.add(cur_node)
+        def dfsHelper(node, weight, visited):
+            if node in visited: return 0
+            visited.add(node)
             ans = 1 if not weight % signalSpeed else 0
-            for neighbor in graph[cur_node]:
-                ans += dfsHelper(neighbor, weight + graph[cur_node][neighbor], visited)
+            for neigbhor in graph[node]:
+                ans += dfsHelper(neigbhor, weight + graph[node][neigbhor], visited)
             return ans
-        
-        def get_paths_in_diff_directs(node):
-            paths = []
-            for neighbor in graph[node]:
-                paths.append(dfsHelper(neighbor, graph[node][neighbor], set([node])))
-            return paths
 
-        def get_result(paths):
+        def get_paths_in_differ_directs(node):
+            pairs = []
+            for neigbhor in graph[node]:
+                pairs.append(dfsHelper(neigbhor, graph[node][neigbhor], set([node])))
+            return pairs
+
+        def get_pairs(nums):
             count = 0
-            for i in range(len(paths) - 1):
-                for j in range(i + 1, len(paths)):
-                    count += paths[i] * paths[j]
+            for i in range(len(nums) - 1):
+                for j in range(i + 1, len(nums)):
+                    count += nums[i] * nums[j]
             return count
 
         # Build the graph
-        graph, n = collections.defaultdict(dict), len(edges) + 1
+        graph = collections.defaultdict(dict)
         for u, v, w in edges:
             graph[u][v] = w
             graph[v][u] = w
-
-        ans = []
-        for i in range(n):
-            paths = get_paths_in_diff_directs(i)
-            ans.append(get_result(paths))
+        
+        n, ans = len(edges) + 1, []
+        for node in range(n):
+            paths = get_paths_in_differ_directs(node)
+            pairs = get_pairs(paths)
+            ans.append(pairs)
         return ans
         
