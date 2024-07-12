c97f145b96a7dd1abe042e206b6ca977a2193d92
Long Bian <bianlong0618@qq.com>
2024-07-12 15:16:26 -0400

    Time: 137 ms (33.09%), Space: 23.9 MB (5.12%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.py
+++ 0087-scramble-string/0087-scramble-string.py
@@ -3,6 +3,6 @@ class Solution:
     def isScramble(self, s1: str, s2: str) -> bool:
         if s1 == s2: return True
         for i in range(1, len(s1)):
-            if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:])) or (self.isScramble(s1[i:], s2[:-i]) and self.isScramble(s1[:i], s2[-i:])):
-                return True
+            if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:])) or (self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:-i])): return True
         return False
+        
