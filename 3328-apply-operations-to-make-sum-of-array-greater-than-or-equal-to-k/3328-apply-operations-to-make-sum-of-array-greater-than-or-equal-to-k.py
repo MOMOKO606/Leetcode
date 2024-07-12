4435c108d3eff0fe7dcc14f0469c246db22d8315
Long Bian <bianlong0618@qq.com>
2024-07-12 15:40:28 -0400

    Time: 32 ms (82.87%), Space: 16.8 MB (6.94%) - LeetHub

--- 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.py
+++ 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.py
@@ -1,8 +1,8 @@
 class Solution:
     def minOperations(self, k: int) -> int:
         ans = inf
-        for i in range(math.ceil(k ** 0.5)):
-            ans = min(ans, math.ceil(k / (i + 1)) +  i - 1)
+        for i in range(1, math.ceil(k ** 0.5) + 1):
+            ans = min(ans, math.ceil(k / i) - 1 + i - 1)
         return ans
 
 
