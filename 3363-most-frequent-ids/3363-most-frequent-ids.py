def2139ec667a49b1ff341096e8ea7f56b99b212
Long Bian <bianlong0618@qq.com>
2024-07-12 19:40:37 -0400

    Time: 963 ms (64.64%), Space: 48.2 MB (49.72%) - LeetHub

--- 3363-most-frequent-ids/3363-most-frequent-ids.py
+++ 3363-most-frequent-ids/3363-most-frequent-ids.py
@@ -1,12 +1,13 @@
 class Solution:
     def mostFrequentIDs(self, nums: List[int], freq: List[int]) -> List[int]:
-        ans, freqs, max_heap = [], {}, []
+        freqs, maxHeap, ans = {}, [], []
         for id, f in zip(nums, freq):
             freqs[id] = freqs.get(id, 0) + f
-            heapq.heappush(max_heap, (-freqs[id], id))
-            while max_heap and -max_heap[0][0] != freqs[max_heap[0][1]]:
-                heapq.heappop(max_heap)
-            ans.append(-max_heap[0][0])
+            heapq.heappush(maxHeap, (-freqs[id], id))
+            while maxHeap and -maxHeap[0][0] != freqs[maxHeap[0][1]]:
+                heapq.heappop(maxHeap)
+            ans += [-maxHeap[0][0]] if maxHeap else [0]
         return ans
+
 
         
