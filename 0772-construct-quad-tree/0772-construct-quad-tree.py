ee7261a87313e68fbd5bcf97f745a729c98c09b3
Long Bian <bianlong0618@qq.com>
2024-07-13 14:59:10 -0400

    Time: 83 ms (88.42%), Space: 17.5 MB (9.06%) - LeetHub

--- 0772-construct-quad-tree/0772-construct-quad-tree.py
+++ 0772-construct-quad-tree/0772-construct-quad-tree.py
@@ -12,20 +12,21 @@ class Node:
 
 class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
-        def check_leaf(r, c, n):
+        def isLeaf(r, c, n):
             for i in range(r, r + n):
                 for j in range(c, c + n):
                     if grid[i][j] != grid[r][c]: return False
             return True
 
-        def helper(r, c, n):
-            if check_leaf(r, c, n): return Node(grid[r][c], True, None, None, None, None)
+        def helper(r=0, c=0, n=len(grid)):
+            if isLeaf(r, c, n): 
+                return Node(grid[r][c], True, None, None, None, None)
             n //= 2
             topLeft = helper(r, c, n)
             topRight = helper(r, c + n, n)
             bottomLeft = helper(r + n, c, n)
             bottomRight = helper(r + n, c + n, n)
-            return Node(0, False, topLeft, topRight, bottomLeft, bottomRight)
+            return Node(1, False, topLeft, topRight, bottomLeft, bottomRight)
 
-        return helper(0, 0, len(grid))
+        return helper()
         
