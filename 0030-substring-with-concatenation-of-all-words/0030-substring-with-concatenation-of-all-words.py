fda3f8cda691d92402eea50eef6f5fcfd4ce3870
Long Bian <bianlong0618@qq.com>
2024-07-14 11:20:22 -0400

    Time: 9448 ms (5.01%), Space: 16.9 MB (97.6%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -2,13 +2,13 @@ class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         word_length = len(words[0])
         window_length = word_length * len(words)
-        target, ans = Counter(words), []
+        target, ans, wordSet = Counter(words), [], set(words)
         for i in range(len(s) - window_length + 1):
-            seq, temp = s[i: i + window_length], []
+            seq, tmp = s[i: i + window_length], []
             while seq:
-                temp.append(seq[:word_length])
+                tmp.append(seq[:word_length])
                 seq = seq[word_length:]
-            if Counter(temp) == target: ans.append(i)
+            if Counter(tmp) == target: ans.append(i)
         return ans
 
         
