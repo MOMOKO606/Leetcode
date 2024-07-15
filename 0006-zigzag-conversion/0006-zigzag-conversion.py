387d4d37f11b68f1642f174220e760ea969f03e0
Long Bian <bianlong0618@qq.com>
2024-07-15 18:01:59 -0400

    Time: 481 ms (5.01%), Space: 23.8 MB (5.12%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,20 +1,20 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        table, deque, j = [[0] * len(s) for _ in range(numRows)], collections.deque(list(s)), 0
-        while deque:
-            # Going down
-            for i in range(numRows):
-                if not deque: break
-                table[i][j] = deque.popleft()
-            
-            # Going up
-            j += 1
-            for i in range(numRows - 2, 0, -1):
-                if not deque: break
-                table[i][j], j = deque.popleft(), j + 1
+        if numRows == 1: return s
+        s, cur_dirc_i, cur_dirc_j, matrix, i, j, ans = list(s), 1, 0, [[None] * len(s) for _ in range(numRows)], 0, 0, ""
+        while s:
+            char = s.pop(0)
+            matrix[i][j] = char
+            if i + cur_dirc_i == numRows:
+                cur_dirc_i, cur_dirc_j = -1, 1
+            elif i + cur_dirc_i == -1:
+                cur_dirc_i, cur_dirc_j = 1, 0
+            i += cur_dirc_i
+            j += cur_dirc_j
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]: ans += matrix[i][j]
+        return ans
 
-        # Print the table
-        return "".join([table[i][j] for i in range(numRows) for j in range(len(s)) if table[i][j]])
-
-            
         
