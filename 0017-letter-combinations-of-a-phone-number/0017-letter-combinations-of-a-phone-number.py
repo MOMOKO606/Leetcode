2cbdc3d72f4b0db20e0dfb52c7ed8fc9d7800d3f
Long Bian <bianlong0618@qq.com>
2024-07-16 17:12:58 -0400

    Time: 23 ms (98.75%), Space: 16.5 MB (64.06%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,16 +1,15 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        def helper(digits=digits):
-            if digits == "": return [""]
-            letters = {
-                "2": "abc", "3": "def", "4": "ghi", 
-                "5": "jkl", "6": "mno", "7": "pqrs",
-                "8": "tuv", "9": "wxyz",
-                }
-            return [letter + seq for letter in letters[digits[0]] for seq in helper(digits[1:])]
-        if digits == "": return []
-        return helper()
-
+        if not digits: return []
+        ans = [""]
+        letters = {
+            "2": "abc", "3": "def", "4": "ghi", 
+            "5": "jkl", "6": "mno", "7": "pqrs",
+            "8": "tuv", "9": "wxyz",
+            }
+        for digit in digits:
+            ans = [seq + char for char in letters[digit] for seq in ans]
+        return ans
 
 
         
