2caef2f32dac793560ea1336fa56b4613e31b660
Long Bian <bianlong0618@qq.com>
2024-07-16 09:01:59 -0400

    Time: 43 ms (78.11%), Space: 16.5 MB (77.5%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,24 +1,18 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        def helper(num):
-            if not num: return ""
-            if num >= 1000: return trans[1000] + helper(num - 1000)
-            elif num >= 900: return trans[900] + helper(num - 900)
-            elif num >= 500: return trans[500] + helper(num - 500)
-            elif num >= 400: return trans[400] + helper(num - 400)
-            elif num >= 100: return trans[100] + helper(num - 100)
-            elif num >= 90: return trans[90] + helper(num - 90)
-            elif num >= 50: return trans[50] + helper(num - 50)
-            elif num >= 40: return trans[40] + helper(num - 40)
-            elif num >= 10: return trans[10] + helper(num - 10)
-            elif num >= 9: return trans[9] + helper(num - 9)
-            elif num >= 5: return trans[5] + helper(num - 5)
-            elif num >= 4: return trans[4] + helper(num - 4)
-            else: return trans[1] + helper(num - 1)
-        trans = {1: "I", 4: "IV", 5: "V", 
-                         9: "IX", 10: "X", 
-                         40: "XL", 50: "L", 
-                         90: "XC", 100: "C",
-                         400: "CD", 500: "D",
-                         900: "CM", 1000: "M"}
-        return helper(num)
+        ans = ""
+        while num:
+            if num >= 1000: ans, num = ans + "M", num - 1000
+            elif num >= 900: ans, num = ans + "CM", num - 900
+            elif num >= 500: ans, num = ans + "D", num - 500
+            elif num >= 400: ans, num = ans + "CD", num - 400
+            elif num >= 100: ans, num = ans + "C", num - 100
+            elif num >= 90: ans, num = ans + "XC", num - 90
+            elif num >= 50: ans, num = ans + "L", num - 50
+            elif num >= 40: ans, num = ans + "XL", num - 40
+            elif num >= 10: ans, num = ans + "X", num - 10
+            elif num >= 9: ans, num = ans + "IX", num - 9
+            elif num >= 5: ans, num = ans + "V", num - 5
+            elif num >= 4: ans, num = ans + "IV", num - 4
+            elif num >= 1: ans, num = ans + "I", num - 1
+        return ans
