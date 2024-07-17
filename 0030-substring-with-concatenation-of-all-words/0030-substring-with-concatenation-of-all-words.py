a91e3e223d9c5ae35afdd84c813bee80a1140dba
Long Bian <bianlong0618@qq.com>
2024-07-17 17:38:53 -0400

    Time: 6900 ms (30.19%), Space: 17.3 MB (57.21%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,14 +1,30 @@
+from collections import defaultdict
+
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        word_length = len(words[0])
-        window_length = word_length * len(words)
-        target, ans, wordSet = Counter(words), [], set(words)
-        for i in range(len(s) - window_length + 1):
-            seq, tmp = s[i: i + window_length], []
-            while seq:
-                tmp.append(seq[:word_length])
-                seq = seq[word_length:]
-            if Counter(tmp) == target: ans.append(i)
-        return ans
-
+        if not s or not words:
+            return []
+        
+        word_count = defaultdict(int)
+        for word in words:
+            word_count[word] += 1
+        
+        substr_len = len(words) * len(words[0])
+        word_len = len(words[0])
+        result = []
+        
+        for i in range(len(s) - substr_len + 1):
+            seen = defaultdict(int)
+            for j in range(i, i + substr_len, word_len):
+                word = s[j:j+word_len]
+                if word in word_count:
+                    seen[word] += 1
+                    if seen[word] > word_count[word]:
+                        break
+                else:
+                    break
+            else:
+                result.append(i)
+        
+        return result
         
