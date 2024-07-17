b4699afb682ee6d22e38b54bad1c3bdc3b790bfc
Long Bian <bianlong0618@qq.com>
2024-07-17 19:41:21 -0400

    Time: 39 ms (43.3%), Space: 16.5 MB (42.09%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,12 +1,13 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         negative = -1 if dividend * divisor < 0 else 1
-        ans, dividend, divisor = 0, abs(dividend), abs(divisor)
+        dividend, divisor, ans = abs(dividend), abs(divisor), 0
         while divisor <= dividend:
-            copied_divisor, count = divisor, 1
-            while copied_divisor <= dividend:
-                dividend -= copied_divisor
-                ans += count
-                count += count
-                copied_divisor += copied_divisor
-        return sorted([-2 ** 31, ans * negative, 2 ** 31 - 1])[1]
+            divisor_copy, count = divisor, 1
+            while divisor_copy <= dividend:
+                ans, divisor_copy, count, dividend = ans + count, divisor_copy + divisor_copy, count + count, dividend - divisor_copy
+        ans *= negative
+        return sorted([ans, -2 ** 31, 2 ** 31 - 1])[1]
+
+
+        
