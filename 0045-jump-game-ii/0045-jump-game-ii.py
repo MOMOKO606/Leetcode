20d4320f1a6004a90c9b486b9e10d32717ceeb81
Long Bian <bianlong0618@qq.com>
2024-07-20 15:57:44 -0400

    Time: 96 ms (93.54%), Space: 17.5 MB (85.05%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,11 +1,12 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        reach, nextReach, steps = 0, -math.inf, 0
+        reach, steps, next_reach = 0, 0, -inf
         for i, num in enumerate(nums):
-            if reach >= len(nums) - 1: return steps
-            nextReach = max(nextReach, i + num)
-            if i == reach: reach, steps = nextReach, steps + 1
+            next_reach = max(next_reach, i + num)
+            if reach >= len(nums) - 1:
+                return steps
+            if i == reach:
+                reach, steps = next_reach, steps + 1
             
-
-
+        
         
