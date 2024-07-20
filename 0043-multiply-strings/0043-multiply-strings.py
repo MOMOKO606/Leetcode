c7a770d39b3713b41fcc75c2fb2d4a6889b22fd7
Long Bian <bianlong0618@qq.com>
2024-07-20 16:25:45 -0400

    Time: 62 ms (44.46%), Space: 16.7 MB (16%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,19 +1,16 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         if num1 == "0" or num2 == "0": return "0"
-        num1, num2, m, n = list(map(int, list(num1))), list(map(int, list(num2))), len(num1), len(num2)
-        ans = [0] * (m + n - 1)
+        num1, num2 = list(map(int, list(num1))), list(map(int, list(num2)))
+        m, n = len(num1), len(num2)
+        digits, carry = [0] * (m + n - 1), 0
         for i in range(m):
             for j in range(n):
-                ans[i + j] += num1[i] * num2[j]
-        carry = 0
-        for k in range(len(ans) - 1, -1, -1):
-            ans[k] += carry
-            carry = ans[k] // 10
-            ans[k] %= 10
-        if carry: ans = [carry] + ans
-        return "".join(map(str, ans))
-
-
-
+                digits[i + j] += num1[i] * num2[j]
+        for i in range(len(digits) - 1, -1, -1):
+            digits[i] += carry
+            carry = digits[i] // 10
+            digits[i] %= 10
+        if carry: digits = [carry] + digits
+        return "".join(map(str, digits))
         
