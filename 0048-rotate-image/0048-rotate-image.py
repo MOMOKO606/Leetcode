effe85a91f6580e600362ff3acb2fbe447ee5e7d
Long Bian <bianlong0618@qq.com>
2024-07-21 19:51:22 -0400

    Time: 41 ms (39.73%), Space: 16.6 MB (34.7%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,22 +3,28 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows, cols = len(matrix), len(matrix[0])
-        backup = [[0] * cols for _ in range(rows)]
-        for i in range(rows):
-            for j in range(cols):
-                backup[i][j] = matrix[i][j]
+        def move_all(i, j, steps):
+            N, to_be_moved, di, dj = 4, matrix[i][j], 0, 1
+            while N:
+                next_i, next_j, di, dj = get_next(i, j, di, dj, steps)
+                matrix[next_i][next_j], to_be_moved = to_be_moved, matrix[next_i][next_j]
+                i, j = next_i, next_j
+                N -= 1
 
-        top, bottom, left, right = 0, rows - 1, 0, cols - 1
+        def get_next(i, j, di, dj, steps):
+            while steps:
+                if not (top <= i + di <= bottom and left <= j + dj <= right):
+                    di, dj = dj, -di
+                i, j, steps = i + di, j + dj, steps - 1
+            return i, j, di, dj
+
+
+        top, bottom, left, right = 0, len(matrix) - 1, 0, len(matrix) - 1
         while top < bottom:
-            for j in range(left, right + 1):
-                matrix[j][right] = backup[top][j]
-            for i in range(top, bottom + 1):
-                matrix[bottom][top + bottom - i] = backup[i][right]
-            for j in range(left, right + 1):
-                matrix[j][left] = backup[bottom][j]
-            for i in range(top, bottom + 1):
-                matrix[top][top + bottom - i] = backup[i][left]
+            steps = n = right - left
+            i, j = top, left
+            while n:
+                move_all(i, j, steps)
+                j += 1
+                n -= 1
             top, bottom, left, right = top + 1, bottom - 1, left + 1, right - 1
-
-        
