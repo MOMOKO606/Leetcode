883665a76e10090b7a9770d6bf26b82390af7ad8
Long Bian <bianlong0618@qq.com>
2024-07-21 21:41:47 -0400

    Time: 54 ms (56.92%), Space: 16.8 MB (65.8%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,11 +1,12 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        if not nums: return [[]]
-        visited, ans = set(), []
-        for i, num in enumerate(nums):
-            if num in visited: continue
-            visited.add(num)
-            for seq in self.permuteUnique(nums[:i] + nums[i + 1:]):
-                ans.append([nums[i]] + seq)
-        return ans
+        def helper(nums):
+            if not nums: return [[]]
+            ans = []
+            for i in range(len(nums)):
+                if i > 0 and nums[i] == nums[i - 1]: continue
+                ans += [[nums[i]] + seq for seq in helper(nums[:i] + nums[i + 1:])]
+            return ans
+        nums = sorted(nums)
+        return helper(nums)
         
