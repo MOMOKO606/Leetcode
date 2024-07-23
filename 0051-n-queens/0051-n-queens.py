91b2d4b44f867d1ee8bf4965c8bf0f93d6dcf9ba
Long Bian <bianlong0618@qq.com>
2024-07-22 23:34:11 -0400

    Time: 44 ms (89.77%), Space: 16.9 MB (87.25%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -2,19 +2,22 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         def helper(i=0, seq=[]):
             if i == n:
-                self.ans.append(seq[:])
-                return 
+                ans.append(seq[:])
+                return
             for j in range(n):
-                if j in cols or i + j in slashs or i - j in back_slashs: continue
+                if j in cols or i - j in slash or i + j in back_slash: continue
                 cols.add(j)
-                slashs.add(i + j)
-                back_slashs.add(i - j)
+                slash.add(i - j)
+                back_slash.add(i + j)
                 helper(i + 1, seq + [j])
                 cols.remove(j)
-                slashs.remove(i + j)
-                back_slashs.remove(i - j)
+                slash.remove(i - j)
+                back_slash.remove(i + j)
 
-        self.ans, cols, slashs, back_slashs = [], set(), set(), set()
+
+        cols, slash, back_slash, ans = set(), set(), set(), []
         helper()
-        return [["." * pos + "Q" + "." * (n - pos - 1) for pos in seq ]for seq in self.ans ]
+        return [["." * j + "Q" + "." * (n - j - 1) for j in seq] for seq in ans]
+
+
         
