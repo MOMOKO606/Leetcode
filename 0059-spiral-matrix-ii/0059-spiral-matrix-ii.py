79a9e7d32fbeb56bcbdcc7e9cd3fc46a7e6eede9
Long Bian <bianlong0618@qq.com>
2024-07-26 10:26:03 -0400

    Time: 30 ms (89.37%), Space: 16.5 MB (30.26%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -3,10 +3,9 @@ class Solution:
         matrix, i, j, di, dj, cur = [[0] * n for _ in range(n)], 0, 0, 0, 1, 1
         while cur <= n * n:
             matrix[i][j] = cur
-            if not (0 <= i + di < n and 0 <= j + dj < n) or matrix[i + di][j + dj] != 0:
+            if not (0 <= i + di < n and 0 <= j + dj < n) or matrix[i + di][j + dj]:
                 di, dj = dj, -di
-            i, j = i + di, j + dj
-            cur += 1
+            i, j, cur = i + di, j + dj, cur + 1
         return matrix
 
         
