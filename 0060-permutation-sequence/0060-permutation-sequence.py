4fe65807f8480f6a35dfc3929f556e97948e5dc7
Long Bian <bianlong0618@qq.com>
2024-07-26 21:53:01 -0400

    Time: 39 ms (48.67%), Space: 16.5 MB (58.52%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,20 +1,18 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        factorial, available,  ans = [1] * (n - 1), set([i for i in range(1, n + 1)]), ""
-        for i in range(len(factorial) - 2, -1 ,-1):
+        factorial, available, ans = [1] * (n - 1), set([i for i in range(1, n + 1)]), ""
+        for i in range(len(factorial) - 2, -1, -1):
             factorial[i] = factorial[i + 1] * (len(factorial) - i)
-
-        for i in range(n - 1):
+        for i in range(len(factorial)):
             for j in range(1, n + 1):
                 if j not in available: continue
                 if factorial[i] >= k:
                     ans += str(j)
                     available.remove(j)
                     break
-                else: k -= factorial[i]
-        return ans + str(available.pop())
-                
+                else:
+                    k -= factorial[i]
+        return ans + str(list(available).pop())
+
         
-
-
         
