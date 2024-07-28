5bf1967862cad42d98d9561eb811ac8e84d5605c
Long Bian <bianlong0618@qq.com>
2024-07-27 21:50:16 -0400

    Time: 46 ms (5.4%), Space: 17.3 MB (8.3%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,7 @@
+class Solution:
+    @cache
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 and n == 1: return 1
+        if m < 1 or n < 1: return 0
+        return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+        
