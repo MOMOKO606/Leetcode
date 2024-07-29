4d64b2feb1d5dbe1ab4bff631bac219d9fb8285e
Long Bian <bianlong0618@qq.com>
2024-07-29 11:22:36 -0400

    Time: 40 ms (43.09%), Space: 16.6 MB (39.6%) - LeetHub

--- 0065-valid-number/0065-valid-number.py
+++ 0065-valid-number/0065-valid-number.py
@@ -1,11 +1,18 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
+        # Ee只能出现1次，且不能在最前和最后
+        # Ee之前可以写一个带符号的整数 / 实数
+        # Ee之后也可以写一个带符号的整数 / 实数
+        # 所以Ee可以看作一个分隔符，当碰到valid的Ee之后，状态重制
         hasDigit, hasDec, hasE, hasSymbol = False, False, False, False
         for char in s:
             if char in "0123456789": hasDigit = True
-            elif char in "+-":
+            # 正负号前只能为空 或 Ee
+            # 即正负号前面有正负号/./数字都不行
+            elif char in "+-": 
                 if hasSymbol or hasDigit or hasDec: return False
                 else: hasSymbol = True
+            # Ee只能出现1次，且前面必须存在数字
             elif char in "Ee":
                 if not hasDigit or hasE: return False
                 else: hasE, hasSymbol, hasDigit, hasDec = True, False, False, False
