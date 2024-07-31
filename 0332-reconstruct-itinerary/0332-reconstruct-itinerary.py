962fdb3c5dfd6f54de549ef8924f0bae006c97cf
Long Bian <bianlong0618@qq.com>
2024-07-30 20:37:12 -0400

    Time: 81 ms (25.37%), Space: 16.9 MB (56.14%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        def dfsHelper(node):
+            neighbors = graph[node]
+            while neighbors:
+                dfsHelper(neighbors.pop(0))
+            ans.append(node)
+
+        # Build the graph
+        graph = collections.defaultdict(list)
+        for u, v in sorted(tickets):
+            graph[u].append(v)
+
+        ans = []
+        dfsHelper("JFK")
+        return ans[::-1]
+        
