77648998319b606222ebeec2b63ed284d114987a
Long Bian <bianlong0618@qq.com>
2024-08-02 21:55:43 -0400

    Time: 4285 ms (44.33%), Space: 16.6 MB (21.38%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,17 +2,18 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def dfsHelper(i, j, k):
             if k == len(word): return True
-            if not (0 <= i < rows and 0 <= j < cols and board[i][j] == word[k]): return False
+            if not (0 <= i < rows and 0 <= j < cols) or board[i][j] != word[k]: return False
             ori = board[i][j]
             board[i][j] = "$"
-            for x, y in [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]:
-                if dfsHelper(x, y, k + 1): return True
+            for di, dj in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+                if dfsHelper(i + di, j + dj, k + 1): return True
             board[i][j] = ori
-            return False
+            return False 
 
         rows, cols = len(board), len(board[0])
         for i in range(rows):
             for j in range(cols):
                 if dfsHelper(i, j, 0): return True
         return False
+
         
