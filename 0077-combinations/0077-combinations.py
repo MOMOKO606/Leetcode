6c752b1a5ee6b7390b2f9abbb7177db61f3e9247
Long Bian <bianlong0618@qq.com>
2024-08-03 14:06:02 -0400

    Time: 823 ms (13.19%), Space: 114.9 MB (5.83%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,4 +1,5 @@
 class Solution:
+    @cache
     def combine(self, n: int, k: int) -> List[List[int]]:
         if not k: return [[]]    
         return [[num] + seq for num in range(n, k - 1, -1) for seq in self.combine(num - 1, k - 1)]
