361aee9d81b62a59f93e55734b42e2fb69e43d8c
Long Bian <bianlong0618@qq.com>
2024-08-05 11:47:56 -0400

    Time: 37 ms (65.45%), Space: 16.3 MB (96.58%) - LeetHub

--- 0065-valid-number/0065-valid-number.py
+++ 0065-valid-number/0065-valid-number.py
@@ -1,24 +1,23 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
-        # Ee只能出现1次，且不能在最前和最后
-        # Ee之前可以写一个带符号的整数 / 实数
-        # Ee之后也可以写一个带符号的整数
-        # 所以Ee可以看作一个分隔符，当碰到valid的Ee之后，状态重制
-        hasDigit, hasDec, hasE, hasSymbol = False, False, False, False
+        hasSign, hasDigit, hasDec, hasEe = False, False, False, False
         for char in s:
             if char in "0123456789": hasDigit = True
-            # 正负号前只能为空 或 Ee
-            # 即正负号前面有正负号/./数字都不行
-            elif char in "+-": 
-                if hasSymbol or hasDigit or hasDec: return False
-                else: hasSymbol = True
-            # Ee只能出现1次，且前面必须存在数字
-            elif char in "Ee":
-                if not hasDigit or hasE: return False
-                else: hasE, hasSymbol, hasDigit, hasDec = True, False, False, False
+            elif char in "+-":
+                # 不能存在2个+-
+                # 不能在digit后再出现+-
+                # 不能在小数点后再出现+- 
+                if hasSign or hasDigit or hasDec: return False
+                hasSign = True
             elif char == ".":
-                if hasDec or hasE: return False
-                else: hasDec = True
-            else: return False
+                # 不能存在2个dot
+                # Ee后也不能有dot
+                if hasDec or hasEe: return False
+                hasDec = True
+            elif char in "Ee":
+                if hasEe or not hasDigit: return False
+                hasEe, hasSign, hasDigit, hasDec = True, False, False, False
+            else:
+                return False
         return hasDigit
         
