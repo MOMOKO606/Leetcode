2a8233de8ea26ffe00e72081ad31b23a02542453
Long Bian <bianlong0618@qq.com>
2024-08-08 11:02:26 -0400

    Time: 209 ms (84.41%), Space: 24.4 MB (60.66%) - LeetHub

--- 1368-web-crawler-multithreaded/1368-web-crawler-multithreaded.py
+++ 1368-web-crawler-multithreaded/1368-web-crawler-multithreaded.py
@@ -8,31 +8,50 @@
 #        :type url: str
 #        :rtype List[str]
 #        """
-import threading, queue
+
+from concurrent.futures import ThreadPoolExecutor
+from threading import Condition
 
 class Solution:
-    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
-        def _crawl():
-            while True:
-                tmp = []
-                for url in htmlParser.getUrls(curQueue.get()):
-                    if url.split("http://")[1].split("/")[0] == domain and url not in visited:
-                        tmp.append(url)
-                        visited.add(url)
-                nextQueue.put(tmp)
-        
-        
-        
-        curQueue, nextQueue, visited, running, domain = queue.Queue(), queue.Queue(), set([startUrl]), 1, startUrl.split("http://")[1].split("/")[0]
-        curQueue.put(startUrl)
-        for _ in range(5):
-            thread = threading.Thread(target=_crawl, daemon=True)
-            thread.start()
-        while running:
-            for url in nextQueue.get():
-                curQueue.put(url)
-                running += 1
-            running -= 1
-        return list(visited)
+    def __init__(self) -> None:
+        self._queue = list()
+        self._lock = Condition()
+        self._visited = set()
 
+    def get_hostname(self, url: str):
+        hostname = '.'.join(url.split('/')[2].split('.')[1:]) 
+        return hostname
+
+    def visit_url(self, url: str):
+        next_urls: List[str] = self._parser.getUrls(url)
+
+        with self._lock:
+            for next_url in next_urls:
+                if next_url not in self._visited and self.current_hostname == self.get_hostname(next_url) :
+                    self._visited.add(next_url)
+                    self._queue.insert(0,next_url)
+
+
+
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        self._queue.insert(0,startUrl)
+        self._visited.add(startUrl)
+        self.current_hostname = self.get_hostname(startUrl)
+        self._parser = htmlParser
+        
+        executor = ThreadPoolExecutor()
+
+        while self._queue:
+            urls = [self._queue.pop(), ]
+
+            while self._queue:
+                urls.append(self._queue.pop())
+
+            excecutor_list = [executor.submit(self.visit_url, (url)) for url in urls]
+            for future in excecutor_list:
+                future.result()
+        
+        executor.shutdown()
+
+        return list(self._visited)
         
