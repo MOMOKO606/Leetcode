3072037777ddd7c498f03e889f93a6ab06cb650f
Long Bian <bianlong0618@qq.com>
2024-08-08 10:41:51 -0400

    Time: 207 ms (86.92%), Space: 25.5 MB (24.64%) - LeetHub

---
+++ 1368-web-crawler-multithreaded/1368-web-crawler-multithreaded.py
@@ -0,0 +1,38 @@
+# """
+# This is HtmlParser's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class HtmlParser(object):
+#    def getUrls(self, url):
+#        """
+#        :type url: str
+#        :rtype List[str]
+#        """
+import threading, queue
+
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        def _crawl():
+            while True:
+                tmp = []
+                for url in htmlParser.getUrls(curQueue.get()):
+                    if url.split("http://")[1].split("/")[0] == domain and url not in visited:
+                        tmp.append(url)
+                        visited.add(url)
+                nextQueue.put(tmp)
+        
+        
+        
+        curQueue, nextQueue, visited, running, domain = queue.Queue(), queue.Queue(), set([startUrl]), 1, startUrl.split("http://")[1].split("/")[0]
+        curQueue.put(startUrl)
+        for _ in range(5):
+            thread = threading.Thread(target=_crawl, daemon=True)
+            thread.start()
+        while running:
+            for url in nextQueue.get():
+                curQueue.put(url)
+                running += 1
+            running -= 1
+        return list(visited)
+
+        
