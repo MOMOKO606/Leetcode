328fe62f387e6da1cf415d519cbdb4f005944803
Long Bian <bianlong0618@qq.com>
2024-08-11 21:13:36 -0400

    Time: 3237 ms (5.01%), Space: 483.7 MB (5.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -4,7 +4,10 @@ class Solution:
         def helper(i, amount):
             if not amount: return 0
             if amount < 0 or i == len(coins): return inf
-            return min(1 + helper(i, amount - coins[i]), helper(i + 1, amount))
+            ans = inf
+            for j in range(i, len(coins)):
+                ans = min(ans, 1 + helper(j, amount - coins[j]))
+            return ans
 
         ans = helper(0, amount)
         return ans if ans != inf else -1
