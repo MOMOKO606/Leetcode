ab13426073ed40161b7be871420065e83e6db97c
Long Bian <bianlong0618@qq.com>
2024-08-11 23:12:12 -0400

    Time: 34 ms (81.81%), Space: 16.5 MB (78.58%) - LeetHub

--- 0065-valid-number/0065-valid-number.py
+++ 0065-valid-number/0065-valid-number.py
@@ -1,26 +1,18 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
-        hasSign, hasDigit, hasDec, hasEe = False, False, False, False
+        hasDigit, hasDec, hasEe, hasSign = False, False, False, False
         for char in s:
-            if char in "0123456789": hasDigit = True
+            if char in "0123456789":
+                hasDigit = True
             elif char in "+-":
-                # 不能存在2个+-
-                # 不能在digit后再出现+-
-                # 不能在小数点后再出现+- 
                 if hasSign or hasDigit or hasDec: return False
                 hasSign = True
             elif char == ".":
-                # 不能存在2个dot
-                # Ee后也不能有dot
-                if hasDec or hasEe: return False
+                if hasEe or hasDec: return False
                 hasDec = True
             elif char in "Ee":
-                # 不能存在2个Ee
-                # Ee前面必须有数字
-                # Ee后重制状态
                 if hasEe or not hasDigit: return False
-                hasEe, hasSign, hasDigit, hasDec = True, False, False, False
-            else:
-                return False
+                hasEe, hasDigit, hasDec, hasSign = True, False, False, False
+            else: return False
         return hasDigit
         
