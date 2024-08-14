9f5a26f9360588fa4e09b659154d84012f117bd5
Long Bian <bianlong0618@qq.com>
2024-08-13 23:08:51 -0400

    Time: 72 ms (76.33%), Space: 16.9 MB (59.95%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -10,7 +10,8 @@ class Solution:
         graph = collections.defaultdict(list)
         for u, v in sorted(tickets):
             graph[u].append(v)
-
+        
+        
         ans = []
         dfsHelper("JFK")
         return ans[::-1]
