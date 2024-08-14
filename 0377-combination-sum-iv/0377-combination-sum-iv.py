7058325c46b5967e47a18b5ee221716f12a6738c
Long Bian <bianlong0618@qq.com>
2024-08-14 15:29:13 -0400

    Time: 50 ms (9.92%), Space: 16.8 MB (10.1%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        @cache
+        def helper(target):
+            if target < 0: return 0
+            if target == 0: return 1
+            ans = 0
+            for num in nums:
+                ans += helper(target - num)
+            return ans
+
+        return helper(target)
+        
