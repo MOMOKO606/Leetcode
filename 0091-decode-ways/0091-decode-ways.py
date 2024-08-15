4caf41e34d6d317b0aa11b6ebfb50b749b5a9310
Long Bian <bianlong0618@qq.com>
2024-08-15 15:20:04 -0400

    Time: 34 ms (71.82%), Space: 17.2 MB (8.05%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -3,7 +3,7 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         if not s: return 1
         ans = 0
-        if 0 < int(s[0]) < 10: ans += self.numDecodings(s[1:])
-        if 9 < int(s[:2]) < 27: ans += self.numDecodings(s[2:])
+        if 1 <= int(s[:1]) <= 9: ans += self.numDecodings(s[1:])
+        if 10 <= int(s[:2]) <= 26: ans += self.numDecodings(s[2:])
         return ans
         
