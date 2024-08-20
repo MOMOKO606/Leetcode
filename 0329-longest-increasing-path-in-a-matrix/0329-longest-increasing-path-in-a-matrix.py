7b79e8df509d2088743330ce407adc515f56a452
Long Bian <bianlong0618@qq.com>
2024-08-20 15:56:40 -0400

    Time: 293 ms (77.61%), Space: 21.4 MB (27.1%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -2,22 +2,17 @@ class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         @cache
         def helper(i, j):
-            if not (0 <= i < rows and 0 <= j < cols and matrix[i][j] != "$"): 
-                return 0
-            ori = matrix[i][j]
-            count, longest, matrix[i][j] = 1, 0, "$"
-            for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
-                if 0 <= x < rows and 0 <= y < cols and matrix[x][y] != "$" and matrix[x][y] < ori:
-                    longest = max(longest, helper(x, y))
-            matrix[i][j] = ori
-            return count + longest
+            if not (0 <= i < rows and 0 <= j < cols): return 0
+            ans = 0
+            for x, y in [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]:
+                if not (0 <= x < rows and 0 <= y < cols) or matrix[x][y] >= matrix[i][j]: continue
+                ans = max(ans, helper(x, y))
+            return ans + 1
+
         
         rows, cols, ans = len(matrix), len(matrix[0]), 1
         for i in range(rows):
             for j in range(cols):
                 ans = max(ans, helper(i, j))
         return ans
-
-        
-
         
