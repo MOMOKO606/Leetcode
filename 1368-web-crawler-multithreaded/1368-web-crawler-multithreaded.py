6633eda835b929d2d4f6e7687b9ab5fbf4dc9d7c
Long Bian <bianlong0618@qq.com>
2024-08-20 15:33:39 -0400

    Time: 203 ms (90.42%), Space: 25.5 MB (26.22%) - LeetHub

--- 1368-web-crawler-multithreaded/1368-web-crawler-multithreaded.py
+++ 1368-web-crawler-multithreaded/1368-web-crawler-multithreaded.py
@@ -8,33 +8,28 @@
 #        :type url: str
 #        :rtype List[str]
 #        """
-import threading
-import queue
-from concurrent.futures import ThreadPoolExecutor
-
+import threading, queue
 class Solution:
     def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
-        def _crawl(url):
-            next_queue = []
-            for new_url in htmlParser.getUrls(url):
-                if new_url.split("http://")[1].split("/")[0] == domain and new_url not in visited:
-                    next_queue.append(new_url)
-                    visited.add(new_url)
-            return next_queue
+        def _crawl():
+            while True:
+                tmp = []
+                for new_url in htmlParser.getUrls(curQueue.get()):
+                    if new_url.split("http://")[1].split("/")[0] == domain and new_url not in visited:
+                        tmp.append(new_url)
+                        visited.add(new_url)
+                nextQueue.put(tmp)
 
-        curQueue, visited, domain = queue.Queue(), set([startUrl]), startUrl.split("http://")[1].split("/")[0]
+        curQueue, nextQueue, running, visited, domain = queue.Queue(), queue.Queue(), 1, set([startUrl]), startUrl.split("http://")[1].split("/")[0]
         curQueue.put(startUrl)
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            while not curQueue.empty():
-                futures = []
-                while not curQueue.empty():
-                    url = curQueue.get()
-                    futures.append(executor.submit(_crawl, url))
-                for future in futures:
-                    result = future.result()
-                    for url in result:
-                        curQueue.put(url)
+        for _ in range(5):
+            thread = threading.Thread(target=_crawl, daemon=True)
+            thread.start()
+        while running:
+            for url in nextQueue.get():
+                curQueue.put(url)
+                running += 1
+            running -= 1
         return list(visited)
 
-
-
+        
