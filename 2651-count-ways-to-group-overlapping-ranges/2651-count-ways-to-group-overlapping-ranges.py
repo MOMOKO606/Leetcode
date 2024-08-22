53626af1f46e449590949f345e08bb86b0f6fae0
Long Bian <bianlong0618@qq.com>
2024-08-22 17:24:14 -0400

    Time: 763 ms (15.03%), Space: 55.7 MB (23.83%) - LeetHub

---
+++ 2651-count-ways-to-group-overlapping-ranges/2651-count-ways-to-group-overlapping-ranges.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countWays(self, ranges: List[List[int]]) -> int:
+        ranges = sorted(ranges)
+        cur_start, cur_end, intervals = ranges[0][0], ranges[0][1], []
+        for i in range(1, len(ranges)):
+            if ranges[i][0] <= cur_end:
+                cur_end = max(cur_end, ranges[i][1])
+            else:
+                intervals.append([cur_start, cur_end])
+                cur_start, cur_end = ranges[i][0], ranges[i][1]
+        intervals.append([cur_start, cur_end])
+        n = len(intervals)
+        return (2 ** n) % (10 ** 9 + 7)
+    
+
+
+        
