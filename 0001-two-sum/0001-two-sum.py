a51b06cd6f121d47397d13add66df02d983c95ef
Long Bian <bianlong0618@qq.com>
2024-08-24 15:37:04 -0400

    Time: 64 ms (47.53%), Space: 17.8 MB (29.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         visited = {}
-        for j, num in enumerate(nums):
-            if target - num in visited: return visited[target - num], j
-            else: visited[num] = j
-             
+        for i, num in enumerate(nums):
+            if target - num in visited: return [visited[target - num], i]
+            visited[num] = i
+            
+        
