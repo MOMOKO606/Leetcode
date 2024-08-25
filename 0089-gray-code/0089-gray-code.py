7eb2796a0f759e0357000c3e8b8a77fd199deea3
Long Bian <bianlong0618@qq.com>
2024-08-25 11:03:49 -0400

    Time: 65 ms (89.68%), Space: 23.8 MB (78.49%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,8 +1,9 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        ans, head = [0], 1
+        head, ans = 1, [0]
         for _ in range(n):
             ans += [head + prev for prev in reversed(ans)]
             head <<= 1
         return ans
+
         
