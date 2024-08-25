c4f62c49686c7d100c96b378bd6a04315a660d50
Long Bian <bianlong0618@qq.com>
2024-08-24 22:13:41 -0400

    Time: 38 ms (45.77%), Space: 16.5 MB (37.48%) - LeetHub

--- 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.py
+++ 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.py
@@ -1,14 +1,12 @@
 class Solution:
     def smallestSubsequence(self, s: str) -> str:
-        boundries, stack, inserted = {}, [], set()
+        stack, boundaries, exist = [], {char: i for i, char in enumerate(s)}, set()
         for i, char in enumerate(s):
-            boundries[char] = i
-
-        for i, char in enumerate(s):
-            if char in inserted: continue
-            while stack and char < stack[-1] and i < boundries[stack[-1]]:
-                node = stack.pop()
-                inserted.remove(node)
+            if char in exist: continue
+            while stack and stack[-1] > char and i < boundaries[stack[-1]]:
+                exist.remove(stack.pop())
+            exist.add(char)
             stack.append(char)
-            inserted.add(char)
         return "".join(stack)
+
+        
