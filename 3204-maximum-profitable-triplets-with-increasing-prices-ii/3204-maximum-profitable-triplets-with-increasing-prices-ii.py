ccc8d28a8d20a9803b06db110118cd57507cfab4
Long Bian <bianlong0618@qq.com>
2024-08-28 10:58:04 -0400

    Time: 2446 ms (14.29%), Space: 26.1 MB (100%) - LeetHub

--- 3204-maximum-profitable-triplets-with-increasing-prices-ii/3204-maximum-profitable-triplets-with-increasing-prices-ii.py
+++ 3204-maximum-profitable-triplets-with-increasing-prices-ii/3204-maximum-profitable-triplets-with-increasing-prices-ii.py
@@ -1,43 +1,36 @@
-# Modified Fenwick Tree for maximum num in nums[:index + 1]
-class Fenwick:
-    def __init__(self, n, original_value):
-        self.size = n
-        self.tree = [original_value] * n
+class FenwickTree:
+    def __init__(self, size, original_value):
+        self.size = size
+        self.tree = [original_value] * size
 
-    def _lowbit(self, index):
+    def __lowbit(self, index):
         return index & -index
 
     def update(self, index, num):
         while index < self.size:
             self.tree[index] = max(self.tree[index], num)
-            index += self._lowbit(index)
+            index += self.__lowbit(index)
 
     def query(self, index):
-        ans = -1
-        while index > 0:
+        ans = -inf
+        while index:
             ans = max(ans, self.tree[index])
-            index -= self._lowbit(index)
+            index -= self.__lowbit(index)
         return ans
-        
 
 class Solution:
     def maxProfit(self, prices: List[int], profits: List[int]) -> int:
-        n, ans = max(prices), -1
-        single, pair = Fenwick(n + 1, -1), Fenwick(2 * n + 1, -1)
+        n = max(prices)
+        ans, single, double = -1, FenwickTree(n + 1, 0), FenwickTree(2 * n + 1, 0)
         for price, profit in zip(prices, profits):
-            pair_max = pair.query(price - 1)
-            if pair_max > 0:
-                ans = max(ans, pair_max + profit)
-            
-            single_max = single.query(price - 1)
-            if single_max > 0:
-                pair.update(price, single_max + profit)
             single.update(price, profit)
+
+            single_max = single.query(price - 1)
+            if single_max: double.update(price, single_max + profit)
+
+            double_max = double.query(price - 1)
+            if double_max: ans = max(ans, double_max + profit)
+
         return ans
-
-            
-
-
-
-
+        
         
