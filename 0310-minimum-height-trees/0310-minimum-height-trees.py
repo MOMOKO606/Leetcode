36dae2266bff70eb3e5636dbe01a1b24ea592b2b
Long Bian <bianlong0618@qq.com>
2024-08-29 15:38:55 -0400

    Time: 425 ms (36.32%), Space: 26.9 MB (65.17%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -2,18 +2,22 @@ class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n < 3: return [i for i in range(n)]
         # Build the graph
-        graph = collections.defaultdict(list)
+        graph = defaultdict(list)
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
 
-        queue = [node for node in range(n) if len(graph[node]) == 1]
+        queue = [node for node in graph if len(graph[node]) < 2]
         while queue:
             if n < 3: return queue
             next_queue = set()
             for node in queue:
-                neighbor, n = graph[node].pop(), n - 1
-                graph[neighbor].remove(node)
-                if len(graph[neighbor]) < 2:
-                    next_queue.add(neighbor)
+                n -= 1
+                for neighbor in graph[node]:
+                    graph[neighbor].remove(node)
+                    if len(graph[neighbor]) < 2:
+                        next_queue.add(neighbor)
             queue = list(next_queue)
+                
+
+        
