5593355ce9a56b3ebc47571dc1d03d21b379e2fe
Long Bian <bianlong0618@qq.com>
2024-08-30 16:30:17 -0400

    Time: 802 ms (34.43%), Space: 35.2 MB (84.84%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.py
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.py
@@ -1,11 +1,12 @@
 class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
-        nums = [num % value for num in nums]
-        freqs = Counter(nums)
-        for i, num in enumerate(nums):
-            if i % value not in freqs or freqs[i % value] == 0: return i
-            freqs[i % value] -= 1
-        return len(nums)
- 
-        
-        
+        reminders = {}
+        for num in nums:
+            reminder = num % value
+            reminders[reminder] = reminders.get(reminder, 0) + 1
+
+        ans = 0
+        while True:
+            if ans % value not in reminders or not reminders[ans % value]: return ans
+            reminders[ans % value] -= 1
+            ans += 1
