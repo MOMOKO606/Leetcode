830ec22a51df8f7b5becfa00eb467dc3dc060387
Long Bian <bianlong0618@qq.com>
2024-08-30 10:55:34 -0400

    Time: 98 ms (28.12%), Space: 17 MB (96%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
@@ -1,8 +1,7 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        people.sort(key=lambda p: (-p[0], p[1]))
         ans = []
-        for p in people:
+        for p in sorted(people, key=lambda x: (-x[0], x[1])):
             ans.insert(p[1], p)
         return ans
 
