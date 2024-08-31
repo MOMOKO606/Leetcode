011f5b95a2f8f2222532c23a279753d6c4c2423e
Long Bian <bianlong0618@qq.com>
2024-08-30 22:00:29 -0400

    Time: 4435 ms (55.32%), Space: 17 MB (57.45%) - LeetHub

--- 3187-maximum-profitable-triplets-with-increasing-prices-i/3187-maximum-profitable-triplets-with-increasing-prices-i.py
+++ 3187-maximum-profitable-triplets-with-increasing-prices-i/3187-maximum-profitable-triplets-with-increasing-prices-i.py
@@ -1,45 +1,13 @@
-class FenwickTree:
-    def __init__(self, size, original_value):
-        self.size = size
-        self.tree = [original_value] * size
-
-    def __lowbit(self, index):
-        return index & -index
-
-    def update(self, index, num):
-        while index < self.size:
-            self.tree[index] = max(self.tree[index], num)
-            index += self.__lowbit(index)
-
-    def query(self, index):
-        ans = 0
-        while index:
-            ans = max(ans, self.tree[index])
-            index -= self.__lowbit(index)
-        return ans
-
 class Solution:
     def maxProfit(self, prices: List[int], profits: List[int]) -> int:
-        max_price, ans = max(prices), -1
-        single = FenwickTree(max_price + 1, 0)
-        double = FenwickTree(max_price * 2 + 1 + 1, 0)
-        for price, profit in zip(prices, profits):
-            # Update single
-            single.update(price, profit)
-            
-            # Update double
-            single_max = single.query(price - 1)
-            if single_max:
-                double.update(price, profit + single_max)
-            
-            # Update ans
-            double_max = double.query(price - 1)
-            if double_max: ans = max(ans, double_max + profit)
-
-            
-        return ans
-
-
-
+        pre_max, post_max, ans = [-inf] * len(prices), [-inf] * len(prices), -1
+        for i in range(len(prices) - 1):
+            for j in range(i + 1, len(prices)):
+                if prices[i] < prices[j]:
+                    pre_max[j] = max(pre_max[j], profits[i])
+                    post_max[i] = max(post_max[i], profits[j])
+        for i in range(len(prices)):
+            ans = max(ans, pre_max[i] + post_max[i] + profits[i])
+        return ans 
 
         
