8c94c1c8489c396f61f4bcb502fb24d7fc9ff558
Long Bian <bianlong0618@qq.com>
2024-09-05 18:55:55 -0400

    Time: 3952 ms (8.81%), Space: 24.3 MB (62.79%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    @cache
+    def minCut(self, s: str) -> int:
+        if not s: return -1
+        ans = inf
+        for i in range(1, len(s) + 1):
+            if s[:i] == s[:i][::-1]:
+                ans = min(ans, 1 + self.minCut(s[i:]))
+        return ans
+                
+        
