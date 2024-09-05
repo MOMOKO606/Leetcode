ef6b1c362817476994d4d1a789e278f0556bfce9
Long Bian <bianlong0618@qq.com>
2024-09-05 17:24:03 -0400

    Time: 114 ms (85%), Space: 18.2 MB (28.83%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,23 +3,25 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        def dfsHelper(i, j):
-            if not (0 <= i < rows and 0 <= j < cols and board[i][j] == "O"): return 
+        def helper(i, j):
+            if not (0 <= i < rows and 0 <= j < cols and board[i][j] == "O"): return
             board[i][j] = "$"
             for x, y in [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]:
-                dfsHelper(x, y)
+                helper(x, y)
 
 
         rows, cols = len(board), len(board[0])
-        for j in [0, cols - 1]:
-            for i in range(rows):
-                dfsHelper(i, j) 
         for i in [0, rows - 1]:
             for j in range(cols):
-                dfsHelper(i, j)
+                if board[i][j] == "O": helper(i, j)
+
+        for j in [0, cols - 1]:
+            for i in range(rows):
+                if board[i][j] == "O": helper(i, j)
 
         for i in range(rows):
             for j in range(cols):
-                if board[i][j] == "$": board[i][j] = "O"
-                elif board[i][j] == "O": board[i][j] = "X"
-        
+                if board[i][j] == "$":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
