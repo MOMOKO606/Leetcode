df2eb133a6576bd48bf8182a992cb8428af3dcc7
Long Bian <bianlong0618@qq.com>
2024-09-05 22:49:51 -0400

    Time: 40 ms (57.82%), Space: 16.8 MB (97.29%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -10,15 +10,17 @@ from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node: return node
-        clones, queue= {node: Node(val=node.val, neighbors=[])}, [node]
+        clones, queue, visited = {node: Node(val=node.val, neighbors=[])}, [node], set([node])
         while queue:
             next_queue = []
             for cur_node in queue:
                 for neighbor in cur_node.neighbors:
                     if neighbor not in clones:
                         clones[neighbor] = Node(val=neighbor.val, neighbors=[])
-                        next_queue.append(neighbor)
                     clones[cur_node].neighbors.append(clones[neighbor])
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        next_queue.append(neighbor)
             queue = next_queue
         return clones[node]
 
