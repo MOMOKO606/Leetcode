4314555860ae09b2227c3d4d9746324a3e383897
Long Bian <bianlong0618@qq.com>
2024-09-06 16:59:14 -0400

    Time: 625 ms (13.28%), Space: 78.3 MB (51.11%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,28 +1,44 @@
-class LRUCache:
+class DLListNode:
+    def __init__(self, prev=None, next=None, key=0, value=0):
+        self.prev, self.next, self.key, self.value = prev, next, key, value
 
+class LRUCache:
     def __init__(self, capacity: int):
-        self.cache = collections.OrderedDict()
+        self.head, self.tail = DLListNode(), DLListNode()
+        self.head.next, self.tail.prev = self.tail, self.head
         self.remain = capacity
-        
+        self.keys = {}
+
+    def add(self, key: int, value: int) -> DLListNode:
+        self.tail.key, self.tail.value = key, value
+        self.tail.next = DLListNode(prev = self.tail)
+        node = self.tail
+        self.tail = self.tail.next
+        self.keys[key] = node
+        self.remain -= 1
+        return node
+
+    def remove(self, node: DLListNode) -> None:
+        prev, next = node.prev, node.next
+        prev.next = next
+        next.prev = prev
+        self.remain += 1
+        self.keys.pop(node.key)
 
     def get(self, key: int) -> int:
-        if key not in self.cache: return -1
-        value = self.cache[key]
-        self.cache.pop(key)
-        self.remain += 1
-        self.put(key, value)
-        return value
+        if key not in self.keys: return -1
+        cur_node = self.keys[key]
+        self.put(key, cur_node.value)
+        return cur_node.value
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.pop(key)
-            self.remain += 1
+        if key in self.keys:
+            cur_node = self.keys[key]
+            self.remove(cur_node)
         elif not self.remain:
-            self.cache.popitem(last=False)
-            self.remain += 1
-        self.cache[key] = value
-        self.remain -= 1
+            self.remove(self.head.next)
+        new_node = self.add(key, value)
 
         
 
