32d8470effe9b5d510be5163235739b9dee5204b
Long Bian <bianlong0618@qq.com>
2024-09-10 10:22:59 -0400

    Time: 77 ms (45.94%), Space: 16.7 MB (89.78%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -2,16 +2,19 @@ class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         def dfsHelper(node):
             while graph[node]:
-                neighbor = graph[node].pop(0)
-                dfsHelper(neighbor)
-            ans.append(node)
+                dfsHelper(graph[node].pop(0))
+            self.ans.append(node)
+
 
         # Build the graph
         graph, tickets = collections.defaultdict(list), sorted(tickets)
         for u, v in tickets:
             graph[u].append(v)
-
-        ans = []
+        
+        self.ans = []
         dfsHelper("JFK")
-        return ans[::-1]
+        return self.ans[::-1]
+
+
+
         
