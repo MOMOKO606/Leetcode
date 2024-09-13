7f43a48df063969b1cb9e09107e0694210f1c5cb
Long Bian <bianlong0618@qq.com>
2024-09-13 17:02:30 -0400

    Time: 708 ms (37.72%), Space: 39.6 MB (6.16%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,11 +1,23 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numSet, ans = set(nums), 0
+        def parent(i):
+            root = i
+            while p[root] != root:
+                root = p[root]
+            while p[i] != i:
+                p[i], i = root, p[i]
+            return root
+
+        def connect(i, j):
+            pi, pj = parent(i), parent(j)
+            p[pi] = pj
+
+        p, numSet, roots = {num: num for num in nums}, set(nums), {}
         for num in nums:
-            if num - 1 in numSet: continue
-            end = num
-            while end + 1 in numSet:
-                end += 1
-            ans = max(ans, end - num + 1)
-        return ans
+            if num - 1 in numSet: connect(num, num - 1)
+            if num + 1 in numSet: connect(num, num + 1)
+        for num in p:
+            pnum = parent(num)
+            roots[pnum] = roots.get(pnum, 0) + 1
+        return max(roots.values()) if roots else 0
         
