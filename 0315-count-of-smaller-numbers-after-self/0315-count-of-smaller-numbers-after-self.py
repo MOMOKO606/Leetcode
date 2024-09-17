a2f0e4deb972ac7e6166dd2150a67ef1e2de3635
Long Bian <bianlong0618@qq.com>
2024-09-17 15:34:12 -0400

    Time: 1352 ms (78.12%), Space: 36.1 MB (70.44%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,27 +1,31 @@
-class Solution:
-    def countSmaller(self, nums: List[int]) -> List[int]:
-        def merge(left, right):
-            sorted_nums = []
-            while left and right:
-                if left[0][0] > right[0][0]:
-                    node = left.pop(0)
-                    sorted_nums.append(node)
-                    ans[node[1]] += len(right)
-                else:
-                    node = right.pop(0)
-                    sorted_nums.append(node)
-            return sorted_nums + (left or right)
+class FenwickTree:
+    def __init__(self, size, original_value):
+        self.size = size
+        self.tree = [original_value] * size
 
+    def __lowbit(self, index):
+        return index & -index
 
-        def mergeSort(nums):
-            if len(nums) == 1: return nums
-            mid = len(nums) // 2
-            left, right = mergeSort(nums[:mid]), mergeSort(nums[mid:])
-            return merge(left, right)
+    def update(self, index, delta):
+        while index < self.size:
+            self.tree[index] += delta
+            index += self.__lowbit(index)
 
-        ans = [0] * len(nums)
-        mergeSort([(num, i) for i, num in enumerate(nums)])
+    def query(self, index):
+        ans = 0
+        while index:
+            ans += self.tree[index]
+            index -= self.__lowbit(index)
         return ans
 
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        transfer, ans = {num: i + 1 for i, num in enumerate(sorted(set(nums)))}, []
+        ft = FenwickTree(len(transfer) + 1, 0)
+        for num in reversed(nums):
+            ans.append(ft.query(transfer[num] - 1))
+            ft.update(transfer[num], 1)
+        return ans[::-1]
 
         
