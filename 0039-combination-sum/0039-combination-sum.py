37ef77172933481b35bbd08417c5585737fd8226
Long Bian <bianlong0618@qq.com>
2024-09-19 16:21:33 -0400

    Time: 57 ms (35.89%), Space: 16.7 MB (43.75%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,15 +1,15 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def helper(i=0, target=target, seq=[]):
+        def helper(i=0, seq=[], target=target):
             if not target: 
-                self.ans.append(seq[:])
+                self.ans.append(seq)
                 return
-            if target < 0 or i == len(candidates): return
+            if target < 0 or i == len(candidates):
+                return 
             for j in range(i, len(candidates)):
-                helper(j, target - candidates[j], seq + [candidates[j]])
+                helper(j, seq + [candidates[j]], target - candidates[j])
 
         self.ans = []
         helper()
         return self.ans
-        
         
