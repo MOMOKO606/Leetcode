4c080188fd6b013994b2d9697773e9619591e65b
Long Bian <bianlong0618@qq.com>
2024-09-19 14:14:47 -0400

    Time: 679 ms (45.84%), Space: 16.8 MB (69.17%) - LeetHub

--- 3254-number-of-divisible-triplet-sums/3254-number-of-divisible-triplet-sums.py
+++ 3254-number-of-divisible-triplet-sums/3254-number-of-divisible-triplet-sums.py
@@ -2,12 +2,12 @@ class Solution:
     def divisibleTripletCount(self, nums: List[int], d: int) -> int:
         ans = 0
         for i in range(len(nums) - 2):
-            seen = {}
+            reminders = collections.defaultdict(int)
             for k in range(i + 1, len(nums)):
-                if -nums[k] % d in seen:
-                    ans += seen[-nums[k] % d]
-                key = (nums[i] + nums[k]) % d
-                seen[key] = seen.get(key, 0) + 1
+                target = -nums[k] % d
+                if target in reminders:
+                    ans += reminders[target]
+                reminders[(nums[i] + nums[k]) % d] += 1
         return ans
 
         
