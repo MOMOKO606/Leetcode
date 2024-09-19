4bc6f3d3c2f88dfac4dc6567a58fd7b3caf3a2d3
Long Bian <bianlong0618@qq.com>
2024-09-19 14:27:45 -0400

    Time: 46 ms (11.11%), Space: 16.5 MB (51.02%) - LeetHub

--- 0065-valid-number/0065-valid-number.py
+++ 0065-valid-number/0065-valid-number.py
@@ -1,18 +1,19 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
-        hasDec, hasSign, hasDigit, hasEe = False, False, False, False
+        hasDigit, hasEe, hasDec, hasSign = False, False, False, False
         for char in s:
             if char in "0123456789":
                 hasDigit = True
-            elif char == ".":
-                if hasDec or hasEe: return False
-                hasDec = True
             elif char in "+-":
-                if hasSign or hasDec or hasDigit: return False
+                if hasSign or hasDigit or hasDec: return False
                 hasSign = True
+            elif char == ".":
+                if hasEe or hasDec: return False
+                hasDec = True
             elif char in "Ee":
                 if hasEe or not hasDigit: return False
-                hasEe, hasSign, hasDigit, hasDec = True, False, False, False
-            else: return False
+                hasEe, hasDigit, hasDec, hasSign = True, False, False, False
+            else: 
+                return False
         return hasDigit
         
