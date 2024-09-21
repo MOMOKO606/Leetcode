aa9e01629c7c1047ca87975da58054f140ce24e9
Long Bian <bianlong0618@qq.com>
2024-09-21 10:22:41 -0400

    Time: 50 ms (73.46%), Space: 17.2 MB (28.89%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -7,10 +7,14 @@ class Solution:
             mid = (low + high) // 2
             if nums[mid] == target: return True
             if nums[low] <= nums[mid]:
-                if nums[low] <= target < nums[mid]: high = mid - 1
-                else: low = mid + 1
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
             else:
-                if nums[mid] < target <= nums[high]: low = mid + 1
-                else: high = mid - 1
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
         return False
         
