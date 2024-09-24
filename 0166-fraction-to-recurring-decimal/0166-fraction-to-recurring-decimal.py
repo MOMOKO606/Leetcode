aaf5714a4702ee3be503bc6db01c466e46371a29
Long Bian <bianlong0618@qq.com>
2024-09-23 22:35:39 -0400

    Time: 36 ms (50.75%), Space: 16.8 MB (20.14%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.py
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.py
@@ -1,18 +1,19 @@
 class Solution:
     def fractionToDecimal(self, numerator: int, denominator: int) -> str:
         if not numerator % denominator: return str(numerator // denominator)
-        negative = "" if numerator * denominator >= 0 else "-"
+        negative = "" if numerator * denominator > 0 else "-"
         numerator, denominator = abs(numerator), abs(denominator)
-        ans, seen = negative + str(numerator // denominator) + ".", {}
+        ans, visited = negative + str(numerator // denominator) + ".", {}
         numerator %= denominator
         while numerator:
             numerator *= 10
-            if numerator in seen: 
-                i = seen[numerator] 
+            if numerator in visited: 
+                i = visited[numerator]
                 return ans[:i] + "(" + ans[i:] + ")"
-            seen[numerator] = len(ans)
-            ans += str(numerator // denominator)
-            numerator %= denominator
+            visited[numerator] = len(ans)
+            factor = numerator // denominator
+            ans += str(factor)
+            numerator -= factor * denominator
         return ans
 
         
