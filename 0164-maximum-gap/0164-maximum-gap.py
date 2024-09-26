28cfa5932b5a7f072fe2992bfa9d236dc17c7a0a
Long Bian <bianlong0618@qq.com>
2024-09-26 17:31:05 -0400

    Time: 873 ms (34.25%), Space: 34.7 MB (16.64%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -1,14 +1,13 @@
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
-        max_num, min_num = max(nums), min(nums)
-        if max_num == min_num: return 0
-        l = math.ceil((max_num - min_num) / (len(nums) - 1))
-        intervals = [[None, None] for _ in range((max_num - min_num) // l + 1)]
+        largest, smallest = max(nums), min(nums)
+        if largest == smallest: return 0
+        l = math.ceil((largest - smallest) / (len(nums) - 1))
+        buckets = [[None, None] for _ in range((largest - smallest) // l + 1)]
         for num in nums:
-            interval = intervals[(num - min_num) // l]
-            interval[0] = min(interval[0], num) if interval[0] else num
-            interval[1] = max(interval[1], num) if interval[1] else num
-        intervals = [interval for interval in intervals if interval[0]]
-        return max([intervals[i][0] - intervals[i - 1][1] for i in range(1, len(intervals))])
-
+            bucket = buckets[(num - smallest) // l]
+            bucket[0] = min(bucket[0], num) if bucket[0] else num
+            bucket[1] = max(bucket[1], num) if bucket[1] else num
+        buckets = [bucket for bucket in buckets if bucket[0]]
+        return max([buckets[i][0] - buckets[i - 1][1] for i in range(1, len(buckets))])
         
