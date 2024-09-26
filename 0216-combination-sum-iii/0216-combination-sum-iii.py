d114d3e387c07c18844480a8109c54015b222b7a
Long Bian <bianlong0618@qq.com>
2024-09-26 15:34:07 -0400

    Time: 37 ms (48.83%), Space: 16.6 MB (35.35%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -2,13 +2,13 @@ class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         def helper(cur=1, k=k, n=n, seq=[]):
             if not n and not k:
-                self.ans.append(seq[:])
+                ans.append(seq[:])
                 return
             if not n or not k: return
             for j in range(cur, 10):
                 helper(j + 1, k - 1, n - j, seq + [j])
 
-        self.ans = []
+        ans = []
         helper()
-        return self.ans
+        return ans
         
