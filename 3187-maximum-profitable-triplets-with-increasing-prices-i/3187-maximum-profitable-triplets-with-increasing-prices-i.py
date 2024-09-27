8a5135351280e09b962cac2a980f30866a69981f
Long Bian <bianlong0618@qq.com>
2024-09-27 17:04:04 -0400

    Time: 4880 ms (35.48%), Space: 17.1 MB (62.37%) - LeetHub

--- 3187-maximum-profitable-triplets-with-increasing-prices-i/3187-maximum-profitable-triplets-with-increasing-prices-i.py
+++ 3187-maximum-profitable-triplets-with-increasing-prices-i/3187-maximum-profitable-triplets-with-increasing-prices-i.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int], profits: List[int]) -> int:
-        ans, pre_max, post_max = -1, [-inf] * len(prices), [-inf] * len(prices)
+        preMax, postMax, ans = [-inf] * len(prices), [-inf] * len(prices), -inf
         for i in range(len(prices) - 1):
             for j in range(i + 1, len(prices)):
                 if prices[i] < prices[j]:
-                    pre_max[j] = max(pre_max[j], profits[i])
-                    post_max[i] = max(post_max[i], profits[j])
-        for i in range(len(prices)):
-            ans = max(ans, pre_max[i] + post_max[i] + profits[i])
-        return ans
+                    preMax[j] = max(preMax[j], profits[i])
+                    postMax[i] = max(postMax[i], profits[j])
+        for i in range(len(prices)): 
+            ans = max(ans, profits[i] + preMax[i] + postMax[i])
+        return ans if ans != -inf else -1
+
+
+
 
         
