8be4655118d33950aa8e2509c9584b3622177115
Long Bian <bianlong0618@qq.com>
2024-09-26 22:05:11 -0400

    Time: 218 ms (71.72%), Space: 24.2 MB (77.08%) - LeetHub

--- 1368-web-crawler-multithreaded/1368-web-crawler-multithreaded.py
+++ 1368-web-crawler-multithreaded/1368-web-crawler-multithreaded.py
@@ -17,10 +17,9 @@ class Solution:
             nextQueue = []
             for newUrl in htmlParser.getUrls(url):
                 if newUrl.split("http://")[1].split("/")[0] == domain and newUrl not in visited:
-                    nextQueue.append(newUrl)
                     visited.add(newUrl)
+                    nextQueue.append(newUrl)
             return nextQueue
-
 
         curQueue, domain, visited = queue.Queue(), startUrl.split("http://")[1].split("/")[0], set([startUrl])
         curQueue.put(startUrl)
@@ -29,11 +28,11 @@ class Solution:
                 futures = []
                 while not curQueue.empty():
                     futures.append(executor.submit(_crawl, curQueue.get()))
-                
+
                 for future in futures:
                     for url in future.result():
                         curQueue.put(url)
         return list(visited)
-            
+
 
         
