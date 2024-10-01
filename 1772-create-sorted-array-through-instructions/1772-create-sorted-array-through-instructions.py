d36b4fcf6ceef7357fec0b22ed8f3c6c1431f734
Long Bian <bianlong0618@qq.com>
2024-09-30 23:25:39 -0400

    Time: 3725 ms (22.9%), Space: 39.8 MB (15.65%) - LeetHub

--- 1772-create-sorted-array-through-instructions/1772-create-sorted-array-through-instructions.py
+++ 1772-create-sorted-array-through-instructions/1772-create-sorted-array-through-instructions.py
@@ -1,35 +1,34 @@
 class FenwickTree:
     def __init__(self, size, value):
-        self.size = size
         self.tree = [value] * size
+        self.size = size
 
-    def _lowbit(self, index):
+    def __lowbit(self, index):
         return index & -index
 
     def update(self, index, delta):
         while index < self.size:
             self.tree[index] += delta
-            index += self._lowbit(index)
-
+            index += self.__lowbit(index)
+    
     def query(self, index):
         ans = 0
         while index:
             ans += self.tree[index]
-            index -= self._lowbit(index)
+            index -= self.__lowbit(index)
         return ans
 
 
 class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
         transfer = {num: i + 1 for i, num in enumerate(sorted(set(instructions)))}
-        fw = FenwickTree(len(transfer) + 1, 0)
-        ans = 0
+        ft, ans = FenwickTree(len(transfer) + 1, 0), 0
         for i, num in enumerate(instructions):
-            index = transfer[num]
-            left = fw.query(index - 1)
-            fw.update(index, 1)
-            right = i - fw.query(index) + 1
-            ans += min(left, right)
+            k = transfer[num]
+            left, right = ft.query(k - 1), i - ft.query(k)
+            ans += min(ft.query(k - 1), i - ft.query(k))
+            ft.update(k, 1)
         return ans % (10 ** 9 + 7)
+
 
         
