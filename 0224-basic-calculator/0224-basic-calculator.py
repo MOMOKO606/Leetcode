fb63f6a4cd993cf9fddce722864c9db12968972d
Long Bian <bianlong0618@qq.com>
2024-10-01 20:37:00 -0400

    Time: 79 ms (36.68%), Space: 74.5 MB (5.02%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        def update(curOper, curNum):
+            if curOper == "+":
+                stack.append(curNum)
+            elif curOper == "-":
+                stack.append(-curNum)
+
+
+        stack, curOper, curNum, i = [], "+", 0, 0
+        while i < len(s):
+            if s[i].isdigit():
+                curNum = curNum * 10 + int(s[i])
+            elif s[i] in "+-":
+                update(curOper, curNum)
+                curOper, curNum = s[i], 0
+            elif s[i] == "(":
+                curNum, j = self.calculate(s[i + 1:])
+                i += j
+            elif s[i] == ")":
+                update(curOper, curNum)
+                return sum(stack), i + 1
+            i += 1
+        update(curOper, curNum)
+        return sum(stack)
+
+        
