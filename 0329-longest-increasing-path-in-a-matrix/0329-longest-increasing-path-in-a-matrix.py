16ff27ed028870036249f765a306f18f5396695b
Long Bian <bianlong0618@qq.com>
2024-10-03 17:18:58 -0400

    Time: 308 ms (65.34%), Space: 21.2 MB (28.42%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,17 +1,19 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         @cache
-        def helper(i, j):
-            ans = 0
-            for x, y in [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]:
-                if not (0 <= x < rows and 0 <= y < cols) or matrix[x][y] >= matrix[i][j]: continue
-                ans = max(ans, helper(x, y))
-            return ans + 1
+        def dfsHelper(i, j):
+            ans = 1
+            for x, y in [[i + 1, j], [i - 1, j], [i, j - 1], [i, j + 1]]:
+                if 0 <= x < rows and 0 <= y < cols and matrix[x][y] < matrix[i][j]:
+                    ans = max(ans, dfsHelper(x, y) + 1)
+            return ans
 
-        
+
         rows, cols, ans = len(matrix), len(matrix[0]), 1
         for i in range(rows):
             for j in range(cols):
-                ans = max(ans, helper(i, j))
+                ans = max(ans, dfsHelper(i, j))
         return ans
+
+
         
