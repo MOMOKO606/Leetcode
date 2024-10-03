58e54800383a37896d01170e6514844aed1f9a7c
Long Bian <bianlong0618@qq.com>
2024-10-03 11:09:02 -0400

    Time: 228 ms (85.85%), Space: 19 MB (63.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,16 +1,11 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def helper(i, j):
-            queue = [[i, j]]
-            while queue:
-                next_queue = []
-                for i, j in queue:
-                    for x, y in [[i + 1, j], [i - 1, j], [i, j - 1], [i, j + 1]]:
-                        if not (0 <= x < rows and 0 <= y < cols and grid[x][y] == "1"): continue
-                        
-                        next_queue.append([x, y])
-                        grid[x][y] = "0"
-                queue = next_queue
+            if grid[i][j] == "0": return
+            grid[i][j] = "0"
+            for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
+                if not (0 <= x < rows and 0 <= y < cols and grid[x][y] == "1"): continue
+                helper(x, y)
                 
 
         rows, cols, ans = len(grid), len(grid[0]), 0
