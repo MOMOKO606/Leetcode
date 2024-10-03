5c0cf99c0c1e2193d81c97a83ee47b08dd86ccd2
Long Bian <bianlong0618@qq.com>
2024-10-03 18:45:48 -0400

    Time: 56 ms (94.05%), Space: 16.9 MB (6.45%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -3,37 +3,41 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        def helper(iter=0):
-            if iter == len(remain):
-                return True
-            i, j = remain[iter]
-            k = 3 * (i // 3) + j // 3
+        def dfsHelper(iter=0):
+            if iter == len(remains): return True
+            i, j = remains[iter]
+            k = (i // 3) * 3 + j // 3
             for num in rows[i] & cols[j] & blocks[k]:
                 board[i][j] = str(num)
                 rows[i].remove(num)
                 cols[j].remove(num)
                 blocks[k].remove(num)
-                if helper(iter + 1): return True
+                if dfsHelper(iter + 1):
+                    return True
                 board[i][j] = "."
                 rows[i].add(num)
                 cols[j].add(num)
                 blocks[k].add(num)
 
+
         rows = [set([i for i in range(1, 10)]) for _ in range(9)]
         cols = [set([i for i in range(1, 10)]) for _ in range(9)]
         blocks = [set([i for i in range(1, 10)]) for _ in range(9)]
-        remain, iter = [], 0
-        for i in range(len(board)):
-            for j in range(len(board[0])):
+        remains, iter = [], 0
+        for i in range(9):
+            for j in range(9):
                 if board[i][j] == ".": 
-                    remain.append([i, j])
+                    remains.append([i, j])
                 else:
-                    k, num = 3 * (i // 3) + j // 3, int(board[i][j])
+                    num, k = int(board[i][j]), (i // 3) * 3 + j // 3
                     rows[i].remove(num)
                     cols[j].remove(num)
                     blocks[k].remove(num)
-        helper()
-        return
+        dfsHelper()
 
         
+                
+
+                
+
         
