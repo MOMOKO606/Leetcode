fc6817696848a87096dc750d8b96167d7bc8f133
Long Bian <bianlong0618@qq.com>
2024-10-03 11:34:22 -0400

    Time: 106 ms (98.3%), Space: 18 MB (39.64%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -6,22 +6,24 @@ class Solution:
         def helper(i, j):
             if not (0 <= i < rows and 0 <= j < cols and board[i][j] == "O"): return
             board[i][j] = "$"
-            for x, y in [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]:
+            for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
                 helper(x, y)
 
 
         rows, cols = len(board), len(board[0])
         for i in [0, rows - 1]:
             for j in range(cols):
-                if board[i][j] == "O": helper(i, j)
+                helper(i, j)
 
         for j in [0, cols - 1]:
             for i in range(rows):
-                if board[i][j] == "O": helper(i, j)
+                helper(i, j)
 
         for i in range(rows):
             for j in range(cols):
-                if board[i][j] == "$":
-                    board[i][j] = "O"
-                elif board[i][j] == "O":
+                if board[i][j] == "O":
                     board[i][j] = "X"
+                elif board[i][j] == "$":
+                    board[i][j] = "O"
+
+        
