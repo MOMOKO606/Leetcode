7fd1f39d3ed7e6294327f6a270df349704e8645a
Long Bian <bianlong0618@qq.com>
2024-10-03 17:49:07 -0400

    Time: 101 ms (12.45%), Space: 16.6 MB (56.47%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,18 +1,16 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set([i for i in range(10)]) for _ in range(9)]
-        cols = [set([i for i in range(10)]) for _ in range(9)]
-        blocks = [set([i for i in range(10)]) for _ in range(9)]
+        rows = [set([i for i in range(1, 10)]) for _ in range(9)]
+        cols = [set([i for i in range(1, 10)]) for _ in range(9)]
+        blocks = [set([i for i in range(1, 10)]) for _ in range(9)]
         for i in range(len(board)):
-            for j in range(len(board[0])):
+            for j in range(len(board)):
                 if board[i][j] == ".": continue
-                num, k = int(board[i][j]), 3 * (i // 3) + j // 3
-                if not num in rows[i] & cols[j] & blocks[k]: return False
-                rows[i].remove(num)
-                cols[j].remove(num)
-                blocks[k].remove(num)
+                num, k = int(board[i][j]), (i // 3) * 3 + j // 3
+                if num in rows[i] and num in cols[j] and num in blocks[k]:
+                    rows[i].remove(num)
+                    cols[j].remove(num)
+                    blocks[k].remove(num)
+                else: return False
         return True
-                
-
-
         
