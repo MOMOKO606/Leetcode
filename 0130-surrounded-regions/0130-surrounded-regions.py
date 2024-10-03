d93b62922a546283facd8bfa6ff30b2f79256762
Long Bian <bianlong0618@qq.com>
2024-10-03 11:40:49 -0400

    Time: 114 ms (85.08%), Space: 17.9 MB (83.83%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -4,19 +4,26 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         def helper(i, j):
-            if not (0 <= i < rows and 0 <= j < cols and board[i][j] == "O"): return
-            board[i][j] = "$"
-            for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
-                helper(x, y)
+            deque = collections.deque([[i, j]])
+            while deque:
+                i, j = deque.popleft()
+                for x, y in [[i + 1, j], [i - 1, j], [i, j - 1], [i, j + 1]]:
+                    if not (0 <= x < rows and 0 <= y < cols and board[x][y] == "O"): continue
+                    board[x][y] = "$"
+                    deque.append([x, y])
 
 
         rows, cols = len(board), len(board[0])
         for i in [0, rows - 1]:
             for j in range(cols):
+                if board[i][j] == "X": continue
+                board[i][j] = "$"
                 helper(i, j)
 
         for j in [0, cols - 1]:
             for i in range(rows):
+                if board[i][j] == "X": continue
+                board[i][j] = "$"
                 helper(i, j)
 
         for i in range(rows):
