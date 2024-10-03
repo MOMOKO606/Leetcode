c4f4bf82a490d5d58ef4f53f193dddd30fc7be45
Long Bian <bianlong0618@qq.com>
2024-10-03 12:00:45 -0400

    Time: 189 ms (35.75%), Space: 17.4 MB (50.94%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def parent(i):
+            root = i
+            while root != p[root]:
+                root = p[root]
+            while i != p[i]:
+                p[i], i = root, p[i]
+            return root
+
+        def connect(i, j):
+            pi, pj = parent(i), parent(j)
+            p[pi] = pj
+        
+        n = len(isConnected)
+        p = [i for i in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if not isConnected[i][j]: continue
+                connect(i, j)
+        return len(set([parent(i) for i in range(n)]))
+        
