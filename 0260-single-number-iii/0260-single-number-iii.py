fc12e38f4ef220aabf1c3ef8d93068fd0ef98229
Long Bian <bianlong0618@qq.com>
2024-10-02 21:14:01 -0400

    Time: 57 ms (63.52%), Space: 18.5 MB (49.7%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,18 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         xor, first, second = 0, 0, 0
-        for num in nums:
-            xor ^= num
-        
-        target = 1
-        while not target & xor:
-            target <<= 1
+        for num in nums: xor ^= num
 
+        xor &= -xor
         for num in nums:
-            if num & target:
-                first ^= num
-            else:
-                second ^= num
+            if num & xor: first ^= num
+            else: second ^= num
         return [first, second]
+
 
         
