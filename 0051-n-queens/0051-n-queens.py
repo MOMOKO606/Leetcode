0d2f4254c105afdf7dff2539aaf45fe60bf9ae20
Long Bian <bianlong0618@qq.com>
2024-10-04 00:00:13 -0400

    Time: 50 ms (70.32%), Space: 17.2 MB (19.16%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,23 +1,21 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def helper(i=0, seq=[]):
-            if i == n:
+        def helper(i=0):
+            if i == n: 
                 ans.append(seq[:])
-                return
+                return 
             for j in range(n):
-                if j in cols or i - j in slash or i + j in back_slash: continue
+                if j in cols or i + j in slashes or i - j in back_slashes: continue
                 cols.add(j)
-                slash.add(i - j)
-                back_slash.add(i + j)
-                helper(i + 1, seq + [j])
+                slashes.add(i + j)
+                back_slashes.add(i - j)
+                seq.append(j)
+                helper(i + 1)
+                seq.pop()
                 cols.remove(j)
-                slash.remove(i - j)
-                back_slash.remove(i + j)
+                slashes.remove(i + j)
+                back_slashes.remove(i - j)
 
-
-        cols, slash, back_slash, ans = set(), set(), set(), []
+        cols, slashes, back_slashes, seq, ans = set(), set(), set(), [], []
         helper()
-        return [["." * j + "Q" + "." * (n - j - 1) for j in seq] for seq in ans]
-
-
-        
+        return [["." * pos + "Q" + "." * (n - pos - 1) for pos in seq] for seq in ans]
