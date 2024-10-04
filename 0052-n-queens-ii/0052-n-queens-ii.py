b8a3b7a6b9fda4630d739928894ff25dd208fe93
Long Bian <bianlong0618@qq.com>
2024-10-03 20:13:35 -0400

    Time: 49 ms (51.8%), Space: 16.4 MB (81.83%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -4,15 +4,17 @@ class Solution:
             if i == n: return 1
             ans = 0
             for j in range(n):
-                if j in cols or i - j in slash or i + j in back_slash: continue
+                if j in cols or i + j in back_slashes or i - j in slashes: continue
                 cols.add(j)
-                slash.add(i - j)
-                back_slash.add(i + j)
+                back_slashes.add(i + j)
+                slashes.add(i - j)
                 ans += helper(i + 1)
                 cols.remove(j)
-                slash.remove(i - j)
-                back_slash.remove(i + j)
+                back_slashes.remove(i + j)
+                slashes.remove(i - j)
             return ans
-        cols, slash, back_slash = set(), set(), set()
+
+        cols, slashes, back_slashes  = set(), set(), set()
         return helper()
+        
         
