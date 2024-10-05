05dde5f0eeec7c2e554d43213743756463de7be2
Long Bian <bianlong0618@qq.com>
2024-10-04 20:35:25 -0400

    Time: 2052 ms (59.84%), Space: 96.8 MB (61.48%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.py
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.py
@@ -2,31 +2,29 @@ class Solution:
     def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
         def parent(i):
             root = i
-            while p[root] != root:
+            while root != p[root]:
                 root = p[root]
-            while p[i] != i:
+            while i != p[i]:
                 p[i], i = root, p[i]
             return root
-
+        
         def connect(i, j):
             pi, pj = parent(i), parent(j)
             p[pi] = pj
+        
+        p, ans = [i for i in range(n)], []
+        for u, v, _ in edges:
+            connect(u, v)
+            
+        root2weight = {parent(i): -1 for i in range(n)}
+        for u, v, w in edges:
+            root2weight[parent(u)] &= w
 
-        p = [i for i in range(n)]
-        for i, j, _ in edges:
-            connect(i, j)
+        for u, v in query:
+            pu, pv = parent(u), parent(v)
+            if pu == pv: ans.append(root2weight[pu])
+            else: ans.append(-1)
 
-        roots = {parent(i): -1 for i in range(n)}
-        for i, j, w in edges:
-            pi = parent(i)
-            roots[pi] = roots[pi] & w
-
-        ans = []
-        for i, j in query:
-            pi, pj = parent(i), parent(j)
-            if pi != pj: ans.append(-1)
-            else: ans.append(roots[pi])
         return ans
-
-
+        
         
