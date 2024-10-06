c1bea66057ab78eb8278eb594eceadd6c5f1464a
Long Bian <bianlong0618@qq.com>
2024-10-06 14:09:04 -0400

    Time: 56 ms (68.93%), Space: 28 MB (65.57%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,10 +1,8 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        visited, ans = set(), set() 
-        for i in range(len(s) - 9):
-            seq = s[i: i + 10]
-            if seq in visited: ans.add(seq)
-            else: visited.add(seq)
-        return list(ans)
-
+        visited = {}
+        for j in range(len(s) - 9):
+            target = s[j: j + 10]
+            visited[target] = visited.get(target, 0) + 1
+        return [key for key, value in visited.items() if value > 1]
         
