46f74f21b20051cec45d099d03798c8302f840f1
Long Bian <bianlong0618@qq.com>
2024-10-06 11:19:28 -0400

    Time: 1367 ms (79.25%), Space: 36.2 MB (70.91%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,10 +1,34 @@
+class FenwickTree:
+    def __init__(self, size, value):
+        self.size = size
+        self.tree = [value] * size
+
+    def __lowbit(self, index):
+        return index & -index
+
+    def update(self, index, delta):
+        while index < self.size:
+            self.tree[index] += delta
+            index += self.__lowbit(index)
+    
+    def query(self, index):
+        ans = 0
+        while index:
+            ans += self.tree[index]
+            index -= self.__lowbit(index)
+        return ans
+
+
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        sorted_nums, ans = [], []
+        transfer, ans = {num: i + 1 for i, num in enumerate(sorted(set(nums)))}, []
+        ft = FenwickTree(len(transfer) + 1, 0)
         for num in reversed(nums):
-            k = bisect.bisect_left(sorted_nums, num)
-            bisect.insort(sorted_nums, num)
-            ans.append(k)
+            k = transfer[num]
+            ft.update(k, 1)
+            ans.append(ft.query(k - 1))
         return ans[::-1]
+
+
 
         
