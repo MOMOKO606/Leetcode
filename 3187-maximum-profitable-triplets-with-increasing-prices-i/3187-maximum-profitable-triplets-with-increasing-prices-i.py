172e46d32531e72420ba2733d519437955b28d5e
Long Bian <bianlong0618@qq.com>
2024-10-06 12:13:45 -0400

    Time: 4616 ms (41.11%), Space: 16.7 MB (97.78%) - LeetHub

--- 3187-maximum-profitable-triplets-with-increasing-prices-i/3187-maximum-profitable-triplets-with-increasing-prices-i.py
+++ 3187-maximum-profitable-triplets-with-increasing-prices-i/3187-maximum-profitable-triplets-with-increasing-prices-i.py
@@ -1,35 +1,14 @@
-class FenwickTree:
-    def __init__(self, size, value):
-        self.tree = [value] * size
-        self.size = size
-
-    def __lowbit(self, index):
-        return index & -index
-
-    def update(self, index, value):
-        while index < self.size:
-            self.tree[index] = max(self.tree[index], value)
-            index += self.__lowbit(index)
-
-    def query(self, index):
-        ans = 0
-        while index:
-            ans = max(ans, self.tree[index])
-            index -= self.__lowbit(index)
-        return ans
-
-
 class Solution:
     def maxProfit(self, prices: List[int], profits: List[int]) -> int:
-        largest = max(prices)
-        single, double, ans = FenwickTree(largest + 1, 0), FenwickTree(2 * largest + 1, 0), -1
-        for price, profit in zip(prices, profits):
-            single.update(price, profit)
-            single_max = single.query(price - 1)
-            if single_max: 
-                double.update(price, single_max + profit)
-            double_max = double.query(price - 1)
-            if double_max: ans = max(ans, double_max + profit)
-        return ans
+        n = len(prices)
+        pre_max, post_max, ans = [-inf] * n, [-inf] * n, -1 
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if prices[i] < prices[j]:
+                    post_max[i] = max(post_max[i], profits[j])
+                    pre_max[j] = max(pre_max[j], profits[i])
 
+        for i in range(n):
+            ans = max(ans, pre_max[i] + profits[i] + post_max[i])
+        return ans 
         
