4bd439afc9d3a3c45908a3b95bb84b71bc1ba178
Long Bian <bianlong0618@qq.com>
2024-10-08 16:23:08 -0400

    Time: 116 ms (46.45%), Space: 16.6 MB (36.06%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         minHeap, visited = [1], set([1])
-        for _ in range(n):
-            node = heapq.heappop(minHeap)
+        for _ in range(n - 1):
+            num = heapq.heappop(minHeap)
             for factor in [2, 3, 5]:
-                new_node = node * factor 
-                if new_node not in visited:
-                    visited.add(new_node)
-                    heapq.heappush(minHeap, new_node)
-        return node
+                newNum = factor * num
+                if newNum not in visited:
+                    heapq.heappush(minHeap, newNum)
+                    visited.add(newNum)
+        return minHeap[0]
 
         
