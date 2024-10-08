5cb1c6c68c4de7d343b3c1e229865194bbc21d74
Long Bian <bianlong0618@qq.com>
2024-10-08 19:59:21 -0400

    Time: 4085 ms (56.57%), Space: 16.8 MB (7.73%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,19 +1,28 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfsHelper(i, j, k):
-            if k == len(word): return True
-            if not (0 <= i < rows and 0 <= j < cols) or board[i][j] != word[k]: return False
+        def dfsHelper(i, j, curNode):
+            if "$" in curNode: return True
+            if not (0 <= i < rows and 0 <= j < cols and board[i][j] in curNode): return False
+            curNode = curNode[board[i][j]]
             ori = board[i][j]
-            board[i][j] = "$"
-            for di, dj in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-                if dfsHelper(i + di, j + dj, k + 1): return True
+            board[i][j] = "#"
+            for x, y in [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]:
+                if dfsHelper(x, y, curNode): return True
             board[i][j] = ori
-            return False 
+            return False
+        
+        # Build the trie
+        trie = node = {}
+        for char in word:
+            node[char] = node.get(char, {})
+            node = node[char]
+        node["$"] = "$"
 
         rows, cols = len(board), len(board[0])
         for i in range(rows):
             for j in range(cols):
-                if dfsHelper(i, j, 0): return True
+                if dfsHelper(i, j, trie): return True
         return False
 
+        
         
