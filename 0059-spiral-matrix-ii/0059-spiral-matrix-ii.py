86b3694321b6d6477a9ca278ce370993771ebc70
Long Bian <bianlong0618@qq.com>
2024-10-09 08:59:04 -0400

    Time: 37 ms (47.39%), Space: 16.5 MB (58.09%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        matrix, i, j, di, dj, cur = [[0] * n for _ in range(n)], 0, 0, 0, 1, 1
-        while cur <= n * n:
-            matrix[i][j] = cur
-            if not (0 <= i + di < n and 0 <= j + dj < n) or matrix[i + di][j + dj]:
+        N, cur, i, j, di, dj = n * n, 1, 0, 0, 0, 1
+        ans = [[0] * n for _ in range(n)]
+        while N:
+            ans[i][j] = cur
+            if not (0 <= i + di < n and 0 <= j + dj < n and not ans[i + di][j + dj]):
                 di, dj = dj, -di
-            i, j, cur = i + di, j + dj, cur + 1
-        return matrix
-
+            i, j, cur, N = i + di, j + dj, cur + 1, N - 1
+        return ans
         
