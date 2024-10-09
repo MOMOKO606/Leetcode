6032246e4176ebbefb1ed8b4abd91806cb39902e
Long Bian <bianlong0618@qq.com>
2024-10-08 22:34:48 -0400

    Time: 463 ms (61.13%), Space: 35.3 MB (13.32%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -2,14 +2,14 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         def helper(s, seq):
             if not s: 
-                self.ans.append(seq[:])
-                return 
+                ans.append(seq)
+                return
             for i in range(1, len(s) + 1):
                 if s[:i] == s[:i][::-1]:
                     helper(s[i:], seq + [s[:i]])
 
-        self.ans = []
-        helper(s, [])
-        return self.ans
 
+        ans = []
+        helper(s, [])
+        return ans
         
