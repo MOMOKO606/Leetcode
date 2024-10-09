708097029a7a9a310217d8fdc2d097e1620672ad
Long Bian <bianlong0618@qq.com>
2024-10-09 11:36:32 -0400

    Time: 1006 ms (63.96%), Space: 17.5 MB (56.82%) - LeetHub

--- 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.py
+++ 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.py
@@ -1,18 +1,19 @@
 class Solution:
     def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
         rows, cols, ans = len(matrix), len(matrix[0]), -inf
-        for bottom in range(rows):
+        for top in range(rows):
             compressed = [0] * cols
-            for i in range(bottom, rows):
-                preSums, preSum = [0], 0
+            for i in range(top, rows):
+                preSum, preSums = 0, [0]
                 for j in range(cols):
                     compressed[j] += matrix[i][j]
                     preSum += compressed[j]
                     index = bisect.bisect_left(preSums, preSum - k)
-                    if index < len(preSums): 
-                        if preSum - preSums[index] == k: return k
-                        ans = max(ans, preSum - preSums[index])
+                    if index < len(preSums): ans = max(ans, preSum - preSums[index])
                     bisect.insort(preSums, preSum)
         return ans
-                
 
+                    
+
+
+        
