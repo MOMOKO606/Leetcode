c5a5a7e7460b7064eba37be309c66eac79608390
Long Bian <bianlong0618@qq.com>
2024-10-09 18:27:07 -0400

    Time: 39 ms (42.6%), Space: 16.7 MB (72.93%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,7 +1,7 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        all_sets = [[]]
+        ans = [[]]
         for num in nums:
-            all_sets += [[num] + s for s in all_sets]
-        return all_sets
+            ans += [seq + [num] for seq in ans]
+        return ans
         
