649453c65e952827a0c58afb26ad7f5e74c10564
Long Bian <bianlong0618@qq.com>
2024-10-08 22:07:27 -0400

    Time: 905 ms (51.84%), Space: 34.3 MB (39.48%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
@@ -1,17 +1,13 @@
 class Solution:
     def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
-        buckets = {}
-        for i, num in enumerate(nums):
-            if i > indexDiff: 
-                j = nums[i - indexDiff - 1] // (valueDiff + 1)
-                del buckets[j]
-            k = nums[i] // (valueDiff + 1)
+        buckets, smallest, largest = {}, min(nums), max(nums)
+        for j, num in enumerate(nums):
+            if j > indexDiff:
+                i = j - indexDiff - 1
+                del buckets[(nums[i] - smallest) // (valueDiff + 1)]
+            k = (num - smallest) // (valueDiff + 1)
             if k in buckets: return True
-            if k - 1 in buckets and abs(num - buckets[k - 1]) <= valueDiff: return True 
-            if k + 1 in buckets and abs(num - buckets[k + 1]) <= valueDiff: return True 
             buckets[k] = num
+            if k - 1 in buckets and abs(buckets[k - 1] - num) <= valueDiff: return True
+            if k + 1 in buckets and abs(buckets[k + 1] - num) <= valueDiff: return True
         return False
-
-
-
-        
