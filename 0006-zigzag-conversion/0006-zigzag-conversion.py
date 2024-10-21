59b962961d8d764fe302d2b831bd4dabea306abc
Long Bian <bianlong0618@qq.com>
2024-10-21 18:39:05 -0400

    Time: 99 ms (11.73%), Space: 23.7 MB (5.48%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,20 +1,14 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1: return s
-        s, cur_dirc_i, cur_dirc_j, matrix, i, j, ans = list(s), 1, 0, [[None] * len(s) for _ in range(numRows)], 0, 0, ""
-        while s:
-            char = s.pop(0)
+        matrix = [[""] * len(s) for _ in range(numRows)]
+        i, j, di, dj = 0, 0, 1, 0
+        for char in s:
             matrix[i][j] = char
-            if i + cur_dirc_i == numRows:
-                cur_dirc_i, cur_dirc_j = -1, 1
-            elif i + cur_dirc_i == -1:
-                cur_dirc_i, cur_dirc_j = 1, 0
-            i += cur_dirc_i
-            j += cur_dirc_j
-        
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j]: ans += matrix[i][j]
-        return ans
-
+            if i + di == numRows:
+                di, dj = -1, 1
+            elif i + di == -1:
+                di, dj = 1, 0
+            i, j = i + di, j + dj
+        return "".join(["".join(matrix[i]) for i in range(numRows)])
         
