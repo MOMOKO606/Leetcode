039ac2bdbe1206c517a8210d0d194d0b5ad44512
Long Bian <bianlong0618@qq.com>
2024-10-21 17:38:43 -0400

    Time: 2 ms (98.27%), Space: 16.9 MB (68.59%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,23 +1,23 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) < len(nums2): s, l = nums1, nums2
+        if len(nums1) < len(nums2): s, l = nums1, nums2  
         else: s, l = nums2, nums1
-        
-        m, n, low, high = len(s), len(l), 0, len(s) - 1
+        m, n = len(s), len(l)
+        low, high = 0, m - 1
         while True:
-            s_mid = math.floor((low + high) / 2)
-            l_mid = math.ceil((m + n) / 2) - s_mid - 2
-            s_median = s[s_mid] if s_mid >= 0 else -inf
-            s_median_next = s[s_mid + 1] if s_mid + 1 < m else inf
-            l_median = l[l_mid] if l_mid >= 0 else -inf
-            l_median_next = l[l_mid + 1] if l_mid + 1 < n else inf
-            if s_median <= l_median_next and l_median <= s_median_next:
-                if (m + n) & 1: return max(s_median, l_median) 
-                else: return (max(s_median, l_median) + min(s_median_next, l_median_next)) / 2.0
-            elif s_median >l_median_next:
-                high = s_mid - 1
+            mid = (low + high) // 2
+            pivot = math.ceil((m + n) / 2) - mid - 2
+            short_mid = s[mid] if mid >= 0 else -inf
+            short_mid_next = s[mid + 1] if mid + 1 < m else inf
+            long_mid = l[pivot] if pivot >= 0 else -inf
+            long_mid_next = l[pivot + 1] if pivot + 1 < n else inf
+            if short_mid <= long_mid_next and long_mid <= short_mid_next:
+                if (m + n) & 1: return max(short_mid, long_mid)
+                else: return (max(short_mid, long_mid) + min(short_mid_next, long_mid_next)) / 2.0
+            elif short_mid > long_mid_next:
+                high = mid - 1
             else:
-                low = s_mid + 1
+                low = mid + 1
 
 
 
