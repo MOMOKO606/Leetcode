e545228a68a1d4d0b128dbfe3941bf27907c09fc
Long Bian <bianlong0618@qq.com>
2024-10-22 18:32:13 -0400

    Time: 0 ms (100%), Space: 16.7 MB (44.13%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,10 +1,9 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        shortest = min(strs, key=len)
-        rows, cols = len(strs), len(shortest)
+        rows, cols = len(strs), len(min(strs, key=len))
         for j in range(cols):
             for i in range(rows):
-                if i > 0 and strs[i][j] != strs[i - 1][j]: return strs[0][:j]
-        return shortest
-
+                if i > 0 and strs[i][j] != strs[i - 1][j]: 
+                    return strs[i][:j]
+        return strs[0][:cols]
         
