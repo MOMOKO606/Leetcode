e2b513123f32e2b62c8e1a20bc85a77619e20dd8
Long Bian <bianlong0618@qq.com>
2024-10-22 16:35:13 -0400

    Time: 7 ms (96.62%), Space: 16.6 MB (28.41%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -3,28 +3,28 @@ class Solution:
         if not num: return ""
         if num >= 1000:
             return "M" + self.intToRoman(num - 1000)
-        elif 1000 > num >= 900:
+        elif 900 <= num < 1000:
             return "CM" + self.intToRoman(num - 900)
-        elif 900 > num >= 500:
+        elif 500 <= num < 900:
             return "D" + self.intToRoman(num - 500)
-        elif 500 > num >= 400:
+        elif 400 <= num < 500:
             return "CD" + self.intToRoman(num - 400)
-        elif 400 > num >= 100:
+        elif 100 <= num < 400:
             return "C" + self.intToRoman(num - 100)
-        elif 100 > num >= 90:
+        elif 90 <= num < 100:
             return "XC" + self.intToRoman(num - 90)
-        elif 90 > num >= 50:
+        elif 50 <= num < 90:
             return "L" + self.intToRoman(num - 50)
-        elif 50 > num >= 40:
+        elif 40 <= num < 50:
             return "XL" + self.intToRoman(num - 40)
-        elif 40 > num >= 10:
+        elif 10 <= num < 40:
             return "X" + self.intToRoman(num - 10)
-        elif 10 > num >= 9:
+        elif 9 <= num < 10:
             return "IX" + self.intToRoman(num - 9)
-        elif 9 > num >= 5:
+        elif 5 <= num < 9:
             return "V" + self.intToRoman(num - 5)
-        elif 5 > num >= 4:
+        elif 4 <= num < 5:
             return "IV" + self.intToRoman(num - 4)
-        elif 4 > num >= 1:
+        elif 1 <= num < 4:
             return "I" + self.intToRoman(num - 1)
         
