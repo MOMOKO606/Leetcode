f53c9bf486124a54879b3bef6318d58a25eb084e
Long Bian <bianlong0618@qq.com>
2024-10-21 23:05:02 -0400

    Time: 4 ms (99.33%), Space: 18 MB (13.74%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -3,8 +3,10 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         if not p: return not s
         if not s: return p[-1] == "*" and self.isMatch(s, p[:-2])
-        if s[-1] == p[-1] or p[-1] == ".": return self.isMatch(s[:-1], p[:-1])
+        if s[-1] == p[-1] or p[-1] == ".":
+            return self.isMatch(s[:-1], p[:-1])
         if p[-1] == "*":
-            return self.isMatch(s, p[:-2]) or (self.isMatch(s[:-1], p) and (p[-2] == s[-1] or p[-2] == "."))
+            return self.isMatch(s, p[:-2]) or ((s[-1] == p[-2] or p[-2] == ".") and self.isMatch(s[:-1], p))
         return False
+
         
