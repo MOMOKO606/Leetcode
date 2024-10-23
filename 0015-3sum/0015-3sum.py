b6d20101694676f054ee93b2440ae33e73d5c84b
Long Bian <bianlong0618@qq.com>
2024-10-22 20:01:41 -0400

    Time: 572 ms (86.09%), Space: 19.8 MB (94.31%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,19 +5,19 @@ class Solution:
             if k > 0 and nums[k] == nums[k - 1]: continue
             i, j = k + 1, len(nums) - 1
             while i < j:
-                key = nums[i] + nums[j] + nums[k]
-                if key == 0: 
-                    ans.append([nums[i], nums[j], nums[k]])
+                pivot = nums[k] + nums[i] + nums[j]
+                if not pivot: 
+                    ans.append([nums[k], nums[i], nums[j]])
                     i += 1
-                    while i < len(nums) and nums[i] == nums[i - 1]:
+                    while i < j and nums[i] == nums[i - 1]:
                         i += 1
                     j -= 1
-                    while j > k and nums[j] == nums[j + 1]:
+                    while i < j and nums[j] == nums[j + 1]:
                         j -= 1
-                elif key < 0: i += 1
-                else: j -= 1
+                elif pivot > 0:
+                    j -= 1
+                else:
+                    i += 1
         return ans
+                
 
-
-
-        
