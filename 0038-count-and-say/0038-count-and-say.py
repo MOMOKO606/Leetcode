6614e203ce0414b23ce23c644765c92415e14265
Long Bian <bianlong0618@qq.com>
2024-10-24 13:34:25 -0400

    Time: 11 ms (92.75%), Space: 16.7 MB (45.59%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,17 +1,15 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        def rle(s):
-            ans, count = "", 1
-            for i in range(1, len(s)):
-                if s[i] != s[i - 1]:
-                    ans, count = ans + str(count) + s[i - 1], 1
-                elif s[i] == s[i - 1]:
+        def rle(nums):
+            count, ans = 1, ""
+            for i in range(1, len(nums) + 1):
+                if i < len(nums) and nums[i] == nums[i - 1]:
                     count += 1
-            return ans + str(count) + s[-1]
+                else:
+                    ans = ans + str(count) + nums[i - 1]
+                    count = 1
+            return ans
 
-        if n == 1: return str(n)
+        if n == 1: return "1"
         return rle(self.countAndSay(n - 1))
-
-
-
         
