042e88ff2dd7e551df15d575752bfe0e2474f948
Long Bian <bianlong0618@qq.com>
2024-10-24 12:55:17 -0400

    Time: 64 ms (95.51%), Space: 16.9 MB (13.29%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -4,40 +4,37 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         def dfsHelper(iter=0):
-            if iter == len(remains): return True
-            i, j = remains[iter]
+            if iter == len(remain): return True
+            i, j = remain[iter]
             k = (i // 3) * 3 + j // 3
             for num in rows[i] & cols[j] & blocks[k]:
-                board[i][j] = str(num)
                 rows[i].remove(num)
                 cols[j].remove(num)
                 blocks[k].remove(num)
-                if dfsHelper(iter + 1):
-                    return True
-                board[i][j] = "."
+                board[i][j] = str(num)
+                if dfsHelper(iter + 1): return True
                 rows[i].add(num)
                 cols[j].add(num)
                 blocks[k].add(num)
+                board[i][j] = "."
 
-
+        
+        
         rows = [set([i for i in range(1, 10)]) for _ in range(9)]
         cols = [set([i for i in range(1, 10)]) for _ in range(9)]
         blocks = [set([i for i in range(1, 10)]) for _ in range(9)]
-        remains, iter = [], 0
-        for i in range(9):
-            for j in range(9):
+        remain = []
+        for i in range(len(board)):
+            for j in range(len(board)):
                 if board[i][j] == ".": 
-                    remains.append([i, j])
+                    remain.append((i, j))
                 else:
-                    num, k = int(board[i][j]), (i // 3) * 3 + j // 3
+                    k, num = (i // 3) * 3 + j // 3, int(board[i][j])
                     rows[i].remove(num)
                     cols[j].remove(num)
                     blocks[k].remove(num)
         dfsHelper()
 
         
-                
-
-                
 
         
