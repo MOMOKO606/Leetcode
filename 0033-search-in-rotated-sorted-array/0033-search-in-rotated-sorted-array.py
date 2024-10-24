cfe37d304d8a613de7aa4759301ff22f35d8fa4b
Long Bian <bianlong0618@qq.com>
2024-10-24 12:01:44 -0400

    Time: 0 ms (100%), Space: 16.8 MB (79.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -4,16 +4,15 @@ class Solution:
         while low <= high:
             mid = (low + high) // 2
             if nums[mid] == target: return mid
-            if nums[mid] <= nums[high]:
-                if nums[mid] < target <= nums[high]:
-                    low = mid + 1
-                else:
-                    high = mid - 1
-            else:
+            if nums[low] <= nums[mid]:
                 if nums[low] <= target < nums[mid]:
                     high = mid - 1
                 else:
                     low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
         return -1
-
         
