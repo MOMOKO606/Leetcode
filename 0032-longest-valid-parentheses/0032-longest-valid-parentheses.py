3b8b40083e2da769222aa0ce409309ab9f82090c
Long Bian <bianlong0618@qq.com>
2024-10-24 11:40:27 -0400

    Time: 3 ms (99.77%), Space: 16.8 MB (79.2%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,13 +1,14 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
+        if not s: return 0
         dp = [0] * len(s)
-        for i in range(len(s)):
-            if i > 0 and s[i] == ")":
-                if s[i - 1] == "(":
-                    dp[i] = dp[i - 2] + 2 if i > 1 else 2
-                else: 
-                    j = i - dp[i - 1] - 1
-                    if j >= 0 and s[j] == "(":
-                        dp[i] = dp[j - 1] + dp[i - 1] + 2 if j > 0 else dp[i - 1] + 2
-        return max(dp) if dp else 0
+        for j in range(1, len(s)):
+            if s[j] == ")":
+                if s[j - 1] == "(":
+                    dp[j] = dp[j - 2] + 2 if j > 1 else 2
+                else:
+                    i = j - dp[j - 1] - 1
+                    if i >= 0 and s[i] == "(":
+                        dp[j] = dp[i - 1] + dp[j - 1] + 2 if i >= 1 else dp[j - 1] + 2
+        return max(dp) 
         
