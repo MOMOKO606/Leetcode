3fecb57a4c466a93aa69accd6da8aba246105ee6
Long Bian <bianlong0618@qq.com>
2024-10-25 15:49:12 -0400

    Time: 1159 ms (5.81%), Space: 777.2 MB (5.02%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -3,7 +3,9 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         if not p: return not s
         if not s: return p[-1] == "*" and self.isMatch(s, p[:-1])
-        if p[-1] == s[-1] or p[-1] == "?": return self.isMatch(s[:-1], p[:-1])
+        if s[-1] == p[-1] or p[-1] == "?":
+            return self.isMatch(s[:-1], p[:-1])
         if p[-1] == "*":
             return self.isMatch(s, p[:-1]) or self.isMatch(s[:-1], p)
-        return False 
+        return False
+        
