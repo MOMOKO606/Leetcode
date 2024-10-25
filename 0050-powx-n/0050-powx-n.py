95e4233a33175ffadf549642e5ba7c5e7e03343f
Long Bian <bianlong0618@qq.com>
2024-10-25 16:09:44 -0400

    Time: 0 ms (100%), Space: 16.7 MB (7.49%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,8 +1,9 @@
 class Solution:
-    @cache
     def myPow(self, x: float, n: int) -> float:
         if n == 0: return 1.0
         if n < 0: return 1 / self.myPow(x, -n)
         half = self.myPow(x, n // 2)
         if n & 1: return half * half * x
         else: return half * half
+
+        
