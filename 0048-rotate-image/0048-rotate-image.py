2367051587a91178bd491632dc69645dbd005ed7
Long Bian <bianlong0618@qq.com>
2024-10-25 17:02:01 -0400

    Time: 1 ms (90.3%), Space: 16.7 MB (23.27%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,28 +3,23 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        def move_all(i, j, steps):
-            N, to_be_moved, di, dj = 4, matrix[i][j], 0, 1
-            while N:
-                next_i, next_j, di, dj = get_next(i, j, di, dj, steps)
-                matrix[next_i][next_j], to_be_moved = to_be_moved, matrix[next_i][next_j]
-                i, j = next_i, next_j
-                N -= 1
-
-        def get_next(i, j, di, dj, steps):
+        def next_position(i, j, di, dj, steps):
             while steps:
-                if not (top <= i + di <= bottom and left <= j + dj <= right):
+                if not (left <= i + di <= right and top <= j + dj <= bottom):
                     di, dj = dj, -di
-                i, j, steps = i + di, j + dj, steps - 1
+                steps, i, j = steps - 1, i + di, j + dj
             return i, j, di, dj
 
+        def move_4_points(i, j, steps):
+            to_be_moved, di, dj = matrix[i][j], 0, 1
+            for _ in range(4):
+                next_i, next_j, di, dj = next_position(i, j, di, dj, steps)
+                matrix[next_i][next_j], to_be_moved, i, j = to_be_moved, matrix[next_i][next_j], next_i, next_j
 
-        top, bottom, left, right = 0, len(matrix) - 1, 0, len(matrix) - 1
-        while top < bottom:
-            steps = n = right - left
-            i, j = top, left
-            while n:
-                move_all(i, j, steps)
-                j += 1
-                n -= 1
-            top, bottom, left, right = top + 1, bottom - 1, left + 1, right - 1
+        left, right, top, bottom = 0, len(matrix) - 1, 0, len(matrix) - 1
+        while left < right:
+            for j in range(left, right):
+                move_4_points(top, j, right - left)
+            left, right, top, bottom = left + 1, right - 1, top + 1, bottom - 1
+
+                 
