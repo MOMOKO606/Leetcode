9e51260dcd10abd358fd90b86131ca58c1ed3a7e
Long Bian <bianlong0618@qq.com>
2024-10-25 14:23:25 -0400

    Time: 16 ms (94.99%), Space: 18.3 MB (97.78%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,11 +1,12 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left_max, right_max, ans = [0] * len(height), [0] * len(height), 0
-        for i in range(1, len(height)):
-            left_max[i] = max(left_max[i - 1], height[i - 1])
-        for j in range(len(height) - 2, -1, -1):
-            right_max[j] = max(right_max[j + 1], height[j + 1])
-        for i in range(1, len(height) - 1):
-            ans += max(min(left_max[i], right_max[i]) - height[i], 0)
+        l, r, i, j, ans = height[0], height[-1], 1, len(height) - 2, 0
+        while i <= j:
+            if l <= r:
+                ans, l, i = ans + max(l - height[i], 0), max(l, height[i]), i + 1
+            else:
+                ans, r, j = ans + max(r - height[j], 0), max(r, height[j]), j - 1
         return ans
+
+
         
