f0e617964e9254979fb4a6ce1cb853073a760ca1
Long Bian <bianlong0618@qq.com>
2024-10-27 21:53:22 -0400

    Time: 0 ms (100%), Space: 16.6 MB (10.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,6 +3,6 @@ class Solution:
         if n < 3: return n
         f1, f2 = 1, 2
         for _ in range(n - 2):
-            f2, f1 = f1 + f2, f2
+            f1, f2 = f2, f1 + f2
         return f2
         
