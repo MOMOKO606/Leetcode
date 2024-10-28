03a1b06dfdfc3228b0d9e655a2a1f8855fdecc2a
Long Bian <bianlong0618@qq.com>
2024-10-28 13:55:27 -0400

    Time: 4349 ms (43.2%), Space: 16.5 MB (80.62%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,28 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfsHelper(i, j, curNode):
-            if "$" in curNode: return True
-            if not (0 <= i < rows and 0 <= j < cols and board[i][j] in curNode): return False
-            curNode = curNode[board[i][j]]
+        def helper(i, j, node):
+            if "#" in node: return True
+            if not (0 <= i < rows and 0 <= j < cols and board[i][j] in node): return False
             ori = board[i][j]
-            board[i][j] = "#"
+            board[i][j] = "$"
             for x, y in [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]:
-                if dfsHelper(x, y, curNode): return True
+                if helper(x, y, node[ori]): return True
             board[i][j] = ori
-            return False
-        
+
         # Build the trie
-        trie = node = {}
+        node = trie = {}
         for char in word:
             node[char] = node.get(char, {})
             node = node[char]
-        node["$"] = "$"
+        node["#"] = "#"
 
         rows, cols = len(board), len(board[0])
         for i in range(rows):
             for j in range(cols):
-                if dfsHelper(i, j, trie): return True
+                if helper(i, j, trie): return True
         return False
 
-        
         
