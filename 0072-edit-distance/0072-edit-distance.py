c66d6a670baa26dbeda6e7cacb483b7d1287fef5
Long Bian <bianlong0618@qq.com>
2024-10-28 09:37:06 -0400

    Time: 67 ms (50.2%), Space: 20.2 MB (27.66%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,11 +1,11 @@
 class Solution:
-    @cache
     def minDistance(self, word1: str, word2: str) -> int:
-        if not word1 and not word2: return 0
-        if not word1: return len(word2)
-        if not word2: return len(word1)
-        if word1[-1] == word2[-1]:
-            return self.minDistance(word1[:-1], word2[:-1])
-        else:
-            return 1 + min(self.minDistance(word1[:-1], word2[:-1]), self.minDistance(word1[:-1], word2), self.minDistance(word1, word2[:-1]))
+        rows, cols = len(word1) + 1, len(word2) + 1
+        dp = [[0] * cols for _ in range(rows)]
+        for j in range(1, cols): dp[0][j] = j
+        for i in range(1, rows): dp[i][0] = i
+        for i in range(1, rows):
+            for j in range(1, cols):
+                dp[i][j] = dp[i - 1][j - 1] if word1[i - 1] == word2[j - 1] else 1 + min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])
+        return dp[-1][-1]
         
