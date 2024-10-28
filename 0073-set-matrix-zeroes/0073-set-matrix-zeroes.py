4124803f081c61d5600a6931ea8720de01aa3ae5
Long Bian <bianlong0618@qq.com>
2024-10-28 10:31:02 -0400

    Time: 0 ms (100%), Space: 17.4 MB (89.37%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,35 +3,31 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows, cols, zero_in_row, zero_in_col = len(matrix), len(matrix[0]), False, False
-        for i in range(rows): 
-            if not matrix[i][0]: 
-                zero_in_col = True
-                break
-        for j in range(cols): 
+        rows, cols = len(matrix), len(matrix[0])
+        firstRow, firstCol = False, False
+        for j in range(cols):
             if not matrix[0][j]: 
-                zero_in_row = True
+                firstRow = True
                 break
+        
         for i in range(rows):
-            for j in range(cols):
-                if not matrix[i][j]: matrix[0][j], matrix[i][0] = 0, 0
-        for i in range(1, rows):
             if not matrix[i][0]:
-                for j in range(1, cols):
+                firstCol = True
+                break
+
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if not matrix[i][j]:
+                    matrix[0][j], matrix[i][0] = 0, 0
+
+
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if not matrix[0][j] or not matrix[i][0]:
                     matrix[i][j] = 0
 
-        for j in range(1, cols):
-            if not matrix[0][j]:
-                for i in range(1, rows):
-                    matrix[i][j] = 0
-
-        if zero_in_row: 
-            for j in range(cols):
-                matrix[0][j] = 0
-        if zero_in_col:
-            for i in range(rows):
-                matrix[i][0] = 0
-
+        if firstRow:
+            for j in range(cols): matrix[0][j] = 0
         
-
-        
+        if firstCol:
+            for i in range(rows): matrix[i][0] = 0
