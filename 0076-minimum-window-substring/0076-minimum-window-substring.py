66a68c777ace3ed22fc83dea2ee41d6b264eecd2
Long Bian <bianlong0618@qq.com>
2024-10-28 14:33:40 -0400

    Time: 123 ms (40.41%), Space: 17.2 MB (86.64%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,20 +1,19 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        target, i, j, remain, ans, l, r = Counter(t), -1, -1, len(t), inf, -1, -1
+        i, j, count, remain, ans, l, r = -1, -1, Counter(t), len(t), inf, 0, 0
         while i <= j:
             if not remain: 
-                if j - i < ans: ans, l, r = j - i, i, j
-            if j < len(s) and remain > 0:
+                if j - i < ans:
+                    ans, l, r = j - i, i, j
+            if j < len(s) and remain:
                 j += 1
-                if j < len(s) and s[j] in target:
-                    target[s[j]] -= 1
-                    if target[s[j]] >= 0: remain -= 1
+                if j < len(s) and s[j] in count:
+                    count[s[j]] -= 1
+                    if count[s[j]] >= 0: remain -= 1
             else:
                 i += 1
-                if i < len(s) and s[i] in target:
-                    target[s[i]] += 1
-                    if target[s[i]] > 0: remain += 1
+                if i < len(s) and s[i] in count:
+                    count[s[i]] += 1
+                    if count[s[i]] > 0: remain += 1
         return s[l + 1: r + 1]
-                
-
         
