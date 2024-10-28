c330871015be8d35ce165249c43220df792d97ce
Long Bian <bianlong0618@qq.com>
2024-10-27 23:24:57 -0400

    Time: 0 ms (100%), Space: 16.6 MB (63.17%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -3,12 +3,11 @@ class Solution:
         line, occupied, ans = [], 0, []
         for word in words:
             if len(line) + occupied + len(word) > maxWidth:
-                for i in range(maxWidth - occupied):
-                    line[i % max((len(line) - 1), 1)] += " "
-                ans += ["".join(line)]
-                line, occupied = [], 0
+                for i in range(maxWidth - occupied - len(line) + 1):
+                    j = i % (len(line) - 1) if len(line) > 1 else 0
+                    line[j] += " "
+                ans, line, occupied = ans + [" ".join(line)], [], 0
             line, occupied = line + [word], occupied + len(word)
-        ans.append(" ".join(line).ljust(maxWidth))
-        return ans
-        
+        return ans + [" ".join(line).ljust(maxWidth)]
 
+        
