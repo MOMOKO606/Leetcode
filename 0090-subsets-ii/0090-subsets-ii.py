34bedd46b6a5bbcf7f97086bf102776ffdedf0c1
Long Bian <bianlong0618@qq.com>
2024-10-30 12:51:19 -0400

    Time: 3 ms (27.39%), Space: 16.7 MB (88.54%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,11 +1,13 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        visited, ans = collections.defaultdict(list), [[]]
+        ans, visited = [[]], {}
         for num in sorted(nums):
             tmp = []
             for seq in ans:
-                if seq in visited[num]: continue
+                if num in visited and seq in visited[num]: continue
+                visited[num] = visited.get(num, []) + [seq]
                 tmp += [[num] + seq]
-                visited[num] += [seq]
             ans += tmp
-        return ans        
+        return ans
+
+        
