13b184f117c520a464936165fed821c6e2aadcb1
Long Bian <bianlong0618@qq.com>
2024-11-01 14:32:49 -0400

    Time: 33 ms (93.22%), Space: 20.9 MB (5.24%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -2,9 +2,9 @@ class Solution:
     @cache
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if not s3: return not s1 and not s2
-        if not s1: return s2 == s3
-        if not s2: return s1 == s3
-        l = self.isInterleave(s1[:-1], s2, s3[:-1]) if s1[-1] == s3[-1] else False
-        r = self.isInterleave(s1, s2[:-1], s3[:-1]) if s2[-1] == s3[-1] else False
-        return l or r
+        possible1, possible2 = False, False
+        if s1 and s3[-1] == s1[-1]: possible1 = self.isInterleave(s1[:-1], s2, s3[:-1])
+        if s2 and s3[-1] == s2[-1]: possible2 = self.isInterleave(s1, s2[:-1], s3[:-1])
+        return possible1 or possible2
+
         
