347e0917aaf749b9504f957e4d79a9ea0240d403
Long Bian <bianlong0618@qq.com>
2024-11-01 12:53:03 -0400

    Time: 0 ms (100%), Space: 16.5 MB (74.43%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,15 +1,17 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        def helper(i=0, j=0, seq=""):
-            if i == len(s) and j == 4: 
-                ans.append(seq[:-1])
+        def helper(s, n, seq):
+            if not n and not s: 
+                ans.append(seq)
                 return
-            if i == len(s) or j == 4: return 
-            helper(i + 1, j + 1, seq + s[i] + ".")
-            if 10 <= int(s[i: i + 2]) <= 99: helper(i + 2, j + 1, seq + s[i: i + 2] + ".")
-            if 100 <= int(s[i: i + 3]) <= 255: helper(i + 3, j + 1, seq + s[i: i + 3] + ".")
+            if not n or not s: return
+            if 0 <= int(s[0]) < 10: helper(s[1:], n - 1, seq + [str(s[0])])
+            if 9 < int(s[:2]) < 100: helper(s[2:], n - 1, seq + [str(s[:2])])
+            if 100 <= int(s[:3]) < 256: helper(s[3:], n - 1, seq + [str(s[:3])])
+
 
         ans = []
-        helper()
-        return ans
+        helper(s, 4, [])
+        return [".".join(seq) for seq in ans]
+
         
