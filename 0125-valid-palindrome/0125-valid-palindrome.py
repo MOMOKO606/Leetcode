188ff1c36a32e945830fecc335a619c789f8fa03
Long Bian <bianlong0618@qq.com>
2024-11-04 14:17:35 -0500

    Time: 7 ms (81.97%), Space: 16.9 MB (91.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,9 +4,9 @@ class Solution:
         while i < j:
             if not s[i].isalnum(): i += 1
             elif not s[j].isalnum(): j -= 1
-            elif s[i].lower() == s[j].lower(): 
-                i, j = i + 1, j - 1
-            else:
-                return False
+            elif s[i].lower() != s[j].lower(): return False
+            else: i, j = i + 1, j - 1
         return True
+
+
         
