f6c678c5c932718fe6502e9cd91d2e93e7df2bee
Long Bian <bianlong0618@qq.com>
2024-11-03 21:08:33 -0500

    Time: 0 ms (100%), Space: 16.8 MB (74.79%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -9,17 +9,19 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        def helper(node):
-            if not node: return
+        def helper(node=root):
+            if not node: return 
             left, right = node.left, node.right
             if self.prev:
-                self.prev.left, self.prev.right = None, node
+                self.prev.right = node
+            node.left = None
             self.prev = node
             helper(left)
             helper(right)
-
-        self.prev = None
-        helper(root)
-
         
+        self.prev = None
+        helper()
+        return root
+            
+
         
