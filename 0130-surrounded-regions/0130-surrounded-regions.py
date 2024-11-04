e1b94c877d6a8ca4f224e0b8a11b0ab024cd9040
Long Bian <bianlong0618@qq.com>
2024-11-04 15:00:01 -0500

    Time: 3 ms (89.96%), Space: 20.7 MB (30.34%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,34 +3,24 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        def helper(i, j):
-            deque = collections.deque([[i, j]])
-            while deque:
-                i, j = deque.popleft()
-                for x, y in [[i + 1, j], [i - 1, j], [i, j - 1], [i, j + 1]]:
-                    if not (0 <= x < rows and 0 <= y < cols and board[x][y] == "O"): continue
-                    board[x][y] = "$"
-                    deque.append([x, y])
-
-
+        def dfsHelper(i, j):
+            if not (0 <= i < rows and 0 <= j < cols and board[i][j] == "O"): return
+            board[i][j] = "$"
+            for x, y in [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]:
+                dfsHelper(x, y)
+        
+        
         rows, cols = len(board), len(board[0])
         for i in [0, rows - 1]:
             for j in range(cols):
-                if board[i][j] == "X": continue
-                board[i][j] = "$"
-                helper(i, j)
+                dfsHelper(i, j)
 
         for j in [0, cols - 1]:
             for i in range(rows):
-                if board[i][j] == "X": continue
-                board[i][j] = "$"
-                helper(i, j)
+                dfsHelper(i, j)
 
         for i in range(rows):
             for j in range(cols):
-                if board[i][j] == "O":
-                    board[i][j] = "X"
-                elif board[i][j] == "$":
-                    board[i][j] = "O"
-
+                if board[i][j] == "$": board[i][j] = "O"
+                else: board[i][j] = "X"
         
