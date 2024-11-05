d0f63b44c37e1a250c063db958dc45e0322a4f03
Long Bian <bianlong0618@qq.com>
2024-11-05 14:16:28 -0500

    Time: 4 ms (41.55%), Space: 17 MB (11.29%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,13 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         @cache
-        def helper(s):
+        def helper(s=s):
             if not s: return True
-            for word in wordDict:
+            for word in wordSet:
                 k = len(word)
-                if s[:k] == word and helper(s[k:]): return True
+                if s[:k] == word:
+                    if helper(s[k:]): return True
             return False
+        
+        wordSet = set(wordDict)
+        return helper()
+        
+        
 
-        wordDict = set(wordDict)
-        return helper(s)
         
