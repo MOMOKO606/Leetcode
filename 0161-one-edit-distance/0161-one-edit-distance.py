6cd26b840bc22d95d545f1a9b3518773fb17fafb
Long Bian <bianlong0618@qq.com>
2024-11-07 14:53:01 -0500

    Time: 55 ms (5.49%), Space: 122 MB (5.45%) - LeetHub

--- 0161-one-edit-distance/0161-one-edit-distance.py
+++ 0161-one-edit-distance/0161-one-edit-distance.py
@@ -1,10 +1,9 @@
 class Solution:
+    @cache
     def isOneEditDistance(self, s: str, t: str) -> bool:
         if not s and not t: return False
-        if not s: return not t[:-1]
-        if not t: return not s[:-1]
-        if s[-1] == t[-1]: 
+        if s and t and s[-1] == t[-1]:
             return self.isOneEditDistance(s[:-1], t[:-1])
-        return s == t[:-1] or s[:-1] == t or s[:-1] == t[:-1]
-
+        else:
+            return s[:-1] == t or s == t[:-1] or s[:-1] == t[:-1]
         
