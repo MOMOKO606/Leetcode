2db77bcdcf78ca9e92e1939d18c9c845883126e2
Long Bian <bianlong0618@qq.com>
2024-11-06 22:58:12 -0500

    Time: 400 ms (5.45%), Space: 31.9 MB (81.35%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,30 +5,29 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def breakList(head):
-            prev = ListNode(next=head)
-            slow = fast = head
-            while fast and fast.next:
-                prev, slow, fast = slow, slow.next, fast.next.next
-            prev.next = None
-            return slow
-
-        def merge(l1, l2):
+        def merge(node1, node2):
             dummy = prev = ListNode()
-            while l1 and l2:
-                if l1.val <= l2.val:
-                    prev.next = l1
-                    l1 = l1.next
+            while node1 and node2:
+                if node1.val < node2.val:
+                    prev.next = node1
+                    node1 = node1.next
                 else:
-                    prev.next = l2
-                    l2 = l2.next
+                    prev.next = node2
+                    node2 = node2.next
                 prev = prev.next
-            prev.next = (l1 or l2)
+            prev.next = (node1 or node2)
             return dummy.next
 
+        def splitList(node):
+            prev = ListNode(next=node)
+            slow = fast = node
+            while fast and fast.next:
+                prev, slow, fast = prev.next, slow.next, fast.next.next
+            prev.next = None
+            return node, slow
 
-        newHead = breakList(head)
-        if head == newHead: return head
-        return merge(self.sortList(head), self.sortList(newHead))
-        
+        h1, h2 = splitList(head)
+        if h1 == h2: return h1
+        h1, h2 = self.sortList(h1), self.sortList(h2)
+        return merge(h1, h2)
         
