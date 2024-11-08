1532e86a812b533ad7034f7ece5cddf037e65d30
Long Bian <bianlong0618@qq.com>
2024-11-08 16:43:56 -0500

    Time: 253 ms (42.9%), Space: 19 MB (71.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,19 +1,16 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def helper(i, j):
-            if grid[i][j] == "0": return
+        def dfsHelper(i, j):
+            if not (0 <= i < rows and 0 <= j < cols and grid[i][j] == "1"): return 0
             grid[i][j] = "0"
             for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
-                if not (0 <= x < rows and 0 <= y < cols and grid[x][y] == "1"): continue
-                helper(x, y)
-                
+                dfsHelper(x, y)
+            return 1
+
 
         rows, cols, ans = len(grid), len(grid[0]), 0
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == "0": continue
-                ans += 1
-                helper(i, j)
+                ans += dfsHelper(i, j)
         return ans
-
         
