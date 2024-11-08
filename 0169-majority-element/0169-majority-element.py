d7dba63df409c891da8efa9f96c87a67634920df
Long Bian <bianlong0618@qq.com>
2024-11-07 23:12:28 -0500

    Time: 3 ms (82.22%), Space: 18.5 MB (6.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        repeated, count = None, 0
+        mode, count = None, 0
         for num in nums:
-            if not count: 
-                repeated, count = num, 1
-            elif num != repeated: count -= 1
-            else: count += 1
-        return repeated
+            if not mode: 
+                mode, count = num, 1
+            elif num != mode:
+                count -= 1
+                if not count: mode = None
+            else:
+                count += 1
+        return mode
         
