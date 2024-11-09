817c0dd84d3ad3fcc8deae9e93d657be0b8218be
Long Bian <bianlong0618@qq.com>
2024-11-08 23:52:00 -0500

    Time: 0 ms (100%), Space: 18.5 MB (33.93%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,20 +1,21 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        def dfsHelper(node):
+            if node in visited: return visited[node]
+            visited[node] = True
+            for neighbor in graph[node]:
+                if dfsHelper(neighbor): return True
+            visited[node] = False
+            ans.append(node)
+        
+        
         # Build the graph
-        graph, indegs = collections.defaultdict(list), {i: 0 for i in range(numCourses)}
+        graph, ans, visited = collections.defaultdict(list), [], {}
         for u, v in prerequisites:
             graph[v].append(u)
-            indegs[u] += 1
-        queue, ans = [node for node in indegs if not indegs[node]], []
-        while queue:
-            nextQueue = []
-            for node in queue:
-                ans.append(node)
-                numCourses -= 1
-                for neighbor in graph[node]:
-                    indegs[neighbor] -= 1
-                    if not indegs[neighbor]: nextQueue.append(neighbor)
-            queue = nextQueue
-        return ans if not numCourses else []
-            
+
+        for node in range(numCourses):
+            if dfsHelper(node): return []
+        return ans[::-1]
+        
         
