df33c11dcfe4886e271a4e75fc4aa0038607da3a
Long Bian <bianlong0618@qq.com>
2024-11-09 20:24:05 -0500

    Time: 139 ms (64.39%), Space: 35.9 MB (24.82%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
@@ -7,8 +7,8 @@ class Solution:
                 del buckets[(nums[i] - smallest) // (valueDiff + 1)]
             k = (num - smallest) // (valueDiff + 1)
             if k in buckets: return True
-            if k - 1 in buckets and abs(num - buckets[k - 1]) < valueDiff: return True
-            if k + 1 in buckets and abs(num - buckets[k + 1]) < valueDiff: return True
+            if k - 1 in buckets and abs(num - buckets[k - 1]) <= valueDiff: return True
+            if k + 1 in buckets and abs(num - buckets[k + 1]) <= valueDiff: return True
             buckets[k] = num
         return False
 
