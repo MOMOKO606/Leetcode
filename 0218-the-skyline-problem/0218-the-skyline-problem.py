366c451380f8933d240b221a0f77975368e5ef9b
Long Bian <bianlong0618@qq.com>
2024-11-12 13:57:45 -0500

    Time: 1313 ms (10.26%), Space: 22 MB (61.94%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.py
+++ 0218-the-skyline-problem/0218-the-skyline-problem.py
@@ -1,42 +1,27 @@
 class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
-        # We use a maxHeap to store all the height we have sweeped, and
-        # Maintain the current highest building at top.
-        # So we initialize the maxHeap with height 0
-        heap = [0]
-        # We use sweepLines to go over the whole pic from left to right
-        # Make the height negative to represent we entry a building from the left,
-        # Make the height to remain positive to represent we leave a building from the right.
-        # Reason: cover the edge cases.
-        #        - 1. If many buildings have the same left points but different height, we should only 
-        #             draw the heighest one.
-        #        - 2. If many buildings have the same right points but different height, we should only 
-        #             draw the lowest one.
-        # So after sorting the sweepLines, the x-coordinate will be sorted first, 
-        # then the highest height when entering, the lowest when leaving
-        sweepLines = []
+        ans, maxHeap, sweepLines = [], [0], []
         for left, right, height in buildings:
             sweepLines.append([left, -height])
             sweepLines.append([right, height])
-        sweepLines.sort()
+        sweepLines = sorted(sweepLines)
 
-        # Now let's sweep the whole pic
-        ans = []
-        for endPoint, height in sweepLines:
-            # If we are entering the building
-            if height < 0:
-                # If we have a higher building now, add it to the ans
-                if height < heap[0]:
-                    ans.append([endPoint, -height])
-                # Remember to keep tracking of all the heights when we enter buildings.
-                heapq.heappush(heap, height)
-            # If we are leaving the building
+        for sweepLine in sweepLines:
+            curEnd, curHeight = sweepLine[0], sweepLine[1]
+            # Enter a building
+            if curHeight < 0:
+                if curHeight < maxHeap[0]:
+                    ans.append([curEnd, -curHeight])
+                heapq.heappush(maxHeap, curHeight)
+            # Leaving a building
             else:
-                heap.remove(-height)
-                heapq.heapify(heap)
-                # If we have a lower building now, add it to the ans
-                if -height < heap[0]:
-                    ans.append([endPoint, -heap[0]])
+                maxHeap.remove(-curHeight)
+                heapq.heapify(maxHeap)
+                if curHeight > -maxHeap[0]:
+                    ans.append([curEnd, -maxHeap[0]])
         return ans
 
-                
+
+
+
+        
