0f6f48cac8d9cdd3c262e7b08ed67f9bfebdffbb
Long Bian <bianlong0618@qq.com>
2024-11-11 19:30:07 -0500

    Time: 0 ms (100%), Space: 16.6 MB (59.41%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -6,7 +6,6 @@ class Solution:
         for char, word in zip(pattern, s):
             if char not in visited and word not in visited.values():
                 visited[char] = word
-            elif char in visited and visited[char] != word: return False
-            elif char not in visited and word in visited.values(): return False
+            elif char in visited and visited[char] != word or char not in visited and word in visited.values(): return False
         return True
         
