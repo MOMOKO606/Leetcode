3aa14cc52a944a6116d56f7decddcd62df1d9232
Long Bian <bianlong0618@qq.com>
2024-11-12 15:26:15 -0500

    Time: 114 ms (61.56%), Space: 28.8 MB (41.43%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -2,26 +2,24 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         rows, cols = len(matrix), len(matrix[0])
-        for j in range(1, cols):
-            matrix[0][j] += matrix[0][j - 1]
-        for i in range(1, rows):
-            matrix[i][0] += matrix[i - 1][0]
+        for j in range(1, cols): matrix[0][j] += matrix[0][j - 1]
+        for i in range(1, rows): matrix[i][0] += matrix[i - 1][0]
+        
         for i in range(1, rows):
             for j in range(1, cols):
-                matrix[i][j] += matrix[i - 1][j] + matrix[i][j - 1] - matrix[i - 1][j - 1]
+                matrix[i][j] += matrix[i][j - 1] + matrix[i - 1][j] - matrix[i - 1][j - 1]
         self.matrix = matrix
-        self.rows = rows
-        self.cols = cols
-
 
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        matrix, rows, cols = self.matrix, self.rows, self.cols
+        matrix = self.matrix
         ans = matrix[row2][col2]
         if row1 > 0: ans -= matrix[row1 - 1][col2]
         if col1 > 0: ans -= matrix[row2][col1 - 1]
         if row1 > 0 and col1 > 0: ans += matrix[row1 - 1][col1 - 1]
         return ans
+
+
         
 
 
