e7c3bfcb296da207c834d3261076a19b2a91e8a3
Long Bian <bianlong0618@qq.com>
2024-11-11 19:29:47 -0500

    Time: 0 ms (100%), Space: 16.6 MB (19.86%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,12 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        visited = {}
+        s = s.split(" ")
+        if len(pattern) != len(s): return False
+        for char, word in zip(pattern, s):
+            if char not in visited and word not in visited.values():
+                visited[char] = word
+            elif char in visited and visited[char] != word: return False
+            elif char not in visited and word in visited.values(): return False
+        return True
+        
