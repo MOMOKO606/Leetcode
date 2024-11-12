e6809ea9025a9920edb685fa54dda8e644fcd54e
Long Bian <bianlong0618@qq.com>
2024-11-11 21:49:26 -0500

    Time: 23 ms (7.07%), Space: 16.7 MB (9.79%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.py
+++ 0299-bulls-and-cows/0299-bulls-and-cows.py
@@ -1,15 +1,14 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        bulls, cows, secret, guess = 0, 0, list(secret), list(guess)
-        for i in range(len(secret)): 
-            if secret[i] == guess[i]:
-                bulls += 1
-                secret[i], guess[i] = "#", "#"
-        count = Counter(secret)
-        for char, freq in Counter(guess).items():
-            if char == "#" or char not in count: continue
-            cows += min(freq, count[char])
-        return f"{bulls}A{cows}B"
+        bulls, cows, targets, indexes = 0, 0, {}, []
+        for i, (char1, char2) in enumerate(zip(secret, guess)):
+            if char1 == char2: bulls += 1
+            else: targets[char1], indexes = targets.get(char1, 0) + 1, indexes + [i]
 
-            
+        for i in indexes:
+            if guess[i] in targets and targets[guess[i]] > 0:
+                cows, targets[guess[i]] = cows + 1, targets[guess[i]] - 1
+        return str(bulls) + "A" + str(cows) + "B"
+
+        
         
