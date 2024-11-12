ac3eadffa2aa570903686684c37c5fbe779f819f
Long Bian <bianlong0618@qq.com>
2024-11-12 10:04:34 -0500

    Time: 2 ms (50.25%), Space: 16.8 MB (32.18%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,27 +1,27 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
+        def dfsHelper(node):
+            if node in visited: return visited[node]
+            visited[node] = True
+            for neighbor in graph[node]:
+                if dfsHelper(neighbor): return True
+            visited[node] = False
+            self.ans += node
+
+
         # Build the graph
-        graph, indegs = collections.defaultdict(set), {char: 0 for word in words for char in word}
+        graph = {char: set() for word in words for char in word}
         for word1, word2 in zip(words[:-1], words[1:]):
             for char1, char2 in zip(word1, word2):
                 if char1 != char2:
-                    if char2 not in graph[char1]:
-                        graph[char1].add(char2)
-                        indegs[char2] += 1
+                    graph[char1].add(char2)
                     break
             else:
                 if len(word1) > len(word2): return ""
-
-        queue, ans = [char for char, indeg in indegs.items() if not indeg], ""
-        while queue:
-            nextQueue = []
-            for node in queue:
-                ans += node
-                for neighbor in graph[node]:
-                    indegs[neighbor] -= 1
-                    if not indegs[neighbor]:
-                        nextQueue.append(neighbor)
-            queue = nextQueue
-        return ans if len(ans) == len(indegs) else ""
-
+        
+        self.ans, visited = [], {}
+        for word in words:
+            for char in word:
+                if dfsHelper(char): return ""
+        return "".join(self.ans[::-1])
         
