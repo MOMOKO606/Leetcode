d44a3de649ebd70f77be06fb8b49792b752aec27
Long Bian <bianlong0618@qq.com>
2024-11-12 19:56:58 -0500

    Time: 59 ms (19.7%), Space: 36.6 MB (9.26%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -3,7 +3,7 @@ class Solution:
         lis = []
         for num in nums:
             k = bisect.bisect_left(lis, num)
-            if k == len(lis): bisect.insort(lis, num)
+            if k == len(lis): lis.append(num)
             else: lis[k] = num
         return len(lis) > 2
         
