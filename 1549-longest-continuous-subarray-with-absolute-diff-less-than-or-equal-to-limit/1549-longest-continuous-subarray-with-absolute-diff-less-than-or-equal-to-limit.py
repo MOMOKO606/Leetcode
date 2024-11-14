97bfe83548a25875835b1b551b430d44359f2d87
Long Bian <bianlong0618@qq.com>
2024-11-14 11:36:57 -0500

    Time: 263 ms (40.27%), Space: 36.6 MB (23.44%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,20 +1,22 @@
-from heapq import heappush, heappop
+from collections import deque
+
 
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        i, maxHeap, minHeap, ans = -1, [], [], 0
+        ans, i, minQueue, maxQueue = 0, -1, deque([]), deque([])
         for j in range(len(nums)):
-            heappush(maxHeap, (-nums[j], j))
-            heappush(minHeap, (nums[j], j))
-            while -maxHeap[0][0] - minHeap[0][0] > limit:
+            while maxQueue and nums[j] > maxQueue[-1][0]:
+                maxQueue.pop()
+            maxQueue.append((nums[j], j))
+            while minQueue and nums[j] < minQueue[-1][0]:
+                minQueue.pop()
+            minQueue.append((nums[j], j))
+            while maxQueue[0][0] - minQueue[0][0] > limit:
                 i += 1
-                while maxHeap[0][1] <= i:
-                    heappop(maxHeap)
-                while minHeap[0][1] <= i:
-                    heappop(minHeap)
+                while maxQueue[0][1] <= i: maxQueue.popleft()
+                while minQueue[0][1] <= i: minQueue.popleft()
             ans = max(ans, j - i)
         return ans
-
 
 
 
