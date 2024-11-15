4a8d1252a72af5fa842f29ee76d36a2404701131
Long Bian <bianlong0618@qq.com>
2024-11-15 10:21:11 -0500

    Time: 213 ms (28.84%), Space: 18 MB (15.1%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -3,11 +3,14 @@ class Solution:
         @cache
         def helper(i=0, prev=1):
             if i == len(nums): return []
-            ans = helper(i + 1, prev)
-            if not nums[i] % prev:
-                ans = max(ans, [nums[i]] + helper(i + 1, nums[i]), key=len)
+            ans = []
+            for j in range(i, len(nums)):
+                if not nums[j] % prev:
+                    ans = max(ans, [nums[j]] + helper(j + 1, nums[j]), key=len)
             return ans
 
         nums = sorted(nums)
         return helper()
+
+
         
