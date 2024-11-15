fc3ff612e22028cb77d63d58ce8ba93277a85256
Long Bian <bianlong0618@qq.com>
2024-11-15 11:46:49 -0500

    Time: 2594 ms (21.96%), Space: 48.3 MB (12.94%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,33 +1,23 @@
-class FenwickTree:
-    def __init__(self, original_value, size):
-        self.ft = [original_value] * size
-
-    def __lowbit(self, i):
-        return i & -i
-
-    def update(self, i, delta):
-        while i < len(self.ft):
-            self.ft[i] += delta
-            i += self.__lowbit(i)
-
-    def query(self, i):
-        ans = 0
-        while i > 0:
-            ans += self.ft[i]
-            i -= self.__lowbit(i)
-        return ans
-
-
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        transfer, ans = {num: i + 1 for i, num in enumerate(sorted(set(nums)))}, []
-        ft = FenwickTree(original_value=0, size=len(transfer) + 1)
-        for num in reversed(nums):
-            i = transfer[num]
-            ft.update(i, 1)
-            ans.append(ft.query(i - 1))
-        return ans[::-1]
-
-       
+        def merge(left, right):
+            nums = []
+            while left and right:
+                if left[0][0] > right[0][0]:
+                    ans[left[0][1]] += len(right)
+                    nums.append(left.pop(0))
+                else:
+                    nums.append(right.pop(0))
+            return nums + (left or right)
+                
+        def mergeSort(nums):
+            if len(nums) == 1: return nums
+            mid = len(nums) // 2
+            left = mergeSort(nums[:mid])
+            right = mergeSort(nums[mid:])
+            return merge(left, right)
 
         
+        ans = [0] * len(nums)
+        mergeSort([(num, i) for i, num in enumerate(nums)])
+        return ans
