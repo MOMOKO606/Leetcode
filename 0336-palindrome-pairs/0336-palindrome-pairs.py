b89ec417ccd1a2f93a5aab77c0e663208428cd09
Long Bian <bianlong0618@qq.com>
2024-11-15 12:17:25 -0500

    Time: 1792 ms (55.49%), Space: 28 MB (68.43%) - LeetHub

--- 0336-palindrome-pairs/0336-palindrome-pairs.py
+++ 0336-palindrome-pairs/0336-palindrome-pairs.py
@@ -1,18 +1,15 @@
 class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
-        headsTails, ans = {word: i for i, word in enumerate(words)}, []
+        visited, ans = {word: i for i, word in enumerate(words)}, set()
         for i, word in enumerate(words):
-            # Cut the word
             for j in range(len(word) + 1):
                 left, right = word[:j], word[j:]
-                # Check head + word
-                if left == left[::-1] and right[::-1] in headsTails and headsTails[right[::-1]] != i: 
-                    ans.append([headsTails[right[::-1]], i])
-                # Check word + tail
-                if right == right[::-1] and left[::-1] in headsTails and headsTails[left[::-1]] != i and len(word) != len(left): 
-                    ans.append([i, headsTails[left[::-1]]])
-        return ans
-
-
-        
+                # head + word = palindrome
+                if left == left[::-1] and right[::-1] in visited and visited[right[::-1]] != i:
+                    ans.add((visited[right[::-1]], i))
+                # word + tail = palindrome
+                if right == right[::-1] and left[::-1] in visited and visited[left[::-1]] != i:
+                    ans.add((i, visited[left[::-1]]))
+        return list(ans)
+         
         
