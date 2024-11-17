5a28f918ba1f790e2799a21b5eb443d7a00ec369
Long Bian <bianlong0618@qq.com>
2024-11-16 22:50:27 -0500

    Time: 3 ms (54.57%), Space: 16.7 MB (10.05%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,7 +1,7 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         target = Counter(s)
-        for char in t:
-            if char not in target or target[char] == 0: return char
+        for i, char in enumerate(t):
+            if not target.get(char, 0): return char
             target[char] -= 1
         
