d6c1a0900c183df0ce864f30ec062695ef54d1b3
Long Bian <bianlong0618@qq.com>
2024-11-18 18:12:48 -0500

    Time: 0 ms (100%), Space: 17.1 MB (78.87%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,12 +1,14 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         ans = []
-        for i in range(n):
-            j = i + 1
-            if not j % 3 and not j % 5: ans.append("FizzBuzz")
-            elif not j % 3: ans.append("Fizz")
-            elif not j % 5: ans.append("Buzz")
-            else: ans.append(str(j))
+        for i in range(1, n + 1):
+            if not i % 3 and not i % 5:
+                ans.append("FizzBuzz")
+            elif not i % 3:
+                ans.append("Fizz")
+            elif not i % 5:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
         return ans
-        
         
