70804d73424260ff31daefdbd11014bf006fb950
Long Bian <bianlong0618@qq.com>
2024-11-20 14:58:06 -0500

    Time: 62 ms (49.01%), Space: 17.4 MB (58.39%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        freqs, n, i, ans = Counter(p), len(p), 0, []
+        for j in range(len(s) + 1):
+            if not n: ans.append(i)
+            while i < len(s) and j - i + 1 > len(p):
+                if s[i] in freqs:
+                    freqs[s[i]] += 1
+                    if freqs[s[i]] > 0: n += 1
+                i += 1
+            
+            if j < len(s) and s[j] in freqs:
+                freqs[s[j]] -= 1
+                if freqs[s[j]] >= 0: n -= 1
+        return ans
