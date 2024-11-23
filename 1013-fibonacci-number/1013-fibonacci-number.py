e11ad8729e741e95139b403c43175398a1948828
Long Bian <bianlong0618@qq.com>
2024-11-23 14:27:13 -0500

    Time: 29 ms (90.87%), Space: 16.7 MB (9.92%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2: return n
+        f1, f2 = 0, 1
+        for _ in range(n - 1):
+            f1, f2 = f2, f1 + f2
+        return f2
