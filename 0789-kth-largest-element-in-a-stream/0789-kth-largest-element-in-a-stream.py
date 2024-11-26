38c8e2eb8d71c1f14960dfdc9d0869de453606e3
Long Bian <bianlong0618@qq.com>
2024-11-26 18:02:41 -0500

    Time: 43 ms (14.42%), Space: 23.7 MB (6.06%) - LeetHub

---
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -0,0 +1,16 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.nums = sorted(nums)
+        self.k = k
+        
+
+    def add(self, val: int) -> int:
+        bisect.insort(self.nums, val)
+        return self.nums[-self.k]
+
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
