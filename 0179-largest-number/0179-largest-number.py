43775b1dd1551c63ea495b8f80f98cd3f5752a9f
Long Bian <bianlong0618@qq.com>
2024-11-27 18:08:25 -0500

    Time: 15 ms (6.1%), Space: 17.2 MB (10%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -3,7 +3,7 @@ class Solution:
         nums = list(map(str, nums))
         for i in range(len(nums) - 1):
             for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] < nums[j] + nums[i]:
+                if nums[j] + nums[i] > nums[i] + nums[j]:
                     nums[i], nums[j] = nums[j], nums[i]
         return "".join(nums) if nums[0] != "0" else "0"
         
