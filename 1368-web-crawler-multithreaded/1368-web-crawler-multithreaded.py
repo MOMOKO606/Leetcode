22b562adc1ac93e8a3742380ad356ba27b2719f2
Long Bian <bianlong0618@qq.com>
2024-12-03 15:17:45 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1368-web-crawler-multithreaded/1368-web-crawler-multithreaded.py
+++ 1368-web-crawler-multithreaded/1368-web-crawler-multithreaded.py
@@ -8,32 +8,32 @@
 #        :type url: str
 #        :rtype List[str]
 #        """
-from queue import Queue
 from threading import Thread
-
+from queue import Queue
 
 class Solution:
     def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
         def _crawl():
+            tmp = []
             while True:
-                tmp = []
                 for url in htmlParser.getUrls(curQueue.get()):
                     if url.split("http://")[1].split("/")[0] == domain and url not in visited:
                         visited.add(url)
                         tmp.append(url)
                 nextQueue.put(tmp)
 
-        curQueue, nextQueue, visited, domain, running = Queue(), Queue(), set([startUrl]), startUrl.split("http://")[1].split("/")[0], 1
+
+        curQueue, nextQueue, running, visited = Queue(), Queue(), 1, set([startUrl])
+        domain = startUrl.split("http://")[1].split("/")[0]
         curQueue.put(startUrl)
         for _ in range(5):
             thread = Thread(target=_crawl, daemon=True)
             thread.start()
-
+        
         while running:
             for url in nextQueue.get():
                 curQueue.put(url)
                 running += 1
             running -= 1
         return list(visited)
-
         
