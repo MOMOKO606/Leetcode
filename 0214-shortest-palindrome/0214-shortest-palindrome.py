43d1be89e6666b832c632c16018aaf0cc01033b5
Long Bian <bianlong0618@qq.com>
2024-12-03 21:50:34 -0500

    Time: 47 ms (78.26%), Space: 21.3 MB (5.16%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,20 +1,18 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        def getNext(s):
-            i, next = 0, [0]
+        def get_next(s):
+            next, i = [0], 0
             for j in range(1, len(s)):
-                while i > 0 and s[i] != s[j]:
+                while i > 0 and s[j] != s[i]:
                     i = next[i - 1]
-                if s[i] == s[j]:
+                if s[i] == s[j]: 
                     i += 1
                     next.append(i)
-                elif i == 0:
+                else:
                     next.append(0)
             return next
 
-        backup = s
-        s = s + "#" + s[::-1]
-        next = getNext(s)
-        return backup[next[-1]:][::-1] + backup
-                
+        next = get_next(s + "#" + s[::-1])
+        i = next[-1]
+        return s[i:][::-1] + s
         
