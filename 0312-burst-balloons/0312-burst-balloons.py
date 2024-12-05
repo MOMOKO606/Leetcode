ab854c67a6529bc2b1358ebb0e13240144e2a7b4
Long Bian <bianlong0618@qq.com>
2024-12-04 23:01:34 -0500

    Time: 4832 ms (55.51%), Space: 35 MB (5.99%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -5,10 +5,9 @@ class Solution:
             if i > j: return 0
             ans = -inf
             for k in range(i, j + 1):
-                ans = max(ans, nums[i - 1] * nums[k] * nums[j + 1] + helper(i, k - 1) + helper(k + 1, j))
+                ans = max(ans, nums[k] * nums[i - 1] * nums[j + 1] + helper(i, k - 1) + helper(k + 1, j))
             return ans
 
         nums = [1] + nums + [1]
         return helper(1, len(nums) - 2)
-
         
