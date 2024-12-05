c9a4e6588df7cf59628c4ae10b67c1c55ed4cb7e
Long Bian <bianlong0618@qq.com>
2024-12-05 12:49:40 -0500

    Time: 0 ms (100%), Space: 17.1 MB (8.63%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.py
+++ 0233-number-of-digit-one/0233-number-of-digit-one.py
@@ -1,18 +1,13 @@
 class Solution:
     def countDigitOne(self, n: int) -> int:
-        digits = []
-        while n: digits, n = digits + [n % 10], n // 10
-        digits, ans = digits[::-1], 0
-        for k, digit in enumerate(digits):
-            left, right, p = 0, 0, 1
-            for i in range(k): left = left * 10 + digits[i]
-            for j in range(k + 1, len(digits)): right, p = right * 10 + digits[j], p * 10
-            if digit == 0: ans += left * p
-            elif digit == 1: ans += left * p + right + 1
-            else: ans += (left + 1) * p
+        digits, ans = [], 0
+        while n: digits, n = [n % 10] + digits, n // 10
+        for j, digit in enumerate(digits):
+            left, right, factor = 0, 0, 1
+            for i in range(j):  left = left * 10 + digits[i]
+            for i in range(j + 1, len(digits)): right, factor = right * 10 + digits[i], factor * 10
+            if digit == 0: ans += left * factor
+            elif digit == 1: ans += left * factor + right + 1
+            else: ans += (left + 1) * factor
         return ans
-
-
-
-
         
