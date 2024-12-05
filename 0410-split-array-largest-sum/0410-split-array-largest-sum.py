add301824f863ddad4c457c43eca66df4a118369
Long Bian <bianlong0618@qq.com>
2024-12-04 19:39:28 -0500

    Time: 0 ms (100%), Space: 17.3 MB (23.04%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,23 +1,23 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        def split(mid):
-            count, total = 1, 0
+        def cutArrayBy(mid):
+            count, cur_total = 1, 0
             for num in nums:
-                if total + num > mid:
-                    count, total = count + 1, num
+                if cur_total + num <= mid:
+                    cur_total += num
                 else:
-                    total += num
+                    cur_total = num
+                    count += 1
             return count
-
 
 
         low, high = max(nums), sum(nums)
         while low <= high:
             mid = (low + high) // 2
-            if split(mid) <= k:
+            if cutArrayBy(mid) <= k:
                 high = mid - 1
             else:
                 low = mid + 1
         return low
-
+        
         
