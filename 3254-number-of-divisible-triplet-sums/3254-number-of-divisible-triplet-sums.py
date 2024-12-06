8d4872a9d8604f1ef36d2ceea6a0cc4e816c33c1
Long Bian <bianlong0618@qq.com>
2024-12-05 22:50:16 -0500

    Time: 550 ms (51.7%), Space: 17.5 MB (36.54%) - LeetHub

--- 3254-number-of-divisible-triplet-sums/3254-number-of-divisible-triplet-sums.py
+++ 3254-number-of-divisible-triplet-sums/3254-number-of-divisible-triplet-sums.py
@@ -3,10 +3,10 @@ class Solution:
         ans = 0
         for i in range(len(nums) - 1):
             visited = {}
-            for k in range(i + 1, len(nums)):
-                if -nums[k] % d in visited: ans += visited[-nums[k] % d]
-                visited[(nums[i] + nums[k]) % d] = visited.get((nums[i] + nums[k]) % d, 0) + 1
+            for j in range(i + 1, len(nums)):
+                if -nums[j] % d in visited:
+                    ans += visited[-nums[j] % d]
+                visited[(nums[i] + nums[j]) % d] = visited.get((nums[i] + nums[j]) % d, 0) + 1
         return ans
-
 
         
