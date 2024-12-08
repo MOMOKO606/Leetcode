aaa41300b3636b9aee9c306bd4a8bcba294f79ad
Long Bian <bianlong0618@qq.com>
2024-12-08 16:05:20 -0500

    Time: 7 ms (85.37%), Space: 17.6 MB (51.17%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,17 +1,11 @@
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        queue, wordSet, words, ans = [""], set(words), sorted(words), ""
-        while queue:
-            next_queue = []
-            for word in queue:
-                for char in "abcdefghijklmnopqrstuvwxyz":
-                    new_word = word + char
-                    if new_word in wordSet:
-                        wordSet.remove(new_word)
-                        next_queue.append(new_word)
-                        ans = max(ans, new_word, key=len)
-            queue = next_queue
+        words, visited, ans = sorted(words), set([""]), ""
+        for word in words:
+            if word[:-1] in visited:
+                visited.add(word)
+                ans = max(ans, word, key=len)
         return ans
 
-        
+
         
