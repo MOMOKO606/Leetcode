29c1afd7b9ec3dcf27ed5485c6fd7109b58fd52f
Long Bian <bianlong0618@qq.com>
2024-12-09 23:42:15 -0500

    Time: 87 ms (10.89%), Space: 23.3 MB (18.4%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,37 +1,28 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        def parent(i):
-            root = i
-            while p[root] != root:
-                root = p[root]
-            while p[i] != i:
-                i, p[i] = p[i], root
-            return root
+        def dfsHelper(i):
+            if i in i_visited: return []
+            i_visited.add(i)
+            emails = []
+            for email in accounts[i][1:]:
+                if email in email_visited: continue
+                email_visited.add(email)
+                emails.append(email)
+                for neighbor in graph[email]:
+                    emails += dfsHelper(neighbor)
+            return sorted(emails)
 
-        def connect(i, j):
-            pi, pj = parent(i), parent(j)
-            p[pi] = pj
 
-        p = [i for i in range(len(accounts))]
 
-        email_to_account, root_to_email = {}, collections.defaultdict(set)
+        # Build the graph
+        graph = collections.defaultdict(list)
         for i, account in enumerate(accounts):
             for email in account[1:]:
-                if email in email_to_account:
-                    connect(i, email_to_account[email])
-                else:
-                    email_to_account[email] = i
+                graph[email].append(i)
 
+        i_visited, email_visited, ans = set(), set([]), []
         for i, account in enumerate(accounts):
-            pi = parent(i)
-            for email in account[1:]:
-                root_to_email[pi].add(email)
-
-        return [[accounts[i][0]] + sorted(list(emails)) for i, emails in root_to_email.items()]
-
-        
-
-        
-
-
+            if i in i_visited: continue
+            ans.append([account[0]] + dfsHelper(i))
+        return ans
         
