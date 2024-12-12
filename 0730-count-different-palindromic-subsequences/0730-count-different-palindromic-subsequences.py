8b3179ae4f2678bb174134605b65c51f24b4ab61
Long Bian <bianlong0618@qq.com>
2024-12-12 18:31:27 -0500

    Time: 691 ms (79.35%), Space: 91.6 MB (24.17%) - LeetHub

--- 0730-count-different-palindromic-subsequences/0730-count-different-palindromic-subsequences.py
+++ 0730-count-different-palindromic-subsequences/0730-count-different-palindromic-subsequences.py
@@ -1,16 +1,16 @@
 class Solution:
     def countPalindromicSubsequences(self, s: str) -> int:
         @cache
-        def helper(i=0, j=len(s) - 1):
+        def helper(i, j):
             if i > j: return 0
             ans = 0
             for char in "abcd":
-                l, r = s.find(char, i, j + 1), s.rfind(char, i, j + 1)
-                if l == -1: continue
-                ans = ans + helper(l + 1, r - 1) + 2 if l != r else ans + 1
+                left = s.find(char, i, j + 1)
+                right = s.rfind(char, i, j + 1)
+                if left == -1: continue
+                ans += helper(left + 1, right - 1) + 2 if left != right else 1
             return ans
-        MOD = 10 ** 9 + 7
-        return helper() % MOD 
 
+        return helper(0, len(s) - 1) % (10 ** 9 + 7)
 
         
