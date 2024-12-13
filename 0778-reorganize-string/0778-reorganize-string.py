efb4dd892b45508734381fbb3a93b563893a1730
Long Bian <bianlong0618@qq.com>
2024-12-13 15:56:16 -0500

    Time: 0 ms (100%), Space: 17.3 MB (12.11%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,17 +1,16 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        max_heap, no_adjcant, ans = [], [], ""
-        for char, freq in Counter(s).items(): heapq.heappush(max_heap, [-freq, char])
-        
+        max_heap, ans, prev = [], "", None
+        for char, freq in Counter(s).items():
+            heapq.heappush(max_heap, (-freq, char))
         while max_heap:
             freq, char = heapq.heappop(max_heap)
             ans, freq = ans + char, freq + 1
-            if no_adjcant:
-                next_freq, next_char = no_adjcant.pop()
-                heapq.heappush(max_heap, [next_freq, next_char])
+            if prev:
+                heapq.heappush(max_heap, prev)
+                prev = None
             if freq:
-                no_adjcant.append([freq, char]) 
-        return ans if not no_adjcant else ""
-
+                prev = (freq, char)
+        return ans if len(ans) == len(s) else ""
 
         
