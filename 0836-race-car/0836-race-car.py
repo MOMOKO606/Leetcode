043ae0b06eea741b6fd1e425baf29702f57fabf4
Long Bian <bianlong0618@qq.com>
2024-12-14 19:29:25 -0500

    Time: 7 ms (88.31%), Space: 18.2 MB (67.87%) - LeetHub

--- 0836-race-car/0836-race-car.py
+++ 0836-race-car/0836-race-car.py
@@ -1,16 +1,14 @@
 class Solution:
     def racecar(self, target: int) -> int:
-        deque = collections.deque([(0, 1, 0)])
-        while deque:
-            pos, speed, steps = deque.popleft()
-            if pos == target: return steps
-            deque.append((pos + speed, speed * 2, steps + 1))
-            if (speed > 0 and pos + speed > target) or (speed < 0 and pos + speed < target):
-                deque.append((pos, -speed / abs(speed), steps + 1))
-
+        queue, steps = [(0, 1)], 0
+        while queue:
+            next_queue = []
+            for pos, speed in queue:
+                if pos == target: return steps
+                next_queue.append((pos + speed, speed * 2))
+                if (pos + speed > target and speed > 0) or (pos + speed < target and speed < 0):
+                    next_queue.append((pos, -speed / abs(speed))) 
+            queue, steps = next_queue, steps + 1
         
-
-        
-
 
         
