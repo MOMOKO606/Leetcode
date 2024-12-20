582c54f18a3416d2af4b6e619c73302221e2cf42
Long Bian <bianlong0618@qq.com>
2024-12-20 13:16:02 -0500

    Time: 0 ms (100%), Space: 17.9 MB (10.13%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,5 +1,4 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        freq = collections.Counter(arr)
-        return len(freq.values()) == len(set(freq.values()))
+        return len(set(Counter(arr).values())) == len(set(arr))
         
