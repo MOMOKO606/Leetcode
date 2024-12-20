d3ffa32e2249f5e6e8e0692cd4483329b5f476cf
Long Bian <bianlong0618@qq.com>
2024-12-19 23:00:18 -0500

    Time: 144 ms (81.63%), Space: 18.3 MB (49.42%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,16 +1,18 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        if grid[0][0] or grid[rows - 1][cols - 1]: return -1
-        ans, queue = 1, [[0, 0]]
+        if grid[0][0] or grid[-1][-1]: return -1
+        rows, cols, queue, ans = len(grid), len(grid[0]), [[0, 0]], 1
         while queue:
             next_queue = []
             for i, j in queue:
                 if (i, j) == (rows - 1, cols - 1): return ans
-                for di, dj in [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]:
-                    if not (0 <= i + di < rows and 0 <= j + dj < cols and not grid[i + di][j + dj]): continue
-                    grid[i + di][j + dj] = 1
-                    next_queue.append([i + di, j + dj])
+                for x, y in [[i - 1, j - 1], [i, j - 1], [i + 1, j - 1], [i - 1, j], [i + 1, j], [i - 1, j + 1], [i, j + 1], [i + 1, j + 1]]:
+                    if not (0 <= x < rows and 0 <= y < cols and not grid[x][y]): continue
+                    grid[x][y] = 1
+                    next_queue.append([x, y])
             queue, ans = next_queue, ans + 1
         return -1
 
+
+
+        
