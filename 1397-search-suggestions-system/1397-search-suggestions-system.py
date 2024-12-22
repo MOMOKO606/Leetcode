10c5f131f1cfa227f423a63d72a8d78f8082a057
Long Bian <bianlong0618@qq.com>
2024-12-21 19:50:57 -0500

    Time: 367 ms (14.3%), Space: 23 MB (22.29%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.py
+++ 1397-search-suggestions-system/1397-search-suggestions-system.py
@@ -1,12 +1,36 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products, ans = sorted(products), []
+        def helper(node, cur_word):
+            if len(seq) >= 3: return 
+            if "#" in node: 
+                seq.append(cur_word)
+            for key in sorted(node.keys()):
+                if key == "#": continue
+                helper(node[key], cur_word + key)
+
+            
+        
+        
+        # Build the trie
+        trie = {}
+        for product in products:
+            node = trie
+            for char in product:
+                node[char] = node.get(char, {})
+                node = node[char]
+            node["#"] = "#"
+
+        ans = []
         for i, char in enumerate(searchWord):
-            suggestion = []
-            for product in products:
-                if i < len(product) and char == product[i]:
-                    suggestion.append(product)
-            ans.append(suggestion[:3])
-            products = suggestion
+            node, seq = trie, []
+            for j in range(i + 1):
+                if searchWord[j] in node: node = node[searchWord[j]]
+                else: break
+
+            else:
+                helper(node, searchWord[:i + 1])
+            ans.append(seq[:])
         return ans
+
+
         
