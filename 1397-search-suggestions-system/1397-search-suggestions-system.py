c9b5d84849dbf9afc9872e57fcde7ed3fd9b3d8d
Long Bian <bianlong0618@qq.com>
2024-12-21 19:52:01 -0500

    Time: 361 ms (14.44%), Space: 22.9 MB (22.29%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.py
+++ 1397-search-suggestions-system/1397-search-suggestions-system.py
@@ -1,7 +1,7 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         def helper(node, cur_word):
-            if len(seq) >= 3: return 
+            if len(seq) == 3: return 
             if "#" in node: 
                 seq.append(cur_word)
             for key in sorted(node.keys()):
@@ -24,12 +24,13 @@ class Solution:
         for i, char in enumerate(searchWord):
             node, seq = trie, []
             for j in range(i + 1):
-                if searchWord[j] in node: node = node[searchWord[j]]
-                else: break
-
+                if searchWord[j] not in node: 
+                    ans.append([])
+                    break
+                node = node[searchWord[j]]
             else:
                 helper(node, searchWord[:i + 1])
-            ans.append(seq[:])
+                ans.append(seq[:])
         return ans
 
 
