2eb038fce57479c2b7cb0c3a1889103e32e2b284
Long Bian <bianlong0618@qq.com>
2024-12-24 15:52:20 -0500

    Time: 745 ms (79.02%), Space: 37.8 MB (37.31%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,32 +1,33 @@
 class FenwickTree:
-    def __init__(self, size, value):
+    def __init__(self, value, size):
         self.size = size
-        self.ft = [value] * size
+        self.nums = [value] * size
 
-    def __lowbit(self, index):
+    def lowbit(self, index):
         return index & -index
 
     def update(self, index, delta):
         while index < self.size:
-            self.ft[index] += delta
-            index += self.__lowbit(index)
+            self.nums[index] += delta
+            index += self.lowbit(index)
+
 
     def query(self, index):
         ans = 0
         while index:
-            ans += self.ft[index]
-            index -= self.__lowbit(index)
+            ans += self.nums[index]
+            index -= self.lowbit(index)
         return ans
-
+        
 
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        transfer, ans = {num: i + 1 for i, num in enumerate(sorted(set(nums)))}, []
-        ft = FenwickTree(len(transfer) + 1, 0)
+        trans = {num: i + 1 for i, num in enumerate(sorted(list(set(nums))))}
+        ft, ans = FenwickTree(0, len(trans) + 1), []
         for num in reversed(nums):
-            i = transfer[num]
-            ans.append(ft.query(i - 1))
-            ft.update(i, 1)
+            k = trans[num]
+            ft.update(k, 1)
+            ans.append(ft.query(k - 1))
         return ans[::-1]
 
         
