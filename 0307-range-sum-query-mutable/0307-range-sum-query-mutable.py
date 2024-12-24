4fbb947dca385d613293f949372f3e27e34e9457
Long Bian <bianlong0618@qq.com>
2024-12-24 17:00:43 -0500

    Time: 524 ms (68.61%), Space: 36.1 MB (59.2%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,42 +1,42 @@
 class FenwickTree:
-    def __init__(self, size, value):
+    def __init__(self, value, size):
+        self.nums = [value] * size
         self.size = size
-        self.ft = [value] * size
 
-    def __lowbit(self, index):
+    def lowbit(self, index):
         return index & -index
 
     def update(self, index, delta):
         while index < self.size:
-            self.ft[index] += delta
-            index += self.__lowbit(index)
-
+            self.nums[index] += delta
+            index += self.lowbit(index)
+    
     def query(self, index):
         ans = 0
         while index:
-            ans += self.ft[index]
-            index -= self.__lowbit(index)
+            ans += self.nums[index]
+            index -= self.lowbit(index)
         return ans
 
 
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.ft = FenwickTree(len(nums) + 1, 0)
-        self.nums = nums
+        ft = FenwickTree(0, len(nums) + 1)
         for i, num in enumerate(nums):
-            self.ft.update(i + 1, num)
-
+            ft.update(i + 1, num)
+        self.ft, self.nums = ft, nums
+        
         
 
     def update(self, index: int, val: int) -> None:
         self.ft.update(index + 1, val - self.nums[index])
         self.nums[index] = val
-
         
 
+
     def sumRange(self, left: int, right: int) -> int:
-        return  self.ft.query(right + 1) - self.ft.query(left)
+        return self.ft.query(right + 1) - self.ft.query(left)
         
 
 
