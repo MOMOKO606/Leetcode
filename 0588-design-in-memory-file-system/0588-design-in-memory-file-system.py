c50b1f154e3ec358c9cfa42daf4f94dad9a4c9c5
Long Bian <bianlong0618@qq.com>
2024-12-23 23:26:33 -0500

    Time: 0 ms (100%), Space: 18.6 MB (6.14%) - LeetHub

--- 0588-design-in-memory-file-system/0588-design-in-memory-file-system.py
+++ 0588-design-in-memory-file-system/0588-design-in-memory-file-system.py
@@ -2,43 +2,38 @@ class FileSystem:
 
     def __init__(self):
         self.trie = {}
-        
 
     def ls(self, path: str) -> List[str]:
-        path = path.split("/")
-        path.pop(0)
-        node = self.trie
+        path, node, prev = path.split("/")[1:], self.trie, None
         for p in path:
-            if p in node: prev, node = p, node[p]
-        return sorted(list(node.keys())) if "#" not in node else [prev]
-        
+            if p in node:
+                node, prev = node[p], p
+        if "#" in node: return [prev]
+        return sorted(list(node.keys()))
+
 
     def mkdir(self, path: str) -> None:
-        path = path.split("/")
-        path.pop(0)
-        node = self.trie
+        path, node = path.split("/")[1:], self.trie
         for p in path:
             node[p] = node.get(p, {})
             node = node[p]
         
 
     def addContentToFile(self, filePath: str, content: str) -> None:
-        filePath = filePath.split("/")
-        filePath.pop(0)
-        node = self.trie
+        filePath, node = filePath.split("/")[1:], self.trie
         for p in filePath:
             node[p] = node.get(p, {})
             node = node[p]
         node["#"] = node.get("#", "") + content
+        
 
     def readContentFromFile(self, filePath: str) -> str:
-        filePath = filePath.split("/")
-        filePath.pop(0)
-        node = self.trie
+        filePath, node = filePath.split("/")[1:], self.trie
         for p in filePath:
             node[p] = node.get(p, {})
             node = node[p]
         return node["#"]
+        
 
 
 # Your FileSystem object will be instantiated and called as such:
