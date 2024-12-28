f0ca398e922c33b4549a775708281b479d077932
Long Bian <bianlong0618@qq.com>
2024-12-28 17:38:27 -0500

    Time: 137 ms (64.28%), Space: 36.8 MB (19.87%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
@@ -3,14 +3,13 @@ class Solution:
         largest, smallest, buckets = max(nums), min(nums), {}
         for j, num in enumerate(nums):
             if j > indexDiff:
-                i = j - indexDiff - 1
-                del buckets[(nums[i] - smallest) // (valueDiff + 1)]
+                del buckets[(nums[j - indexDiff - 1] - smallest) // (valueDiff + 1)]
             k = (num - smallest) // (valueDiff + 1)
             if k in buckets: return True
-            if k - 1 in buckets and abs(buckets[k - 1] - num) <= valueDiff: return True
-            if k + 1 in buckets and abs(buckets[k + 1] - num) <= valueDiff: return True
+            if k - 1 in buckets and num - buckets[k - 1] <= valueDiff: return True
+            if k + 1 in buckets and buckets[k + 1] - num <= valueDiff: return True
             buckets[k] = num
-        return False
+        return False 
 
 
         
