84837b3577e13331d534b8f8c0881eafff19ceac
Long Bian <bianlong0618@qq.com>
2024-12-31 17:55:47 -0500

    Time: 39 ms (32.12%), Space: 24.7 MB (11.22%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,32 +1,32 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        def parent(i):
-            root = i
-            while p[root] != root:
-                root = p[root]
-            while p[i] != i:
-                p[i], i = root, p[i]
-            return root
+        def dfs_helper(i):
+            if i in id_visited: return []
+            id_visited.add(i)
+            emails = []
+            for email in accounts[i][1:]:
+                if email in email_visited: continue
+                email_visited.add(email)
+                emails.append(email)
+                for neighbor in graph[email]:
+                    emails += dfs_helper(neighbor)
+            return emails
 
-        def connect(i, j):
-            pi, pj = parent(i), parent(j)
-            p[pi] = pj
-        
-        p, email_to_id, id_to_email = [i for i in range(len(accounts))], {}, collections.defaultdict(set)
+
+
+        # Build the graph
+        graph = collections.defaultdict(list)
         for i, account in enumerate(accounts):
             for email in account[1:]:
-                if email in email_to_id:
-                    connect(email_to_id[email], i)
-                else:
-                    email_to_id[email] = i
+                graph[email].append(i)
 
+        id_visited, email_visited, ans = set(), set([]), []
         for i, account in enumerate(accounts):
-            pi = parent(i)
-            for email in account[1:]:
-                id_to_email[pi].add(email)
-        return [[accounts[i][0]] + sorted(emails) for i, emails in id_to_email.items()]        
-
-
+            if i in id_visited: continue
+            name = account[0]
+            emails = dfs_helper(i)
+            ans.append([name] + sorted(emails))
+        return ans
 
 
         
