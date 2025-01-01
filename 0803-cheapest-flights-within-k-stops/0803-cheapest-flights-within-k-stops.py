85fee1aaacfcb7436768174d87d7844443af1904
Long Bian <bianlong0618@qq.com>
2025-01-01 10:26:06 -0500

    Time: 63 ms (33.73%), Space: 19.9 MB (5.07%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -2,19 +2,20 @@ class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         # Build the graph
         graph = collections.defaultdict(dict)
-        for u, v, price in flights:
-            graph[u][v] = price
+        for u, v, w in flights:
+            graph[u][v] = w
 
-        queue, k, ans, visited = [(src, 0)],  k + 1, inf, [inf] * n
+        queue, ans, k, visited = [(src, 0)], inf, k + 2, set([src, 0])
         while queue and k:
             next_queue = []
-            for node, cur_cost in queue:
+            for node, price in queue:
+                if node == dst: 
+                    ans = min(ans, price)
                 for neighbor in graph[node]:
-                    if cur_cost + graph[node][neighbor] > visited[neighbor]: continue
-                    visited[neighbor] = cur_cost + graph[node][neighbor]
-                    next_queue.append((neighbor, cur_cost + graph[node][neighbor]))
-            queue, k = next_queue, k - 1
-        return visited[dst] if visited[dst] != inf else -1
-
-
+                    if price + graph[node][neighbor] > ans or (neighbor, price + graph[node][neighbor]) in visited: continue
+                    next_queue.append((neighbor, price + graph[node][neighbor]))
+                    visited.add((neighbor, price + graph[node][neighbor]))
+                    
+            k, queue = k - 1, next_queue
+        return ans if ans != inf else -1
         
