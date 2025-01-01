8607c374a6e497bcc6a8fcbc82d0a92423ba2f6b
Long Bian <bianlong0618@qq.com>
2025-01-01 13:10:33 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0897-prime-palindrome/0897-prime-palindrome.py
+++ 0897-prime-palindrome/0897-prime-palindrome.py
@@ -1,16 +1,16 @@
 class Solution:
     def primePalindrome(self, n: int) -> int:
         def is_prime(num):
-            for divisor in range(2, int(num ** 0.5) + 1):
+            for divisor in range(2, ceil(num ** 0.5) + 1):
                 if not num % divisor: return False
             return True
 
         if n < 3: return 2
-        if 7 < n <= 11: return 11
-        i = 2
+        if 7 < n < 11: return 11
+        i = 1
         while True:
-            num = int(str(i) + str(i)[::-1][1:])
-            if num >= n and is_prime(num):
+            num = int(str(i) + str(i)[:-1][::-1])
+            if num > n and is_prime(num):
                 return num
             i += 1
         
