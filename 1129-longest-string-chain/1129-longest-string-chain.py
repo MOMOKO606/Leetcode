4f0cf9405ef6aa423d24d16fdad3190c7e1f66b8
Long Bian <bianlong0618@qq.com>
2025-01-03 16:11:00 -0500

    Time: 42 ms (96.82%), Space: 18.2 MB (40.85%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.py
+++ 1129-longest-string-chain/1129-longest-string-chain.py
@@ -1,18 +1,12 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        @cache
-        def dfs_helper(word):
-            if word not in word_set: return -inf
-            ans = 1
+        words, visited, ans = sorted(words, key=len), {word: 1 for word in words}, 1
+        for word in words:
             for i in range(len(word)):
                 predecessor = word[:i] + word[i + 1:]
-                ans = max(ans, 1 + dfs_helper(predecessor))
-            return ans
-
-
-        words, ans, word_set = sorted(words, key=len, reverse=True), 1, set(words)
-        for word in words:
-            ans = max(ans, dfs_helper(word))
+                if predecessor in visited:
+                    visited[word] = max(visited[word], visited[predecessor] + 1)
+            ans = max(ans, visited[word])
         return ans
 
         
