1ef6373ebe59ecbffad1c44957b3f526da8336ae
Long Bian <bianlong0618@qq.com>
2025-01-03 14:14:28 -0500

    Time: 1059 ms (10.57%), Space: 18 MB (43.11%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.py
+++ 1129-longest-string-chain/1129-longest-string-chain.py
@@ -1,14 +1,15 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        chains, ans, words = {word: 1 for word in words}, 1, sorted(words, key=len)
+        words, visited, ans = sorted(words, key=len, reverse=True), set(), 1
         for word in words:
-            for i in range(len(word)):
-                predecessor = word[:i] + word[i + 1:]
-                if predecessor in chains:
-                    chains[word] = max(chains[word], chains[predecessor] + 1)
-            ans = max(ans, chains[word])
+            if word in visited: continue
+            deque = collections.deque([(word, 1)])
+            while deque:
+                node, count = deque.popleft()
+                ans = max(ans, count)
+                for i in range(len(node)):
+                    predecessor = node[:i] + node[i + 1:]
+                    if predecessor in words and predecessor not in visited:
+                        deque.append((predecessor, count + 1))
+                        visited.add(predecessor)
         return ans
-
-
-
-        
