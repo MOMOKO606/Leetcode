e0ab2696321c571284288be3a2ec628310d6efba
Long Bian <bianlong0618@qq.com>
2025-01-04 17:25:30 -0500

    Time: 132 ms (95.17%), Space: 47.2 MB (61.37%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,20 +1,22 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        graph = collections.defaultdict(dict)
         # Build the graph
-        edge = collections.defaultdict(list)
         for u, v in connections:
-            edge[u].append((v, 1))
-            edge[v].append((u, 0))
-        # BFS
+            graph[u][v] = 1
+            graph[v][u] = 0
+        
         queue, ans, visited = [0], 0, set([0])
         while queue:
-            nextQueue = []
-            for u in queue:
-                for v, weight in edge[u]: 
-                    if v not in visited:
-                        ans += weight
-                        visited.add(v)
-                        nextQueue.append((v))
-            queue = nextQueue
+            next_queue = []
+            for node in queue:
+                for neighbor in graph[node]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        next_queue.append(neighbor)
+                        ans += graph[node][neighbor]
+            queue = next_queue
         return ans
+
+
         
