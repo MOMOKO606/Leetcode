3fdb810f00839a153f75f47eb8b403684f332ce1
Long Bian <bianlong0618@qq.com>
2025-01-04 18:00:48 -0500

    Time: 320 ms (34.96%), Space: 40.4 MB (5.48%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,20 +1,21 @@
 from collections import deque
 
-
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        ans, i, minQueue, maxQueue = 0, -1, deque([]), deque([])
+        max_window, min_window, ans, i = deque([]), deque([]), 0, -1
         for j in range(len(nums)):
-            while maxQueue and nums[j] > maxQueue[-1][0]:
-                maxQueue.pop()
-            maxQueue.append((nums[j], j))
-            while minQueue and nums[j] < minQueue[-1][0]:
-                minQueue.pop()
-            minQueue.append((nums[j], j))
-            while maxQueue[0][0] - minQueue[0][0] > limit:
+            while max_window and max_window[-1][0] < nums[j]:
+                max_window.pop()
+            max_window.append([nums[j], j])
+            while min_window and min_window[-1][0] > nums[j]:
+                min_window.pop()
+            min_window.append([nums[j], j])
+            while max_window[0][0] - min_window[0][0] > limit:
                 i += 1
-                while maxQueue[0][1] <= i: maxQueue.popleft()
-                while minQueue[0][1] <= i: minQueue.popleft()
+                while i >= max_window[0][1]:
+                    max_window.popleft()
+                while i >= min_window[0][1]:
+                    min_window.popleft() 
             ans = max(ans, j - i)
         return ans
 
