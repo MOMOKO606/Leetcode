ae8112e6bee0334221962199f63a8cf6aea0db4a
Long Bian <bianlong0618@qq.com>
2025-01-05 09:56:41 -0500

    Time: 1638 ms (10.25%), Space: 19.2 MB (43.44%) - LeetHub

---
+++ 1571-allocate-mailboxes/1571-allocate-mailboxes.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minDistance(self, houses: List[int], k: int) -> int:
+        def cost(i, j):
+            mid = (i + j) // 2
+            return sum(abs(houses[k] - houses[mid]) for k in range(i, j + 1))
+
+        @cache
+        def helper(i, k):
+            if i == len(houses) and not k: return 0
+            if i == len(houses) or not k: return inf
+            ans = inf
+            for j in range(i, len(houses)):
+                ans = min(ans, helper(j + 1, k - 1) + cost(i, j))
+            return ans
+
+        houses = sorted(houses)
+        return helper(0, k)
+
+
+        
