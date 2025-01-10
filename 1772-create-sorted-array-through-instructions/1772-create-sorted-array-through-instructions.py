03384924512267dbf355e0c0d26018fe3ebf756b
Long Bian <bianlong0618@qq.com>
2025-01-09 23:25:21 -0500

    Time: 2709 ms (64.23%), Space: 40.1 MB (17.89%) - LeetHub

--- 1772-create-sorted-array-through-instructions/1772-create-sorted-array-through-instructions.py
+++ 1772-create-sorted-array-through-instructions/1772-create-sorted-array-through-instructions.py
@@ -1,33 +1,31 @@
 class FenwickTree:
     def __init__(self, value, size):
+        self.nums = [value] * size
         self.size = size
-        self.ft = [value] * size
 
-    def lowbit(self, index):
+    def _lowbit(self, index):
         return index & -index
 
-    def update(self, i, delta):
-        while i < self.size:
-            self.ft[i] += delta
-            i += self.lowbit(i)
+    def update(self, index, delta):
+        while index < self.size:
+            self.nums[index] += delta
+            index += self._lowbit(index)
     
-    def query(self, i):
+    def query(self, index):
         ans = 0
-        while i:
-            ans += self.ft[i]
-            i -= self.lowbit(i)
+        while index:
+            ans += self.nums[index]
+            index -= self._lowbit(index)
         return ans
-
 
 class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
-        trans = {num: i + 1 for i, num in enumerate(sorted(list(set(instructions))))}
-        ft, ans = FenwickTree(0, len(trans) + 1), 0
+        transfer = {num: i + 1 for i, num in enumerate(sorted(set(instructions)))}
+        ft, ans = FenwickTree(value=0, size=len(transfer) + 1), 0
         for i, num in enumerate(instructions):
-            k = trans[num]
+            k = transfer[num]
             ft.update(k, 1)
             ans += min(ft.query(k - 1), i + 1 - ft.query(k))
         return ans % (10 ** 9 + 7)
-
 
         
