c94bbc31c8c2c2e1704939b2860df10d992ce81b
Long Bian <bianlong0618@qq.com>
2025-01-14 11:17:29 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2213-find-all-people-with-secret/2213-find-all-people-with-secret.py
+++ 2213-find-all-people-with-secret/2213-find-all-people-with-secret.py
@@ -3,28 +3,27 @@ from itertools import groupby
 
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        def parent(i):
-            root = i
-            while p[root] != root:
-                root = p[root]
-            while p[i] != i:
-                p[i], i = root, p[i]
-            return root
+        def dfsHelper(node):
+            if node in visited: return
+            visited.add(node)
+            for neighbor in graph[node]:
+                dfsHelper(neighbor)
 
-        def connect(i, j):
-            pi, pj = parent(i), parent(j)
-            p[pi] = pj
-
-        p = [i for i in range(n)]
-        connect(0, firstPerson)
+        known = set([0, firstPerson])
         for _, group in groupby(sorted(meetings, key=lambda x: x[2]), key=lambda x: x[2]):
-            visited = set([])
+            graph, visited = collections.defaultdict(list), set([])
             for u, v, _ in group:
-                connect(u, v)
-                visited.add(u)
-                visited.add(v)
+                graph[u].append(v)
+                graph[v].append(u)
+
+            for node in known:
+                dfsHelper(node)
+
             for node in visited:
-                if parent(node) != parent(0):
-                    p[node] = node
-        return [i for i in range(n) if parent(i) == parent(0)]
+                known.add(node)
+        return list(known)
+                
+
+            
+
         
