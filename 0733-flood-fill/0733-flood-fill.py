790f4d19e9a7758d87751e50ff7c60e1830fb0ea
Long Bian <bianlong0618@qq.com>
2025-01-16 17:19:07 -0500

    Time: 0 ms (100%), Space: 18.1 MB (12.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,19 +1,15 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        def dfs_helper(i, j):
-            if not (0 <= i < rows and 0 <= j < cols and image[i][j] == ori_color): return
-            image[i][j] = "#"
-            for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
-                dfs_helper(x, y)
-
-
-
-        rows, cols, ori_color = len(image), len(image[0]), image[sr][sc]
-        dfs_helper(sr, sc)
-        for i in range(rows):
-            for j in range(cols):
-                if image[i][j] == "#":
-                    image[i][j] = color
+        rows, cols, ori_color, queue, visited, image[sr][sc] = len(image), len(image[0]), image[sr][sc], [(sr, sc)], set((sr, sc)), color
+        while queue:
+            next_queue = []
+            for i, j in queue:
+                for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
+                    if 0 <= x < rows and 0 <= y < cols and image[x][y] == ori_color and (x, y) not in visited:
+                        image[x][y] = color
+                        visited.add((x, y))
+                        next_queue.append([x, y])
+            queue = next_queue
         return image
 
         
