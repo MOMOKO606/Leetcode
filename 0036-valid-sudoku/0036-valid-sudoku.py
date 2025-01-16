c2de487a6665fedc6cb9bba0844e5e8671d67bd9
Long Bian <bianlong0618@qq.com>
2025-01-16 14:57:30 -0500

    Time: 3 ms (83.43%), Space: 17.8 MB (39.03%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -3,15 +3,13 @@ class Solution:
         rows = [set([i for i in range(1, 10)]) for _ in range(9)]
         cols = [set([i for i in range(1, 10)]) for _ in range(9)]
         blocks = [set([i for i in range(1, 10)]) for _ in range(9)]
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == ".": continue
-                num, k = int(board[i][j]), (i // 3) * 3 + j // 3
-                if num not in rows[i] & cols[j] & blocks[k]: return False
+                k, num = (i // 3) * 3 + j // 3, int(board[i][j])
+                if not (num in rows[i] and num in cols[j] and num in blocks[k]): return False
                 rows[i].remove(num)
                 cols[j].remove(num)
                 blocks[k].remove(num)
         return True
-                
-
-        
