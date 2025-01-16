58c0907fad3ac6a860d55e4000ddaf5b9b780471
Long Bian <bianlong0618@qq.com>
2025-01-16 12:59:48 -0500

    Time: 4178 ms (59.12%), Space: 18 MB (19.47%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,24 +2,24 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def helper(i, j, node):
             if "#" in node: return True
-            if not (0 <= i < rows and 0 <= j < cols and board[i][j] in node): return False
+            if  not (0 <= i < rows and 0 <= j < cols and board[i][j] in node): return False
             ori = board[i][j]
             board[i][j] = "$"
-            for x, y in [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]:
+            for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
                 if helper(x, y, node[ori]): return True
             board[i][j] = ori
+            
 
+        
         # Build the trie
-        node = trie = {}
+        trie = node = {}
         for char in word:
             node[char] = node.get(char, {})
             node = node[char]
         node["#"] = "#"
-
+        
         rows, cols = len(board), len(board[0])
         for i in range(rows):
             for j in range(cols):
                 if helper(i, j, trie): return True
         return False
-
-        
