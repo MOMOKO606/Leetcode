28986b7949629959d1b6248cd2895ec2f77640a0
Long Bian <bianlong0618@qq.com>
2025-01-16 17:13:54 -0500

    Time: 4 ms (6.35%), Space: 18.3 MB (5.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,16 +1,19 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]: 
-        target, queue,  rows, cols = image[sr][sc], [[sr, sc]], len(image), len(image[0])
-        if target == color: return image
-        image[sr][sc] = color
-        while queue:
-            next_queue = []
-            for i, j in queue:
-                for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
-                    if not (0 <= x < rows and 0 <= y < cols and image[x][y] == target): continue
-                    next_queue.append([x, y])
-                    image[x][y] = color
-            queue = next_queue
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        def dfs_helper(i, j):
+            if not (0 <= i < rows and 0 <= j < cols and image[i][j] == ori_color): return
+            image[i][j] = "#"
+            for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
+                dfs_helper(x, y)
+
+
+
+        rows, cols, ori_color = len(image), len(image[0]), image[sr][sc]
+        dfs_helper(sr, sc)
+        for i in range(rows):
+            for j in range(cols):
+                if image[i][j] == "#":
+                    image[i][j] = color
         return image
 
         
