46e5b6b3b34a37718ae2c76519ed1639f517d353
Long Bian <bianlong0618@qq.com>
2025-01-16 21:38:56 -0500

    Time: 12 ms (82.5%), Space: 18 MB (73.5%) - LeetHub

---
+++ 1757-minimum-jumps-to-reach-home/1757-minimum-jumps-to-reach-home.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
+        deque, forbidden, boundary = collections.deque([(0, 0, 0)]), set(forbidden), max(max(forbidden) + a + b, x + b)
+        while deque:
+            pos, prev, steps = deque.popleft()
+            if pos == x: return steps
+            if pos + a not in forbidden and pos + a <= boundary:
+                deque.append([pos + a, a, steps + 1])
+                forbidden.add(pos + a)
+            if prev > 0 and pos - b >= 0 and pos - b not in forbidden:
+                deque.append([pos - b, -b, steps + 1])
+                # forbidden.add(pos - b)
+        return -1
+           
+
+        
