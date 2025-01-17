d5908f01bd9118e2bcea0a773afcf1ca5ddf5732
Long Bian <bianlong0618@qq.com>
2025-01-17 14:00:01 -0500

    Time: 0 ms (100%), Space: 18.2 MB (15.14%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,28 +1,25 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        def dfsHelper(u, v, visited):
+        def dfs_helper(u, v, visited):
             if u not in graph or u in visited: return -1.0
             if u == v: return 1.0
             visited.add(u)
             for neighbor in graph[u]:
-                next = dfsHelper(neighbor, v, visited)
-                if next != -1:
-                    return graph[u][neighbor] * next
+                rest = dfs_helper(neighbor, v, visited)
+                if rest != -1.0: 
+                    return graph[u][neighbor] * rest
+            visited.remove(u)
             return -1.0
-        
-        
+
         # Build the graph
         graph = collections.defaultdict(dict)
-        for (u, v), w in zip(equations, values):
-            graph[u][v] = w
-            graph[v][u] = 1 / w
+        for e, v in zip(equations, values):
+            graph[e[0]][e[1]] = v
+            graph[e[1]][e[0]] = 1 / v
 
         ans = []
         for u, v in queries:
-            ans.append(dfsHelper(u, v, set()))
+            ans.append(dfs_helper(u, v, set()))
         return ans
 
-        
-
-        
-        
+            
