3ef25dd85e32464bde857402f1b9d246231d2565
Long Bian <bianlong0618@qq.com>
2025-01-16 23:08:26 -0500

    Time: 751 ms (82.16%), Space: 18.6 MB (91.41%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,18 +1,18 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        def dfsHelper(i, j, node, seq):
-            if "#" in node:
-                ans.append(seq)
+        def dfs_helper(i, j, seq, node):
+            if "#" in node: 
+                self.ans.append(seq[:])
                 del node["#"]
             if not (0 <= i < rows and 0 <= j < cols and board[i][j] in node): return
             ori = board[i][j]
             board[i][j] = "$"
             for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
-                dfsHelper(x, y, node[ori], seq + ori)
+                dfs_helper(x, y, seq + ori, node[ori])
             board[i][j] = ori
             if not node[ori]: del node[ori]
-                
-
+            
+        
         # Build the trie
         trie = {}
         for word in words:
@@ -22,8 +22,9 @@ class Solution:
                 node = node[char]
             node["#"] = "#"
 
-        rows, cols, ans = len(board), len(board[0]), []
+        rows, cols, self.ans = len(board), len(board[0]), []
         for i in range(rows):
             for j in range(cols):
-                dfsHelper(i, j, trie, "")
-        return ans
+                dfs_helper(i, j, "", trie)
+        return self.ans
+        
