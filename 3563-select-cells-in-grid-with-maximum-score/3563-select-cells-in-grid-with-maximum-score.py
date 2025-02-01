b75172a228ed6d41a94b65e13cee1b414c5f87be
Long Bian <bianlong0618@qq.com>
2025-01-31 22:54:38 -0500

    Time: 4604 ms (5.31%), Space: 18 MB (68.14%) - LeetHub

--- 3563-select-cells-in-grid-with-maximum-score/3563-select-cells-in-grid-with-maximum-score.py
+++ 3563-select-cells-in-grid-with-maximum-score/3563-select-cells-in-grid-with-maximum-score.py
@@ -1,19 +1,11 @@
 class Solution:
     def maxScore(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        # flatten all the values in the grid and deduplicate
-        value_set = set([grid[r][c] for r in range(rows) for c in range(cols)])
-        
-        # sort values from in descending order
-        value_list = sorted(list(value_set))[::-1]
+        rows, cols, val_to_rows = len(grid), len(grid[0]), defaultdict(set)
+        for i in range(rows):
+            for j in range(cols):
+                val_to_rows[grid[i][j]].add(i)
 
-        # create a map mapping from each value to the list of row indexes with these values
-        val_to_rows = defaultdict(list)
-        for value in value_list:
-            val_to_rows[value] = [r for r in range(rows) if value in grid[r]]
-
-        # Run dfs on rows with cell values in descending order.
+  
         def dfs(row_set, remaining_values, score):
             if not remaining_values:
                 return score
@@ -30,5 +22,4 @@ class Solution:
                 score_list.append(dfs(row_set, remaining_values, score))
             return max(score_list)
 
-        return dfs(set(), value_list, 0)
-        
+        return dfs(set(), sorted(list(val_to_rows.keys()), reverse=True), 0)
