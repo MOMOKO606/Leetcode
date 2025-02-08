c77cd91d3cad57338bb13afad27848ab7a712e07
Long Bian <bianlong0618@qq.com>
2025-02-07 22:54:16 -0500

    Time: 1191 ms (31.86%), Space: 18.1 MB (73.08%) - LeetHub

--- 3395-minimum-length-of-anagram-concatenation/3395-minimum-length-of-anagram-concatenation.py
+++ 3395-minimum-length-of-anagram-concatenation/3395-minimum-length-of-anagram-concatenation.py
@@ -4,8 +4,10 @@ class Solution:
         for l in range(1, n + 1):
             if n % l: continue
             target = Counter(s[:l])
-            for j in range(l, n, l):
-                if Counter(s[j: j + l]) != target: break
-            else:
-                return l
+            for i in range(l, n, l):
+                if Counter(s[i: i + l]) != target: break
+            else: return l
+
+
+
         
