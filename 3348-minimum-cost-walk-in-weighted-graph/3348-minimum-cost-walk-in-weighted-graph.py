e5454fc55bed864f7886813f98505f5c2a419849
Long Bian <bianlong0618@qq.com>
2025-02-12 14:20:39 -0500

    Time: 217 ms (36.59%), Space: 92.6 MB (65.85%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.py
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.py
@@ -2,29 +2,31 @@ class Solution:
     def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
         def parent(i):
             root = i
-            while root != p[root]:
+            while p[root] != root:
                 root = p[root]
-            while i != p[i]:
+            while p[i] != i:
                 p[i], i = root, p[i]
             return root
-        
+
         def connect(i, j):
             pi, pj = parent(i), parent(j)
             p[pi] = pj
-        
+
         p, ans = [i for i in range(n)], []
         for u, v, _ in edges:
             connect(u, v)
-            
-        root2weight = {parent(i): -1 for i in range(n)}
-        for u, v, w in edges:
-            root2weight[parent(u)] &= w
 
+        roots = {parent(i): -1 for i in range(n)}
+        for u, v, w in edges:
+            roots[parent(u)] &= w
+            
         for u, v in query:
             pu, pv = parent(u), parent(v)
-            if pu == pv: ans.append(root2weight[pu])
+            if pu == pv: ans.append(roots[pu])
             else: ans.append(-1)
-
         return ans
-        
+
+
+
+
         
