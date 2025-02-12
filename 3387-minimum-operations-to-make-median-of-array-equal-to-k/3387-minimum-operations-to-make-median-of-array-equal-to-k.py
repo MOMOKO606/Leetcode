cfa14b5b4d376a9969cc657a91001d6985afbb2a
Long Bian <bianlong0618@qq.com>
2025-02-12 14:47:07 -0500

    Time: 87 ms (43.06%), Space: 41.4 MB (10.53%) - LeetHub

--- 3387-minimum-operations-to-make-median-of-array-equal-to-k/3387-minimum-operations-to-make-median-of-array-equal-to-k.py
+++ 3387-minimum-operations-to-make-median-of-array-equal-to-k/3387-minimum-operations-to-make-median-of-array-equal-to-k.py
@@ -1,19 +1,9 @@
 class Solution:
     def minOperationsToMakeMedianK(self, nums: List[int], k: int) -> int:
-        pivot, nums = len(nums) // 2, sorted(nums)
-        ans = abs(nums[pivot] - k)
-        for j in range(pivot + 1, len(nums)):
-            if nums[j] < k: 
-                ans += k - nums[j] 
-            else: break
-        for i in range(pivot - 1, -1, -1):
-            if nums[i] > k:
-                ans += nums[i] - k
-            else: break
+        mid, nums = len(nums) // 2, sorted(nums)
+        ans, i, j = abs(nums[mid] - k), mid - 1, mid + 1
+        while i >= 0 and nums[i] > k:
+            i, ans = i - 1, ans + abs(nums[i] - k)
+        while j < len(nums) and nums[j] < k:
+            j, ans = j + 1, ans + abs(nums[j] - k)
         return ans
-
-
-        
-
-
-        
