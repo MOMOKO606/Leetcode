1ebe2cbbd5a472d8bef097deb7d7fb926601ec62
Long Bian <bianlong0618@qq.com>
2025-02-18 20:29:30 -0500

    Time: 7 ms (73.94%), Space: 18.3 MB (14.49%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,9 +1,9 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        for i, pos in enumerate(flowerbed):
-            if pos: continue
-            if (i == 0 or not flowerbed[i - 1]) and (i == len(flowerbed) - 1 or not flowerbed[i + 1]):
-                flowerbed[i], n = 1, n - 1
-            
-        return n <= 0
+        if not n: return True
+        for i, num in enumerate(flowerbed):
+            if num: continue
+            if (i == 0 or not flowerbed[i - 1]) and (i == len(flowerbed) - 1 or not flowerbed[i + 1]): n, flowerbed[i] = n - 1, 1
+            if not n: return True
+        return False
         
