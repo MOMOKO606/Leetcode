6c7161a754e3e50d174b6533e2be64f52d120d49
Long Bian <bianlong0618@qq.com>
2025-02-19 16:11:35 -0500

    Time: 1852 ms (48.81%), Space: 30 MB (30.02%) - LeetHub

--- 0336-palindrome-pairs/0336-palindrome-pairs.py
+++ 0336-palindrome-pairs/0336-palindrome-pairs.py
@@ -1,14 +1,15 @@
 class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
-        indexes, ans = {word: i for i, word in enumerate(words)}, set()
+        words_set, visited, ans = {word: i for i, word in enumerate(words)}, set([]), []
         for i, word in enumerate(words):
             for j in range(len(word) + 1):
                 left, right = word[:j], word[j:]
-                if right[::-1] in indexes and indexes[right[::-1]] != i and left == left[::-1]:
-                    ans.add((indexes[right[::-1]], i))
-                if left[::-1] in indexes and i != indexes[left[::-1]] and right == right[::-1]:
-                    ans.add((i, indexes[left[::-1]]))
-        return list(ans)
-            
+                if right == right[::-1] and left[::-1] in words_set and i != words_set[left[::-1]] and (i, words_set[left[::-1]]) not in visited:
+                    visited.add((i, words_set[left[::-1]]))
+                    ans.append([i, words_set[left[::-1]]])
+                if left == left[::-1] and right[::-1] in words_set and i != words_set[right[::-1]] and (words_set[right[::-1]], i) not in visited:
+                    visited.add((words_set[right[::-1]], i))
+                    ans.append([words_set[right[::-1]], i])
+        return ans
 
         
