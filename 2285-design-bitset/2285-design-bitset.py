78243c62a86ab6df058f4d03e59a6c4e00efb18f
Long Bian <bianlong0618@qq.com>
2025-02-20 18:12:40 -0500

    Time: 144 ms (91.84%), Space: 51.4 MB (58.5%) - LeetHub

--- 2285-design-bitset/2285-design-bitset.py
+++ 2285-design-bitset/2285-design-bitset.py
@@ -2,39 +2,36 @@ class Bitset:
 
     def __init__(self, size: int):
         self.bits = [0] * size
+        self.size = size
         self.flipped = False
-        self.ones, self.n = 0, size
+        self.ones = 0
         
 
     def fix(self, idx: int) -> None:
         if not self.flipped:
             if not self.bits[idx]: 
-                self.bits[idx] = 1
-                self.ones += 1
+                self.bits[idx], self.ones = 1, self.ones + 1
         else:
             if self.bits[idx]: 
-                self.bits[idx] = 0
-                self.ones += 1
+                self.bits[idx], self.ones = 0, self.ones + 1
 
-        
+
     def unfix(self, idx: int) -> None:
         if not self.flipped:
-            if self.bits[idx]: 
-                self.bits[idx] = 0
-                self.ones -= 1
+            if self.bits[idx]:
+                self.bits[idx], self.ones = 0, self.ones - 1
         else:
-            if not self.bits[idx]: 
-                self.bits[idx] = 1
-                self.ones -= 1
-            
+            if not self.bits[idx]:
+                self.bits[idx], self.ones = 1, self.ones - 1
+        
 
     def flip(self) -> None:
         self.flipped = not self.flipped
-        self.ones = self.n - self.ones
-        
+        self.ones = self.size - self.ones
+
 
     def all(self) -> bool:
-        return self.ones == self.n
+        return self.ones == self.size
         
 
     def one(self) -> bool:
@@ -46,16 +43,14 @@ class Bitset:
         
 
     def toString(self) -> str:
-        ans = ""
         if not self.flipped:
-            for bit in self.bits:
-                ans += str(bit)
+            return "".join(map(str, self.bits))
         else:
+            ans = ""
             for bit in self.bits:
-                ans = ans + "1" if not bit else ans + "0"
-        return ans
-
-        
+                if bit: ans += "0"
+                else: ans += "1"
+            return ans
 
 
 # Your Bitset object will be instantiated and called as such:
