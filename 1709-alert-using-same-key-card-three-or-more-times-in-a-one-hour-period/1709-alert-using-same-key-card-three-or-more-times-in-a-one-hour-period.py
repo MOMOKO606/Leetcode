f529c1d8cc3e11bd19c0fa746f067b3736339012
Long Bian <bianlong0618@qq.com>
2025-02-20 20:02:16 -0500

    Time: 123 ms (60.71%), Space: 40.7 MB (37.33%) - LeetHub

--- 1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
+++ 1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
@@ -1,16 +1,17 @@
 class Solution:
     def alertNames(self, keyName: List[str], keyTime: List[str]) -> List[str]:
-        logs, ans = collections.defaultdict(list), []
+        freqs, ans = collections.defaultdict(list), []
         for name, time in zip(keyName, keyTime):
-            time = int(time[:2]) * 60 + int(time[3:])
-            bisect.insort(logs[name], time)
-        
-        for name, times in logs.items():
+            time = time.split(":")
+            bisect.insort(freqs[name], int(time[0]) * 60 + int(time[1]))
+
+        for name, times in freqs.items():
             for i in range(len(times) - 2):
-                if i + 2 < len(times) and times[i + 2] - times[i] <= 60:
+                if times[i + 2] <= times[i] + 60:
                     ans.append(name)
                     break
         return sorted(ans)
-            
+
+        
 
         
