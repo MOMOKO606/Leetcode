735d4a35198c8c453beb1c4b4c695959a9d07610
Long Bian <bianlong0618@qq.com>
2025-02-20 23:14:46 -0500

    Time: 276 ms (60.79%), Space: 17.8 MB (72.78%) - LeetHub

--- 0465-optimal-account-balancing/0465-optimal-account-balancing.py
+++ 0465-optimal-account-balancing/0465-optimal-account-balancing.py
@@ -1,23 +1,23 @@
 class Solution:
     def minTransfers(self, transactions: List[List[int]]) -> int:
-        def dfs_helper(i):
+        def dfs_helper(i=0):
             while i < len(nums) and not nums[i]:
                 i += 1
-            if i == len(accounts): return 0
+            if i == len(nums): return 0
             ans = inf
-            for j in range(i + 1, len(accounts)):
-                if nums[i] * nums[j] < 0:
+            for j in range(i + 1, len(nums)):
+                if nums[j] * nums[i] < 0:
                     nums[j] += nums[i]
                     ans = min(ans, 1 + dfs_helper(i + 1))
                     nums[j] -= nums[i]
             return ans
 
+
+        # Build the graph
         accounts = collections.defaultdict(int)
         for u, v, w in transactions:
-            accounts[v] += w
             accounts[u] -= w
-
+            accounts[v] += w
         nums = list(accounts.values())
-        return dfs_helper(0)
-
+        return dfs_helper()
         
