0026abd61dbaf76e2b004cf8ccd0eeded395a225
Long Bian <bianlong0618@qq.com>
2025-02-21 22:55:57 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3563-select-cells-in-grid-with-maximum-score/3563-select-cells-in-grid-with-maximum-score.py
+++ 3563-select-cells-in-grid-with-maximum-score/3563-select-cells-in-grid-with-maximum-score.py
@@ -1,16 +1,17 @@
 class Solution:
     def maxScore(self, grid: List[List[int]]) -> int:
-        def dfs(row_set, i):
-            if i == len(remain): return 0
+        @cache
+        def helper(i=0, cur_total=0):
+            if i == rows: return cur_total
             ans = -inf
-            for row in val_to_rows[remain[i]]:
-                if row not in row_set:
-                   ans = max(ans, remain[i] + dfs(row_set | {row}, i + 1))
-            return ans if ans != -inf else dfs(row_set, i + 1)
-
-        rows, cols, val_to_rows = len(grid), len(grid[0]), defaultdict(set)
-        for i in range(rows):
             for j in range(cols):
-                val_to_rows[grid[i][j]].add(i)
-        remain = sorted(list(val_to_rows.keys()), reverse=True)
-        return dfs(set(), 0)
+                if grid[i][j] in visited: continue
+                visited.add(grid[i][j])
+                ans = max(ans, helper(i + 1, cur_total + grid[i][j]))
+                visited.remove(grid[i][j])
+            return ans if ans != -inf else cur_total + helper(i + 1)
+
+        rows, cols, visited = len(grid), len(grid[0]), set()
+        return helper()
+
+        
