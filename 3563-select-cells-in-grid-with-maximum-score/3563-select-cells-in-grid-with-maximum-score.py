6155b50943a55abce211fd2173ac467ef14fb00a
Long Bian <bianlong0618@qq.com>
2025-02-22 10:48:36 -0500

    Time: 5180 ms (5.21%), Space: 18.2 MB (57.39%) - LeetHub

--- 3563-select-cells-in-grid-with-maximum-score/3563-select-cells-in-grid-with-maximum-score.py
+++ 3563-select-cells-in-grid-with-maximum-score/3563-select-cells-in-grid-with-maximum-score.py
@@ -1,18 +1,20 @@
 class Solution:
     def maxScore(self, grid: List[List[int]]) -> int:
-        def dfs_helper(i=0, visited=set()):
+        def dfs_helper(i=0):
             if i == len(remain): return 0
             ans = -inf
             for row in val_to_rows[remain[i]]:
                 if row not in visited:
-                    ans = max(ans, remain[i] + dfs_helper(i + 1, visited | {row}))
-            return ans if ans != -inf else dfs_helper(i + 1, visited)
+                    visited.add(row)
+                    ans = max(ans, remain[i] + dfs_helper(i + 1))
+                    visited.remove(row)
+            return ans if ans != -inf else dfs_helper(i + 1)
 
         
         rows, cols, val_to_rows = len(grid), len(grid[0]), collections.defaultdict(set)
         for i in range(rows):
             for j in range(cols):
                 val_to_rows[grid[i][j]].add(i)
-        remain = sorted(list(val_to_rows.keys()), reverse=True)
+        remain, visited = sorted(list(val_to_rows.keys()), reverse=True), set()
         return dfs_helper()
         
