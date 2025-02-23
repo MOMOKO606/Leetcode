459109a5c93a30a8e21c7bc526ed502c8d4c843c
Long Bian <bianlong0618@qq.com>
2025-02-23 18:18:40 -0500

    Time: 49 ms (85.35%), Space: 18.8 MB (38.85%) - LeetHub

---
+++ 1410-traffic-light-controlled-intersection/1410-traffic-light-controlled-intersection.py
@@ -0,0 +1,23 @@
+from threading import Lock
+
+
+class TrafficLight:
+    def __init__(self):
+        self.lock = Lock()
+        self.green_road = 1
+
+    def carArrived(
+        self,
+        carId: int,                      # ID of the car
+        roadId: int,                     # ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        direction: int,                  # Direction of the car
+        turnGreen: 'Callable[[], None]', # Use turnGreen() to turn light to green on current road
+        crossCar: 'Callable[[], None]'   # Use crossCar() to make car cross the intersection
+    ) -> None:
+        with self.lock:
+            if roadId != self.green_road:
+                turnGreen()
+                self.green_road = roadId
+            crossCar()
+    
+        
