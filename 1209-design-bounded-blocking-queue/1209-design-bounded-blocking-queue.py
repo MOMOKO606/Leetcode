a2c2122e1642ce4c393c5f6f56db316abfb0f767
Long Bian <bianlong0618@qq.com>
2025-02-24 17:24:33 -0500

    Time: 48 ms (69.34%), Space: 18.7 MB (43.78%) - LeetHub

--- 1209-design-bounded-blocking-queue/1209-design-bounded-blocking-queue.py
+++ 1209-design-bounded-blocking-queue/1209-design-bounded-blocking-queue.py
@@ -1,31 +1,33 @@
-from threading import Condition
+from threading import Condition, Lock
 
 class BoundedBlockingQueue(object):
 
     def __init__(self, capacity: int):
-        self.queue = []
+        self.lock = Lock()
         self.capacity = capacity
-        self.cv = Condition()
+        self.queue = []
+        self.space_available = Condition(self.lock)
+        self.item_available = Condition(self.lock)
         
 
     def enqueue(self, element: int) -> None:
-        with self.cv:
+        with self.lock:
             while len(self.queue) == self.capacity:
-                self.cv.wait()
+                self.space_available.wait()
             self.queue.append(element)
-            self.cv.notify_all()
-        
+            self.item_available.notify()
+    
 
     def dequeue(self) -> int:
-        with self.cv:
+        with self.lock:
             while len(self.queue) == 0:
-                self.cv.wait()
+                self.item_available.wait()
             element = self.queue.pop(0)
-            self.cv.notify_all()
+            self.space_available.notify()
             return element
         
 
     def size(self) -> int:
-        with self.cv:
+        with self.lock:
             return len(self.queue)
         
