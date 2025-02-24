b862108bbbbb545b1d2f73e15af92988ad2f2932
Long Bian <bianlong0618@qq.com>
2025-02-24 17:17:07 -0500

    Time: 41 ms (93.02%), Space: 18.7 MB (70.36%) - LeetHub

--- 1209-design-bounded-blocking-queue/1209-design-bounded-blocking-queue.py
+++ 1209-design-bounded-blocking-queue/1209-design-bounded-blocking-queue.py
@@ -1,26 +1,31 @@
-import threading
+from threading import Condition
 
-class BoundedBlockingQueue:
+class BoundedBlockingQueue(object):
+
     def __init__(self, capacity: int):
+        self.queue = []
         self.capacity = capacity
-        self.queue = []  # 用于存储队列元素的列表
-        self.cv = threading.Condition()  # 通用的条件变量
+        self.cv = Condition()
+        
 
     def enqueue(self, element: int) -> None:
-        with self.cv:  # 获取锁
-            while len(self.queue) == self.capacity:  # 如果队列已满
-                self.cv.wait()  # 等待队列未满的条件
-            self.queue.append(element)  # 添加元素
-            self.cv.notify_all()  # 唤醒所有等待的线程（包括生产者和消费者）
+        with self.cv:
+            while len(self.queue) == self.capacity:
+                self.cv.wait()
+            self.queue.append(element)
+            self.cv.notify_all()
+        
 
     def dequeue(self) -> int:
-        with self.cv:  # 获取锁
-            while len(self.queue) == 0:  # 如果队列为空
-                self.cv.wait()  # 等待队列非空的条件
-            element = self.queue.pop(0)  # 移除元素
-            self.cv.notify_all()  # 唤醒所有等待的线程（包括生产者和消费者）
+        with self.cv:
+            while len(self.queue) == 0:
+                self.cv.wait()
+            element = self.queue.pop(0)
+            self.cv.notify_all()
             return element
+        
 
     def size(self) -> int:
-        with self.cv:  # 获取锁
+        with self.cv:
             return len(self.queue)
+        
