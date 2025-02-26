0335695f69c76aa0f8bee1ff4452402f3585af33
Long Bian <bianlong0618@qq.com>
2025-02-26 00:00:04 -0500

    Time: 1828 ms (7.69%), Space: 25.6 MB (100%) - LeetHub

--- 3204-maximum-profitable-triplets-with-increasing-prices-ii/3204-maximum-profitable-triplets-with-increasing-prices-ii.py
+++ 3204-maximum-profitable-triplets-with-increasing-prices-ii/3204-maximum-profitable-triplets-with-increasing-prices-ii.py
@@ -1,36 +1,38 @@
 class FenwickTree:
-    def __init__(self, value, size):
+    def __init__(self, size, value):
         self.size = size
         self.nums = [value] * size
 
     def lowbit(self, index):
         return index & -index
 
-    def update(self, i, value):
-        while i < self.size:
-            self.nums[i] = max(self.nums[i], value)
-            i += self.lowbit(i)
+    def update(self, index, value):
+        while index < self.size:
+            self.nums[index] = max(self.nums[index], value)
+            index += self.lowbit(index)
 
-    def query(self, i):
-        ans = -inf
-        while i:
-            ans = max(ans, self.nums[i])
-            i -= self.lowbit(i)
+    def query(self, index):
+        ans = 0
+        while index:
+            ans = max(ans, self.nums[index])
+            index -= self.lowbit(index)
         return ans
 
 
 class Solution:
     def maxProfit(self, prices: List[int], profits: List[int]) -> int:
         max_price = max(prices)
-        single, double, ans = FenwickTree(0, max_price + 1), FenwickTree(0, max_price + 1), -1
+        single, double, ans = FenwickTree(max_price + 1, 0), FenwickTree(max_price + 1, 0), 0
         for price, profit in zip(prices, profits):
             single.update(price, profit)
-            single_max = single.query(price - 1)
-            if single_max:
-                double.update(price, profit + single_max)
-            double_max = double.query(price - 1)
-            if double_max:
-                ans = max(ans, double_max + profit)
-        return ans
+            cur_single = single.query(price - 1)
+            if cur_single:
+                double.update(price, cur_single + profit)
+
+            cur_double = double.query(price - 1)
+            if cur_double:
+                ans = max(ans, cur_double + profit)
+
+        return ans if ans != 0 else -1
 
         
