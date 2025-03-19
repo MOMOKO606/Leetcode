c65ac0164f7d304a91d83c2d0d05f5f333ef0102
Long Bian <bianlong0618@qq.com>
2025-03-19 19:12:30 -0400

    Time: 3 ms (90.82%), Space: 17.9 MB (14.68%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,11 +1,9 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        left, right = collections.defaultdict(int), collections.defaultdict(int)
-        for i, char in enumerate(s):
-            right[char] = i
-        for j in range(len(s) - 1, -1, -1):
-            left[s[j]] = j
-        intervals = sorted([[left[char], right[char]] for char in set(s)])
+        letters, intervals = set(s), []
+        for letter in letters:
+            intervals.append([s.find(letter), s.rfind(letter)])
+        intervals.sort()
         cur_left, cur_right, ans = intervals[0][0], intervals[0][1], []
         for i in range(1, len(intervals)):
             if intervals[i][0] < cur_right:
@@ -14,6 +12,6 @@ class Solution:
                 ans.append(cur_right - cur_left + 1)
                 cur_left, cur_right = intervals[i][0], intervals[i][1]
         return ans + [cur_right - cur_left + 1]
-        
-       
+
+            
         
