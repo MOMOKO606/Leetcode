f4b23b3f814a88599f5f1d28e6e8ed1f48e4b2a2
Long Bian <bianlong0618@qq.com>
2025-03-20 23:54:10 -0400

    Time: 3 ms (78.86%), Space: 21.9 MB (78.82%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.py
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -3,10 +3,10 @@ class Solution:
         fives, tens = 0, 0
         for bill in bills:
             if bill == 5: fives += 1
-            elif bill == 10: fives, tens = fives - 1, tens + 1
-            else: 
+            if bill == 10: fives, tens = fives - 1, tens + 1
+            if bill == 20:
                 if tens: tens, fives = tens - 1, fives - 1
                 else: fives -= 3
-            if not (fives >= 0 and tens >= 0): return False
+            if fives < 0: return False
         return True
         
