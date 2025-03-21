b38a7e3b3bb5b010224f73199388034af25e805d
Long Bian <bianlong0618@qq.com>
2025-03-20 23:02:17 -0400

    Time: 0 ms (100%), Space: 17.9 MB (36.81%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,12 +1,10 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        if not num: return 0
-        digits = []
-        while num: digits, num = digits + [num % 10], num // 10
-
-        while len(digits) > 1:
-            num = sum([digit for digit in digits])
-            digits = []
-            while num: digits, num = digits + [num % 10], num // 10
-        return digits[0]
+        while num > 9:
+            next_num = 0
+            while num:
+                next_num += num % 10
+                num //= 10
+            num = next_num
+        return num
         
