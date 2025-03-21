0591a71b2718bf6148051a920e5ff144c268b3ab
Long Bian <bianlong0618@qq.com>
2025-03-20 22:37:40 -0400

    Time: 7 ms (55.55%), Space: 19 MB (9.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = 0
-        for j, num in enumerate(nums):
-            if num != 0:
-                nums[i], nums[j], i = nums[j], nums[i], i + 1
-
+        i = -1
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                i += 1
+                nums[i], nums[j] = nums[j], nums[i]
         
