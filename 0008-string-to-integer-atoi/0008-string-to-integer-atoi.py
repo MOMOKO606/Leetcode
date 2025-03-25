4b882738c685a32b61f1fae0ecb0a3063ac271d2
Long Bian <bianlong0618@qq.com>
2025-03-24 22:30:49 -0400

    Time: 0 ms (100%), Space: 17.8 MB (77.29%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,19 +1,13 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        # Step1. Ignore any leading whitespace
         i = 0
         while i < len(s) and s[i] == " ":
             i += 1
+        
+        positive = 1
+        if i < len(s) and s[i] == "-": positive, i = -1, i + 1
+        elif i < len(s) and s[i] == "+": i += 1
 
-        # Step2. 
-        negative = 1
-        if i < len(s) and s[i] == "+":
-            i += 1
-        elif i < len(s) and s[i] == "-":
-            negative = -1
-            i += 1
-
-        # Step3
         while i < len(s) and s[i] == "0":
             i += 1
 
@@ -21,12 +15,6 @@ class Solution:
         while i < len(s) and s[i].isdigit():
             ans = ans * 10 + int(s[i])
             i += 1
-
-
-        return sorted([-2 ** 31, 2 ** 31 - 1, ans * negative])[1]
-
-
-        
-
-        
+        ans *= positive
+        return sorted([-2 ** 31, ans, 2 ** 31 - 1])[1]
         
