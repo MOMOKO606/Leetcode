6a4b5aecac16ad00497e8ac6722cea0dbbb372fb
Long Bian <bianlong0618@qq.com>
2025-04-01 21:22:59 -0400

    Time: 42 ms (39.28%), Space: 17.9 MB (20.62%) - LeetHub

--- 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.py
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.py
@@ -1,16 +1,14 @@
 class Solution:
     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
-        transfer = {
-            "a": 1,
-            "b": 2,
-            "c": 3,
-            "d": 4,
-            "e": 5,
-            "f": 6,
-            "g": 7,
-            "h": 8
+        trans = {
+            "a": 0,
+            "b": 1,
+            "c": 2,
+            "d": 3,
+            "e": 4,
+            "f": 5,
+            "g": 6,
+            "h": 7,
             }
-
-        return (transfer[coordinate1[0]] + int(coordinate1[1])) & 1 == (transfer[coordinate2[0]] + int(coordinate2[1])) & 1
-        
+        return (trans[coordinate1[0]] + int(coordinate1[1])) & 1 ==  (trans[coordinate2[0]] + int(coordinate2[1])) & 1
         
