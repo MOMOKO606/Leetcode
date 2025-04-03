345c7b3cd4ca03dd2ac352e90c729a28ba1d922e
Long Bian <bianlong0618@qq.com>
2025-04-03 16:48:40 -0400

    Time: 211 ms (76.55%), Space: 90.9 MB (76.55%) - LeetHub

--- 3495-k-th-nearest-obstacle-queries/3495-k-th-nearest-obstacle-queries.py
+++ 3495-k-th-nearest-obstacle-queries/3495-k-th-nearest-obstacle-queries.py
@@ -1,13 +1,11 @@
 class Solution:
     def resultsArray(self, queries: List[List[int]], k: int) -> List[int]:
-        max_heap, ans, heap_size = [], [], 0
-        for i, (x, y) in enumerate(queries):
+        max_heap, ans = [], []
+        for x, y in queries:
             heapq.heappush(max_heap, -(abs(x) + abs(y)))
-            heap_size += 1
-            if heap_size > k:
-                heapq.heappop(max_heap)
-                heap_size -= 1
-            if heap_size == k: ans.append(-max_heap[0])
-            else: ans.append(-1)
+            if len(max_heap) > k: heapq.heappop(max_heap)
+            if len(max_heap) < k: ans.append(-1)
+            else: ans.append(-max_heap[0])
         return ans
+            
         
