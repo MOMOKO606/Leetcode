3773034b208efb0cb5db4b62676f79aa0c5df242
Long Bian <bianlong0618@qq.com>
2025-04-03 16:49:35 -0400

    Time: 219 ms (60.69%), Space: 91.1 MB (39.31%) - LeetHub

--- 3495-k-th-nearest-obstacle-queries/3495-k-th-nearest-obstacle-queries.py
+++ 3495-k-th-nearest-obstacle-queries/3495-k-th-nearest-obstacle-queries.py
@@ -3,9 +3,12 @@ class Solution:
         max_heap, ans = [], []
         for x, y in queries:
             heapq.heappush(max_heap, -(abs(x) + abs(y)))
-            if len(max_heap) > k: heapq.heappop(max_heap)
-            if len(max_heap) < k: ans.append(-1)
-            else: ans.append(-max_heap[0])
+            if len(max_heap) > k: 
+                heapq.heappop(max_heap)
+            if len(max_heap) < k: 
+                ans.append(-1)
+                continue
+            ans.append(-max_heap[0])
         return ans
             
         
