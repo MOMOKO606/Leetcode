907c1f32f57a7a30710f34ce5c22c4608b168aa9
Long Bian <bianlong0618@qq.com>
2025-04-04 21:38:46 -0400

    Time: 135 ms (23.46%), Space: 36.7 MB (20.99%) - LeetHub

--- 3413-find-the-first-player-to-win-k-games-in-a-row/3413-find-the-first-player-to-win-k-games-in-a-row.py
+++ 3413-find-the-first-player-to-win-k-games-in-a-row/3413-find-the-first-player-to-win-k-games-in-a-row.py
@@ -1,12 +1,14 @@
 class Solution:
     def findWinningPlayer(self, skills: List[int], k: int) -> int:
         if k >= len(skills): return skills.index(max(skills))
-        queue, count = collections.deque([(skill, i) for i, skill in enumerate(skills)]), 0
-        while True:
-            if queue[0][0] > queue[1][0]: queue[0], queue[1], count = queue[1], queue[0], count + 1
-            else: count = 1
-            queue.append(queue.popleft())
-            if count == k: return queue[0][1]
-
-
+        deque = collections.deque([(skill, i) for i, skill in enumerate(skills)])
+        count= 0
+        while count < k:
+            if deque[0][0] > deque[1][0]:
+                count += 1
+                deque[0], deque[1] = deque[1], deque[0]     
+            else:
+                count = 1
+            deque.append(deque.popleft())
+        return deque[0][1]
         
