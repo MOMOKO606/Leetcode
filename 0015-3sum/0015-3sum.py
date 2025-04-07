aeed22a160fb163312f34963147c526418af168c
Long Bian <bianlong0618@qq.com>
2025-04-06 22:39:04 -0400

    Time: 483 ms (81.34%), Space: 22.6 MB (5.82%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,37 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        ans = []
-        for k in range(len(nums) - 2):
-            if k > 0 and nums[k] == nums[k - 1]: continue
-            i, j = k + 1, len(nums) - 1
-            while i < j:
-                pivot = nums[i] + nums[j] + nums[k]
-                if pivot == 0: 
-                    ans.append([nums[k], nums[i], nums[j]])
-                    i += 1
-                    while i < len(nums) and nums[i] == nums[i - 1]:
+        def helper(n=3, pos=0, seq=[], target=0):
+            if n == 2:
+                i, j = pos, len(nums) - 1
+                while i < j:
+                    pivot = nums[i] + nums[j]
+                    if pivot == target:
+                        self.ans.append(seq + [nums[i], nums[j]])
                         i += 1
-                    j -= 1
-                    while j >= 0 and nums[j] == nums[j + 1]:
+                        while i < len(nums) and nums[i] == nums[i - 1]:
+                            i += 1
                         j -= 1
-                elif pivot < 0:
-                    i += 1
-                else:
-                    j -= 1
-        return ans
+                        while j >= 0 and nums[j] == nums[j + 1]:
+                            j -= 1
+                    elif pivot < target:
+                        i += 1
+                    else:
+                        j -= 1
+                return
+            
+            for k in range(pos, len(nums) - n + 1):
+                if k > pos and nums[k] == nums[k - 1]: continue
+                helper(n - 1, k + 1, seq + [nums[k]], target - nums[k])
         
+        nums.sort()
+        self.ans = []
+        helper()
+        return self.ans
+
+
+                    
+
+
+
 
         
